67c25a71f0233d95a2ccaad52c3188646e41b644
DmitriVanGuard <dimavanguard@gmail.com>
2018-12-28 13:10:23 +0300

    Delay removed

--- src/components/WordCloud/fetchWordContextEpic.js
+++ src/components/WordCloud/fetchWordContextEpic.js
@@ -8,7 +8,7 @@ export const fetchWordContextEpic = (action$, store, { client }) =>
   action$
     .ofType(actions.WORDCLOUD_CONTEXT_FETCH)
     .switchMap(({ payload: word }) => {
-      const startTime = Date.now()
+      // const startTime = Date.now()
 
       if (store.getState().wordCloud.word === word) {
         return Observable.of({
@@ -17,32 +17,34 @@ export const fetchWordContextEpic = (action$, store, { client }) =>
         })
       }
 
-      return Observable.fromPromise(
-        client.query({
-          query: wordCloudGQL,
-          variables: {
-            word,
-            to: moment().toISOString(),
-            from: moment()
-              .subtract(3, 'd') // @NOTE(vanguard) query fails, if the value is more in past
-              .toISOString(),
-            size: 25
-          }
-        })
-      )
-        .delayWhen(() => Observable.timer(500 + startTime - Date.now()))
-        .mergeMap(({ data: { wordContext } }) => {
-          return Observable.of({
-            type: actions.WORDCLOUD_CONTEXT_FETCH_SUCCESS,
-            payload: {
+      return (
+        Observable.fromPromise(
+          client.query({
+            query: wordCloudGQL,
+            variables: {
               word,
-              cloud: wordContext,
-              isLoading: false,
-              error: false
+              to: moment().toISOString(),
+              from: moment()
+                .subtract(3, 'd') // @NOTE(vanguard) query fails, if the value is more in past
+                .toISOString(),
+              size: 25
             }
           })
-        })
-        .catch(
-          handleErrorAndTriggerAction(actions.WORDCLOUD_CONTEXT_FETCH_FAILED)
         )
+          // .delayWhen(() => Observable.timer(500 + startTime - Date.now()))
+          .mergeMap(({ data: { wordContext } }) => {
+            return Observable.of({
+              type: actions.WORDCLOUD_CONTEXT_FETCH_SUCCESS,
+              payload: {
+                word,
+                cloud: wordContext,
+                isLoading: false,
+                error: false
+              }
+            })
+          })
+          .catch(
+            handleErrorAndTriggerAction(actions.WORDCLOUD_CONTEXT_FETCH_FAILED)
+          )
+      )
     })
