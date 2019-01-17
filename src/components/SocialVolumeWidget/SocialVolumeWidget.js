8a07c2188dcf2ceaf3099f0cccf93e5c49819711
DmitriVanGuard <dimavanguard@gmail.com>
2019-01-17 18:39:03 +0300

    Widget stories

--- src/components/SocialVolumeWidget/SocialVolumeWidget.js
+++ src/components/SocialVolumeWidget/SocialVolumeWidget.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BarChart, Bar, XAxis, YAxis, ResponsiveContainer } from 'recharts'
+import { FadeIn } from 'animate-components'
 import moment from 'moment'
 import { connect } from 'react-redux'
 import styles from './SocialVolumeWidget.module.scss'
@@ -10,41 +11,74 @@ const RoundBar = props => {
   return <rect x={x} y={y} width='6px' height={height} rx='3' fill='#E0E4EE' />
 }
 
-const SocialVolumeWidget = ({ socialVolumeData, slug = 'bitcoin' }) => (
-  <div className={styles.chart}>
-    <div className={styles.info}>
-      <span className={styles.slug}>{slug}</span> social volume score
-    </div>
-    <ResponsiveContainer width='100%' height={170}>
-      <BarChart
-        data={socialVolumeData}
-        margin={{ top: 0, right: -25, left: 0, bottom: -10 }}
-      >
-        <XAxis
-          dataKey='datetime'
-          interval='preserveStartEnd'
-          axisLine={false}
-          tickLine={false}
-          tickFormatter={date => {
-            const tickDate = moment(date)
-            console.log(tickDate.isSame(Date.now(), 'year'))
-            return tickDate.isSame(Date.now(), 'year')
-              ? tickDate.format('MMM DD')
-              : tickDate.format('MMM DD YYYY')
-          }}
-          minTickGap={20}
-        />
-        <YAxis orientation='right' axisLine={false} tickLine={false} />
+const SocialVolumeWidget = ({ data = [], slug, isLoading, error }) => {
+  if (isLoading) {
+    return (
+      <div className={styles.wrapper + ' ' + styles.WordCloudLoading}>
+        <FadeIn duration='2s' timingFunction='ease-out'>
+          <h3>Loading...</h3>
+        </FadeIn>
+      </div>
+    )
+  }
 
-        <Bar dataKey='mentionsCount' shape={<RoundBar />} />
-      </BarChart>
-    </ResponsiveContainer>
-  </div>
-)
+  if (error) {
+    return (
+      <div className={styles.wrapper + ' ' + styles.WordCloudLoading}>
+        <FadeIn duration='2s' timingFunction='ease-out'>
+          <h3>Can't find anything about this trend...</h3>
+        </FadeIn>
+      </div>
+    )
+  }
+
+  if (data.length === 0) {
+    return (
+      <div className={styles.wrapper + ' ' + styles.WordCloudLoading}>
+        <FadeIn duration='2s' timingFunction='ease-out'>
+          <h3>Choose any word below to see its social context</h3>
+        </FadeIn>
+      </div>
+    )
+  }
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.info}>
+        <span className={styles.slug}>{slug}</span> social volume score
+      </div>
+      <ResponsiveContainer width='100%' height={170}>
+        <BarChart
+          data={data}
+          margin={{ top: 0, right: -25, left: 0, bottom: -10 }}
+        >
+          <XAxis
+            dataKey='datetime'
+            interval='preserveStartEnd'
+            axisLine={false}
+            tickLine={false}
+            tickFormatter={date => {
+              const tickDate = moment(date)
+
+              return tickDate.isSame(Date.now(), 'year')
+                ? tickDate.format('MMM DD')
+                : tickDate.format('MMM DD YYYY')
+            }}
+            minTickGap={20}
+          />
+          <YAxis orientation='right' axisLine={false} tickLine={false} />
+
+          <Bar dataKey='mentionsCount' shape={<RoundBar />} />
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  )
+}
 
 const mapStateToProps = state => ({
   slug: state.socialVolume.slug,
-  socialVolumeData: state.socialVolume.data
+  data: state.socialVolume.data
 })
 
+export const UnwrappedSocialVolumeWidget = SocialVolumeWidget
 export default connect(mapStateToProps)(SocialVolumeWidget)
