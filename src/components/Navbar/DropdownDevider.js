4d5c75a1940b11e71d9757fdba09d1929d519df2
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-04 18:45:55 +0300

    Devider styles updated

--- src/components/Navbar/DropdownDevider.js
+++ src/components/Navbar/DropdownDevider.js
@@ -1,16 +1,8 @@
 import React from 'react'
+import styles from './Devider.module.scss'
 
-const DropdownDevider = ({ style, ...props }) => {
-  return (
-    <div
-      {...props}
-      style={{
-        height: 1,
-        background: '#ebeef5',
-        ...style
-      }}
-    />
-  )
+const DropdownDevider = ({ ...props }) => {
+  return <div {...props} className={styles.wrapper} />
 }
 
 export default DropdownDevider
