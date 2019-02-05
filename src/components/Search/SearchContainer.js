ea4220045a6fe7b203c54fd627ca798355d568d6
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-05 19:57:50 +0300

    Using san-ui searchwithsuggestions

--- src/components/Search/SearchContainer.js
+++ src/components/Search/SearchContainer.js
@@ -1,52 +1,103 @@
-import React from 'react'
-import { push } from 'react-router-redux'
-import { connect } from 'react-redux'
-import { graphql } from 'react-apollo'
+import React, { Fragment } from 'react'
 import { compose } from 'recompose'
-import { allProjectsForSearchGQL } from './../../pages/Projects/allProjectsGQL'
-import { getAll } from './../../pages/Projects/projectSelectors'
-import * as actions from './../../actions/types'
-import Search from './Search'
+import { graphql } from 'react-apollo'
+import { withRouter } from 'react-router-dom'
+import { SearchWithSuggestions, Button } from '@santiment-network/ui'
+import { allProjectsForSearchGQL } from '../../pages/Projects/allProjectsGQL'
+import ProjectIcon from '../ProjectIcon'
+import styles from './SearchContainer.module.scss'
 
-const SearchContainer = ({ projects = [], isFocused, resetFocus, goto }) => {
-  if (projects.length === 0) {
-    return <Search loading />
-  }
+export const NavbarAssetsSearch = ({ history, data: { allProjects = [] } }) => {
+  console.log(allProjects)
   return (
-    <Search
-      onSelectProject={cmcId => goto(cmcId)}
-      focus={isFocused}
-      resetFocus={resetFocus}
-      projects={projects}
+    <SearchWithSuggestions
+      className={styles.wrapper}
+      iconPosition='left'
+      onSuggestionSelect={({ coinmarketcapId }) =>
+        history.push(`/projects/${coinmarketcapId}`)
+      }
+      data={allProjects}
+      predicate={searchTerm => {
+        const upperCaseSearchTerm = searchTerm.toUpperCase()
+        return ({ ticker, name }) =>
+          name.toUpperCase().includes(upperCaseSearchTerm) ||
+          ticker.toUpperCase().includes(upperCaseSearchTerm)
+      }}
+      suggestionContent={({ name, ticker }) => {
+        return (
+          <Fragment>
+            <ProjectIcon
+              className={styles.icon}
+              size={16}
+              ticker={ticker}
+              name={name}
+            />{' '}
+            {name} ({ticker})
+          </Fragment>
+        )
+      }}
+      // className={styles.search}
     />
   )
 }
 
-const mapDataToProps = ({ allProjects }) => ({
-  projects: getAll(allProjects.allProjects)
-})
-
-const mapStateToProps = ({ rootUi }) => ({
-  isFocused: rootUi.isSearchInputFocused
-})
-
-const mapDispatchToProps = dispatch => ({
-  resetFocus: () => dispatch({ type: actions.APP_TOGGLE_SEARCH_FOCUS }),
-  goto: cmcId => dispatch(push(`/projects/${cmcId}`))
-})
-
 const enhance = compose(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  ),
-  graphql(allProjectsForSearchGQL, {
-    name: 'allProjects',
-    props: mapDataToProps,
-    options: () => ({
-      context: { isRetriable: true }
-    })
-  })
+  graphql(allProjectsForSearchGQL),
+  withRouter
 )
 
-export default enhance(SearchContainer)
+export default enhance(NavbarAssetsSearch)
+
+/* import React from 'react' */
+/* import { push } from 'react-router-redux' */
+/* import { connect } from 'react-redux' */
+/* import { graphql } from 'react-apollo' */
+/* import { compose } from 'recompose' */
+/* import { allProjectsForSearchGQL } from './../../pages/Projects/allProjectsGQL' */
+/* import { getAll } from './../../pages/Projects/projectSelectors' */
+/* import * as actions from './../../actions/types' */
+/* import Search from './Search' */
+/*  */
+/* const SearchContainer = ({ projects = [], isFocused, resetFocus, goto }) => { */
+/* if (projects.length === 0) { */
+/* return <Search loading /> */
+/* } */
+/* return ( */
+/* <Search */
+/* onSelectProject={cmcId => goto(cmcId)} */
+/* focus={isFocused} */
+/* resetFocus={resetFocus} */
+/* projects={projects} */
+/* /> */
+/* ) */
+/* } */
+/*  */
+/* const mapDataToProps = ({ allProjects }) => ({ */
+/* projects: getAll(allProjects.allProjects) */
+/* }) */
+/*  */
+/* const mapStateToProps = ({ rootUi }) => ({ */
+/* isFocused: rootUi.isSearchInputFocused */
+/* }) */
+/*  */
+/* const mapDispatchToProps = dispatch => ({ */
+/* resetFocus: () => dispatch({ type: actions.APP_TOGGLE_SEARCH_FOCUS }), */
+/* goto: cmcId => dispatch(push(`/projects/${cmcId}`)) */
+/* }) */
+/*  */
+
+/* const enhance = compose( */
+/* connect( */
+/* mapStateToProps, */
+/* mapDispatchToProps */
+/* ), */
+/* graphql(allProjectsForSearchGQL, { */
+/* name: 'allProjects', */
+/* props: mapDataToProps, */
+/* options: () => ({ */
+/* context: { isRetriable: true } */
+/* }) */
+/* }) */
+/* ) */
+/*  */
+/* export default enhance(SearchContainer) */
