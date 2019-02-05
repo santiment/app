39470121c15b9473d1ed2a1c6d95ca71f27e3d62
Dmitri Marov <dimavanguard@gmail.com>
2019-02-05 22:31:29 +0300

    Update SearchContainer.js

--- src/components/Search/SearchContainer.js
+++ src/components/Search/SearchContainer.js
@@ -36,7 +36,6 @@ export const NavbarAssetsSearch = ({ history, data: { allProjects = [] } }) => {
           </Fragment>
         )
       }}
-      // className={styles.search}
     />
   )
 }
@@ -47,57 +46,3 @@ const enhance = compose(
 )
 
 export default enhance(NavbarAssetsSearch)
-
-/* import React from 'react' */
-/* import { push } from 'react-router-redux' */
-/* import { connect } from 'react-redux' */
-/* import { graphql } from 'react-apollo' */
-/* import { compose } from 'recompose' */
-/* import { allProjectsForSearchGQL } from './../../pages/Projects/allProjectsGQL' */
-/* import { getAll } from './../../pages/Projects/projectSelectors' */
-/* import * as actions from './../../actions/types' */
-/* import Search from './Search' */
-/*  */
-/* const SearchContainer = ({ projects = [], isFocused, resetFocus, goto }) => { */
-/* if (projects.length === 0) { */
-/* return <Search loading /> */
-/* } */
-/* return ( */
-/* <Search */
-/* onSelectProject={cmcId => goto(cmcId)} */
-/* focus={isFocused} */
-/* resetFocus={resetFocus} */
-/* projects={projects} */
-/* /> */
-/* ) */
-/* } */
-/*  */
-/* const mapDataToProps = ({ allProjects }) => ({ */
-/* projects: getAll(allProjects.allProjects) */
-/* }) */
-/*  */
-/* const mapStateToProps = ({ rootUi }) => ({ */
-/* isFocused: rootUi.isSearchInputFocused */
-/* }) */
-/*  */
-/* const mapDispatchToProps = dispatch => ({ */
-/* resetFocus: () => dispatch({ type: actions.APP_TOGGLE_SEARCH_FOCUS }), */
-/* goto: cmcId => dispatch(push(`/projects/${cmcId}`)) */
-/* }) */
-/*  */
-
-/* const enhance = compose( */
-/* connect( */
-/* mapStateToProps, */
-/* mapDispatchToProps */
-/* ), */
-/* graphql(allProjectsForSearchGQL, { */
-/* name: 'allProjects', */
-/* props: mapDataToProps, */
-/* options: () => ({ */
-/* context: { isRetriable: true } */
-/* }) */
-/* }) */
-/* ) */
-/*  */
-/* export default enhance(SearchContainer) */
