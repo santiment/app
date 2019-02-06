e675da74cf1a12f34769460fd0f7cfb147eaa83c
Yura Zatcepin <yura.zatsepin@gmail.com>
2019-02-06 12:11:47 +0300

    Clean

--- src/pages/Account/AccountEthKeyForm.js
+++ src/pages/Account/AccountEthKeyForm.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { connect } from 'react-redux'
 import { Input, Button } from '@santiment-network/ui'
 import { getUserWallet } from './../UserSelectors'
