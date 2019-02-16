08770842115551657629dc7f23b8db540f020875
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-16 21:10:00 +0300

    Junk removed

--- src/pages/SonarFeed/SonarFeedRecommendations.js
+++ src/pages/SonarFeed/SonarFeedRecommendations.js
@@ -1,31 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import SignalCardsGrid from '../../components/SignalCard/SignalCardsGrid'
+import SignalCardsGrid, {
+  defaultSignals
+} from '../../components/SignalCard/SignalCardsGrid'
 import Image from './sonar_activity_artboard.png'
 import styles from './SonarFeedRecommendations.module.scss'
 
-const defaultSignals = [
-  {
-    id: 0,
-    title: 'Daily trending words',
-    description:
-      'Subscribe to this signal to get daily list of trending words connected with crypto',
-    author: 'Santiment team',
-    subscriptionsNumber: 0,
-    isSubscribed: false,
-    isPublished: true
-  },
-  {
-    id: 1,
-    title: 'Ethereum price tracking',
-    description:
-      'Subscribe to this signal to track the activity of selected address based on the Ethereum',
-    author: 'Santiment team',
-    subscriptionsNumber: 0,
-    isSubscribed: false,
-    isPublished: true
-  }
-]
 const SonarFeedRecommendations = () => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
