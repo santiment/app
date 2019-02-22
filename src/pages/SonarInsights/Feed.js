d7beeb0bf6f7cc31882506961feef94a4d0d4e4d
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-22 16:39:28 +0300

    Using BigList in feed

--- src/pages/SonarInsights/Feed.js
+++ src/pages/SonarInsights/Feed.js
@@ -1,28 +1,35 @@
 import React, { Fragment } from 'react'
 import moment from 'moment'
-import LazyLoad from 'react-lazyload'
 import styles from './Feed.module.scss'
+import BigList from './BigList'
 
 // NOTE(vanguard): implement infinite-scroll, not lazy load
 const Feed = ({ component: El, data, dateKey }) => {
   let lastDateKey
 
-  return data.map((item, index) => {
-    const id = item.id || index
-    const date = moment(item[dateKey]).format('MMM D')
-    const isNotSameAsLastDate = date !== lastDateKey
+  return (
+    <BigList
+      list={data}
+      render={list =>
+        list.map((item, index) => {
+          const id = item.id || index
+          const date = moment(item[dateKey]).format('MMM D')
+          const isNotSameAsLastDate = date !== lastDateKey
 
-    if (isNotSameAsLastDate) {
-      lastDateKey = date
-    }
+          if (isNotSameAsLastDate) {
+            lastDateKey = date
+          }
 
-    return (
-      <LazyLoad offset={1000} once key={id}>
-        {isNotSameAsLastDate && <h4 className={styles.date}>{date}</h4>}
-        <El className={styles.signal} {...item} />
-      </LazyLoad>
-    )
-  })
+          return (
+            <Fragment key={id}>
+              {isNotSameAsLastDate && <h4 className={styles.date}>{date}</h4>}
+              <El className={styles.signal} {...item} />
+            </Fragment>
+          )
+        })
+      }
+    />
+  )
 }
 
 export default Feed
