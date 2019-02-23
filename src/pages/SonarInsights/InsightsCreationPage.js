34f1a442020137d6293c5d3b947f676be6b9073a
DmitriVanGuard <dimavanguard@gmail.com>
2019-02-23 21:24:57 +0300

    Insights creation title auto resizable textarea

--- src/pages/SonarInsights/InsightsCreationPage.js
+++ src/pages/SonarInsights/InsightsCreationPage.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react'
 import Editor from './Editor'
+import styles from './InsightsCreationPage.module.scss'
 
 class InsightsCreationPage extends Component {
+  state = {
+    titleInput: {
+      rows: 1,
+      value: ''
+    }
+  }
+
+  componentDidMount () {
+    this.titleOneLineHeight = this.titleInputRef.current.clientHeight
+  }
+
+  onTitleChange = ({ currentTarget }) => {
+    currentTarget.rows = 1
+    currentTarget.rows = currentTarget.scrollHeight / this.titleOneLineHeight
+
+    this.setState({
+      titleInput: {
+        value: currentTarget.value
+      }
+    })
+  }
+
+  titleInputRef = React.createRef()
+
   render () {
+    const {
+      titleInput: { rows, value }
+    } = this.state
+
     return (
       <div>
-        123
-        <Editor />
+        <textarea
+          rows='1'
+          ref={this.titleInputRef}
+          className={styles.title}
+          value={value}
+          placeholder="Insight's title"
+          onChange={this.onTitleChange}
+        />
+        <Editor placeholder='Write something interesting here...' />
       </div>
     )
   }
