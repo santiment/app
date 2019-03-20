583aebd4c581d155bea84377243ebcf4f70fd4e5
devanshu <devanshu.g@santiment.net>
2019-03-20 23:12:37 +0530

    Updated descriptions of the cards

--- src/pages/Labs.js
+++ src/pages/Labs.js
@@ -6,7 +6,7 @@ const cards = [
   {
     title: 'Historical balance',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+      'look up any wallet’s history of Ethereum and ERC20 holdings. Choose up to 5 assets at a time for a detailed breakdown of the wallet’s balance over the last 15 months.',
     link: '/labs/balance',
     className: styles.historicalBalance,
     bgClassName: styles.historicalBalanceBg
@@ -14,15 +14,15 @@ const cards = [
   {
     title: 'Social trends',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+      'Explore the social volume of ANY word/phrase on crypto social media over time. Results gathered from 100s of Telegram channels, crypto subreddits, discord groups, private trading chats and more.',
     link: '/labs/trends',
     className: styles.trends,
     bgClassName: styles.trendsBg
   },
   {
-    title: 'Dashboards',
+    title: 'SANgraphs',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.',
+      'Our advanced on-chain, social and development metrics for all crypto projects in Santiment’s database. For those that want a full 360° overview of the crypto market and its biggest driving forces.',
     link: '/dashboards',
     className: styles.dashboard,
     bgClassName: styles.dashboardBg
