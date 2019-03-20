ba4a76b84f326fb3fd288c5f7cdce8545507d7b7
Yura Zatsepin <yura.zatsepin@gmail.com>
2019-03-20 08:30:48 +0000

    Add signals last activities

--- src/pages/SonarFeed/SonarFeedActivityPage.js
+++ src/pages/SonarFeed/SonarFeedActivityPage.js
@@ -1,22 +1,48 @@
 import React, { Fragment } from 'react'
+import { graphql } from 'react-apollo'
+import Markdown from 'react-markdown'
+import gql from 'graphql-tag'
 import SonarFeedRecommendations from './SonarFeedRecommendations'
 import SignalCard from '../../components/SignalCard/SignalCard'
 import styles from './SonarFeedActivityPage.module.scss'
 
-const SonarFeedActivityPage = ({ activities }) => {
+export const TRIGGER_ACTIVITIES_QUERY = gql`
+  query signalsHistoricalActivity($datetime: DateTime!) {
+    activities: signalsHistoricalActivity(
+      limit: 5
+      cursor: { type: BEFORE, datetime: $datetime }
+    ) {
+      cursor {
+        before
+        after
+      }
+      activity {
+        payload
+        triggeredAt
+        userTrigger {
+          trigger {
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
+
+const SonarFeedActivityPage = ({ activities, isLoading, isError, isEmpty }) => {
+  if (isLoading) {
+    return <div className={styles.wrapper}>Loading...</div>
+  }
+  if (isEmpty) {
+    return ''
+  }
   return activities ? (
     <div className={styles.wrapper}>
-      {Object.keys(activities).map(date => (
-        <Fragment key={date}>
-          <h4 className={styles.date}>{date}</h4>
-          {activities[date].map(({ id, title, description }) => (
-            <SignalCard
-              key={id}
-              className={styles.signal}
-              title={title}
-              description={description}
-            />
-          ))}
+      {activities.map(activity => (
+        <Fragment key={activity.triggeredAt}>
+          <h4 className={styles.date}>{activity.triggeredAt}</h4>
+          <Markdown source={Object.values(activity.payload)[0]} />
         </Fragment>
       ))}
     </div>
@@ -25,4 +51,21 @@ const SonarFeedActivityPage = ({ activities }) => {
   )
 }
 
-export default SonarFeedActivityPage
+const enhance = graphql(TRIGGER_ACTIVITIES_QUERY, {
+  options: () => ({
+    variables: {
+      datetime: new Date().toISOString()
+    },
+    fetchPolicy: 'cache-and-network'
+  }),
+  props: ({ data }) => {
+    return {
+      activities: (data.activities || {}).activity,
+      isLoading: data.loading,
+      isError: !!data.error,
+      isEmpty: ((data.activities || {}).activity || []).length === 0
+    }
+  }
+})
+
+export default enhance(SonarFeedActivityPage)
