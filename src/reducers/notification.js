b7dc3f80f855f4a738cda50c8079f661666641df
devanshu <devanshu.g@santiment.net>
2019-03-25 17:30:40 +0530

    Revamp initial state

--- src/reducers/notification.js
+++ src/reducers/notification.js
@@ -1,8 +1,6 @@
 import { SHOW_NOTIFICATION } from './../actions/types.js'
 
-export const initialState = {
-  notification: null
-}
+export const initialState = null
 
 export default (state = initialState, action) => {
   switch (action.type) {
