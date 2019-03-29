f568b8be31ae15f1510f41a42b4f3b1ad72b385f
DmitriVanGuard <dimavanguard@gmail.com>
2019-03-29 16:30:39 +0300

    Updated word hightlight styles

--- src/components/WordCloud/WordCloud.js
+++ src/components/WordCloud/WordCloud.js
@@ -1,33 +1,18 @@
 import React, { Fragment } from 'react'
 import { connect } from 'react-redux'
+import cx from 'classnames'
 import TagCloud from 'react-tag-cloud'
 import HelpPopupWordCloud from './HelpPopupWordCloud'
 import WidgetTrend from '../Widget/WidgetTrend'
 import styles from './WordCloud.module.scss'
 
-const WORD_BIG = {
-  color: '#7a859e',
-  fontSize: 28
-}
-
-const WORD_MEDIUM = {
-  color: '#7a859e',
-  fontSize: 20
-}
-
-const WORD_SMALL = {
-  fontSize: 12
-}
-
 const getWordStyles = index => {
-  switch (index) {
-    case 0:
-      return WORD_BIG
-    case 1:
-    case 2:
-      return WORD_MEDIUM
-    default:
-      return WORD_SMALL
+  if (index < 3) {
+    return styles.text_big
+  }
+
+  if (index < 9) {
+    return styles.text_medium
   }
 }
 
@@ -56,11 +41,7 @@ export const WordCloud = ({
         style={{ width: '95%', height: '85%', padding: 10, marginTop: 0 }}
       >
         {cloud.map(({ word }, index) => (
-          <div
-            key={word}
-            style={getWordStyles(index)}
-            className={`${styles.text}`}
-          >
+          <div key={word} className={cx(styles.text, getWordStyles(index))}>
             {word}
           </div>
         ))}
