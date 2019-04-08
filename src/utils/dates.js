bf9e9649fc01dae117955a5ba6de6ffab6089d12
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-08 15:13:22 +0300

    Junk removed

--- src/utils/dates.js
+++ src/utils/dates.js
@@ -3,7 +3,7 @@ export const ONE_MINUTE_IN_MS = ONE_SECOND_IN_MS * 60
 export const ONE_HOUR_IN_MS = ONE_MINUTE_IN_MS * 60
 export const ONE_DAY_IN_MS = ONE_HOUR_IN_MS * 24
 export const ONE_MONTH_IN_MS = ONE_DAY_IN_MS * 29 // Estimate
-export const ONE_YEAR_IN_MS = 31557600000 - ONE_MONTH_IN_MS // Estimate
+export const ONE_YEAR_IN_MS = 31557600000 // Estimate
 
 export const SECOND = 's'
 export const MINUTE = 'min'
@@ -35,7 +35,12 @@ const FormatToString = {
   [YEAR]: 'year'
 }
 
-const IndexToFormat = [SECOND, MINUTE, HOUR, DAY, MONTH, YEAR]
+const FormatToTimestamp = {
+  [SECOND]: ONE_SECOND_IN_MS,
+  [MINUTE]: ONE_MINUTE_IN_MS,
+  [HOUR]: ONE_HOUR_IN_MS,
+  [DAY]: ONE_DAY_IN_MS
+}
 
 export const getTimeIntervalFromToday = (amount, dateFormat) => {
   const from = new Date()
@@ -53,61 +58,9 @@ export const getTimeIntervalFromToday = (amount, dateFormat) => {
   }
 }
 
-export const timeDifference = (from, to) => {
-  const diff = to - from
-  const result = []
-
-  const seconds = parseInt(diff / ONE_SECOND_IN_MS, 10)
-  result.push(seconds)
-
-  if (seconds < 60) return result
-
-  const minutes = parseInt(diff / ONE_MINUTE_IN_MS, 10)
-  result.push(minutes)
-
-  if (minutes < 60) return result
-
-  const hours = parseInt(diff / ONE_HOUR_IN_MS, 10)
-  result.push(hours)
-
-  if (hours < 24) return result
-
-  const days = parseInt(diff / ONE_DAY_IN_MS, 10)
-  result.push(days)
-
-  return result
-}
-
-const FormatToTimestamp = {
-  [SECOND]: ONE_SECOND_IN_MS,
-  [MINUTE]: ONE_MINUTE_IN_MS,
-  [HOUR]: ONE_HOUR_IN_MS,
-  [DAY]: ONE_DAY_IN_MS
-}
-
 const getDiffWithFormat = (diff, format) =>
   parseInt(diff / FormatToTimestamp[format], 10)
 
-const _getTimeFromTo = (from, to = new Date(), format = YEAR) => {
-  const isFormatYear = format === YEAR
-  if (isFormatYear || format === MONTH) {
-    const yearDiff = to.getFullYear() - from.getFullYear()
-    const monthDiff = to.getMonth() - from.getMonth() + yearDiff * 12
-
-    if (isFormatYear && monthDiff > 11) {
-      return yearDiff
-    }
-
-    const dayDiff = getDiffWithFormat(to - from, DAY)
-
-    if (format === MONTH && dayDiff > 28) {
-      return monthDiff
-    }
-  }
-
-  /* return timeDifference(from, to)[FormatToIndex[format]] */
-  return getDiffWithFormat(to - from, format)
-}
 const getFormattedDiffString = (amount, format) => {
   if (format === SECOND && amount < 60) {
     return 'a few seconds ago'
@@ -120,19 +73,6 @@ const getFormattedDiffString = (amount, format) => {
 }
 
 export const getTimeFromTo = (from, to = new Date(), format = YEAR) => {
-  let index = FormatToIndex[format]
-  let result
-
-  while (index > -1) {
-    result = _getTimeFromTo(from, to, IndexToFormat[index])
-    if (result) break
-    index--
-  }
-
-  return getFormattedDiffString(result, IndexToFormat[index])
-}
-
-export const new_getTimeFromTo = (from, to = new Date(), format = YEAR) => {
   const diff = to - from
   let resultFormat
 
@@ -153,16 +93,18 @@ export const new_getTimeFromTo = (from, to = new Date(), format = YEAR) => {
     FormatToIndex[format] < FormatToIndex[resultFormat] ? format : resultFormat
 
   let result
-  if (resultFormat === YEAR) {
-    result = to.getFullYear() - from.getFullYear()
-  } else if (resultFormat === MONTH) {
-    result =
-      to.getMonth() -
-      from.getMonth() +
-      (to.getFullYear() - from.getFullYear()) * 12
+  if (resultFormat === YEAR || resultFormat === MONTH) {
+    const yearDiff = to.getFullYear() - from.getFullYear()
+    const monthDiff = to.getMonth() - from.getMonth() + yearDiff * 12
+    if (monthDiff > 11 && format === YEAR) {
+      resultFormat = YEAR
+      result = yearDiff
+    } else {
+      result = monthDiff
+    }
   } else {
     result = getDiffWithFormat(diff, resultFormat)
   }
-  /* return timeDifference(from, to)[FormatToIndex[format]] */
+
   return getFormattedDiffString(result, resultFormat)
 }
--- src/utils/dates.spec.js
+++ src/utils/dates.spec.js
@@ -1,151 +1,157 @@
 /* eslint-env jest */
-import { new_getTimeFromTo } from './dates'
+import { getTimeFromTo } from './dates'
 
 const TO = new Date('2019-04-08T08:05:50.038Z')
 
-describe('new_getTimeFromTo', () => {
+describe('getTimeFromTo', () => {
   describe('specified format', () => {
     it('should return seconds string', () => {
       const FROM = new Date('2019-04-08T08:05:40.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 's')).toEqual('a few seconds ago')
+      expect(getTimeFromTo(FROM, TO, 's')).toEqual('a few seconds ago')
     })
 
     it('should return minute string', () => {
       const FROM = new Date('2019-04-08T08:04:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'min')).toEqual('1 minute ago')
+      expect(getTimeFromTo(FROM, TO, 'min')).toEqual('1 minute ago')
     })
 
     it('should return minutes (plural) string', () => {
       const FROM = new Date('2019-04-08T08:03:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'min')).toEqual('2 minutes ago')
+      expect(getTimeFromTo(FROM, TO, 'min')).toEqual('2 minutes ago')
     })
 
     it('should return hour string', () => {
       const FROM = new Date('2019-04-08T07:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'h')).toEqual('1 hour ago')
+      expect(getTimeFromTo(FROM, TO, 'h')).toEqual('1 hour ago')
     })
 
     it('should return hours (plural) string', () => {
       const FROM = new Date('2019-04-08T06:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'h')).toEqual('2 hours ago')
+      expect(getTimeFromTo(FROM, TO, 'h')).toEqual('2 hours ago')
     })
 
     it('should return day string', () => {
       const FROM = new Date('2019-04-07T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'd')).toEqual('1 day ago')
+      expect(getTimeFromTo(FROM, TO, 'd')).toEqual('1 day ago')
     })
 
     it('should return days (plural) string', () => {
       const FROM = new Date('2019-04-06T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'd')).toEqual('2 days ago')
+      expect(getTimeFromTo(FROM, TO, 'd')).toEqual('2 days ago')
     })
 
     it('should return month string', () => {
       const FROM = new Date('2019-03-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'm')).toEqual('1 month ago')
+      expect(getTimeFromTo(FROM, TO, 'm')).toEqual('1 month ago')
     })
 
     it('should return months (plural) string', () => {
       const FROM = new Date('2019-02-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'm')).toEqual('2 months ago')
+      expect(getTimeFromTo(FROM, TO, 'm')).toEqual('2 months ago')
     })
 
     it('should return year string', () => {
       const FROM = new Date('2018-04-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'y')).toEqual('1 year ago')
+      expect(getTimeFromTo(FROM, TO, 'y')).toEqual('1 year ago')
     })
 
     it('should return years (plural) string', () => {
       const FROM = new Date('2017-04-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'y')).toEqual('2 years ago')
+      expect(getTimeFromTo(FROM, TO, 'y')).toEqual('2 years ago')
     })
   })
 
   describe('auto format', () => {
     it('should return seconds string', () => {
       const FROM = new Date('2019-04-08T08:05:40.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('a few seconds ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('a few seconds ago')
     })
 
     it('should return minute string', () => {
       const FROM = new Date('2019-04-08T08:04:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('1 minute ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('1 minute ago')
     })
 
     it('should return minutes (plural) string', () => {
       const FROM = new Date('2019-04-08T08:03:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('2 minutes ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('2 minutes ago')
     })
 
     it('should return hour string', () => {
       const FROM = new Date('2019-04-08T07:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('1 hour ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('1 hour ago')
     })
 
     it('should return hours (plural) string', () => {
       const FROM = new Date('2019-04-08T06:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('2 hours ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('2 hours ago')
     })
 
     it('should return day string', () => {
       const FROM = new Date('2019-04-07T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('1 day ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('1 day ago')
     })
 
     it('should return days (plural) string', () => {
       const FROM = new Date('2019-04-06T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('2 days ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('2 days ago')
     })
 
     it('should return month string', () => {
       const FROM = new Date('2019-03-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('1 month ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('1 month ago')
     })
 
     it('should return months (plural) string', () => {
       const FROM = new Date('2019-02-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('2 months ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('2 months ago')
     })
 
     it('should return year string', () => {
       const FROM = new Date('2018-04-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('1 year ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('1 year ago')
     })
 
     it('should return years (plural) string', () => {
       const FROM = new Date('2017-04-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO)).toEqual('2 years ago')
+      expect(getTimeFromTo(FROM, TO)).toEqual('2 years ago')
     })
   })
 
   describe('special cases', () => {
     it('should return year difference in months', () => {
       const FROM = new Date('2018-03-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO, 'm')).toEqual('13 months ago')
+      expect(getTimeFromTo(FROM, TO, 'm')).toEqual('13 months ago')
     })
 
     it('should return days difference when month changed but 30 days not passed', () => {
       const FROM = new Date('2019-03-28T08:05:50.038Z')
       const TO_SPECIAL = new Date('2019-04-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO_SPECIAL, 'd')).toEqual('11 days ago')
+      expect(getTimeFromTo(FROM, TO_SPECIAL, 'd')).toEqual('11 days ago')
     })
 
     it('should return days difference when month changed but 30 days not passed (auto format)', () => {
       const FROM = new Date('2019-03-28T08:05:50.038Z')
       const TO_SPECIAL = new Date('2019-04-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO_SPECIAL)).toEqual('11 days ago')
+      expect(getTimeFromTo(FROM, TO_SPECIAL)).toEqual('11 days ago')
     })
 
     it('should return month difference when year changed but 12 months not passed', () => {
       const FROM = new Date('2018-12-08T08:05:50.038Z')
       const TO_SPECIAL = new Date('2019-01-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO_SPECIAL, 'm')).toEqual('1 month ago')
+      expect(getTimeFromTo(FROM, TO_SPECIAL, 'm')).toEqual('1 month ago')
     })
 
     it('should return month difference when year changed but 12 months not passed (auto format)', () => {
       const FROM = new Date('2018-12-08T08:05:50.038Z')
       const TO_SPECIAL = new Date('2019-01-08T08:05:50.038Z')
-      expect(new_getTimeFromTo(FROM, TO_SPECIAL)).toEqual('1 month ago')
+      expect(getTimeFromTo(FROM, TO_SPECIAL)).toEqual('1 month ago')
+    })
+
+    it('should return 11 month when it close to the 1 year difference', () => {
+      const FROM = new Date('2019-01-08T08:05:50.038Z')
+      const TO_SPECIAL = new Date('2019-12-29T08:05:50.038Z')
+      expect(getTimeFromTo(FROM, TO_SPECIAL)).toEqual('11 months ago')
     })
   })
 })
