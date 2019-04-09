3b483028f433d2181b7ad593778ea66d6c521b72
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-09 13:43:11 +0300

    Removed moment from table header

--- src/components/Trends/TrendsTable/TrendsTables.js
+++ src/components/Trends/TrendsTable/TrendsTables.js
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react'
-import moment from 'moment'
 import TrendsTable from './TrendsTable'
+import { dateDifferenceInWords, HOUR } from '../../../utils/dates'
 import styles from './TrendsTables.module.scss'
 
 class TrendsTables extends PureComponent {
@@ -10,20 +10,18 @@ class TrendsTables extends PureComponent {
     return (
       <div className={styles.tables}>
         {trends.length > 1 &&
-          trends
-            .slice(0, -1)
-            .map((trend, index) => (
-              <TrendsTable
-                header={`${moment(Date.now()).diff(
-                  new Date(trend.datetime),
-                  'hours'
-                )} hours ago`}
-                notSelected
-                key={index}
-                className={styles.table}
-                trend={trend}
-              />
-            ))}
+          trends.slice(0, -1).map((trend, index) => (
+            <TrendsTable
+              header={dateDifferenceInWords({
+                from: new Date(trend.datetime),
+                format: HOUR
+              })}
+              notSelected
+              key={index}
+              className={styles.table}
+              trend={trend}
+            />
+          ))}
         <TrendsTable
           className={styles.table}
           isLoading={isLoading}
