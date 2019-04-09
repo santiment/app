c3cd9797969c6508b0a2eaaf7851b7d9ab8c59a8
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-09 19:54:50 +0300

    small insight cards

---
+++ src/components/Insight/InsightCardSmall.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Panel } from '@santiment-network/ui'
+import cx from 'classnames'
+import { Link } from 'react-router-dom'
+import moment from 'moment'
+import LikeBtn from '../Like/LikeBtn'
+import { getSEOLinkFromIdAndTitle } from '../../pages/Insights/utils'
+import styles from './InsightCardSmall.module.scss'
+
+const InsightCard = ({
+  className = '',
+  id,
+  title,
+  createdAt,
+  votedAt,
+  votes: { totalVotes }
+}) => {
+  return (
+    <div className={className}>
+      <Link
+        to={`/insights/read/${getSEOLinkFromIdAndTitle(id, title)}`}
+        className={styles.title}
+      >
+        {title}
+      </Link>
+      <div className={styles.meta}>
+        <div className={styles.date}>
+          {moment(createdAt).format('MMM D, YYYY')}
+        </div>
+        <LikeBtn small grey liked={!!votedAt} likesNumber={totalVotes} />
+      </div>
+    </div>
+  )
+}
+
+InsightCard.defaultProps = {
+  votes: {},
+  comments: 0
+}
+
+export default InsightCard
--- src/components/Insight/InsightsFeatured.js
+++ src/components/Insight/InsightsFeatured.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { graphql } from 'react-apollo'
-import InsightCard from './InsightCard'
+import InsightCardSmall from './InsightCardSmall'
 import { FEATURED_INSIGHTS_QUERY } from './insightsGQL'
 
 const InsightsTrends = ({ data: { insights = [] }, ...props }) => {
   return insights.map(({ id, ...insight }) => (
-    <InsightCard
+    <InsightCardSmall
       key={id}
       {...props}
       id={id}
