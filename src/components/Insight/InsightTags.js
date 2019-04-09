6e45eeb866ae722f38421452f5d823a3fd18ad18
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-09 13:19:10 +0300

    Hiding trending tag in all places

--- src/components/Insight/InsightTags.js
+++ src/components/Insight/InsightTags.js
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { noTrendTagsFilter } from './utils'
 import styles from './InsightCard.module.scss'
 
 const InsightTags = ({ tags = [] }) => {
-  return tags.map(({ name }) => {
+  return tags.filter(noTrendTagsFilter).map(({ name }) => {
     return (
       <Link to={`/insights/tags/${name}`} key={name} className={styles.tag}>
         {name}
