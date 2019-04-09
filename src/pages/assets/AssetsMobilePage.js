738876d16a879e8616015cd05d8869f962fe2cba
Yura Zatsepin <yura.zatsepin@gmail.com>
2019-04-09 20:26:24 +0300

    Show assets on mobile

--- src/pages/assets/AssetsMobilePage.js
+++ src/pages/assets/AssetsMobilePage.js
@@ -1,23 +1,46 @@
 import React from 'react'
+import { List, AutoSizer } from 'react-virtualized'
 import Assets from './Assets'
 import AssetCard from './AssetCard'
+import styles from './AssetsMobilePage.module.scss'
 
 const AssetsMobilePage = props => {
   return (
-    <div className='page'>
+    <div className='page' className={styles.wrapper}>
       <Assets
         {...props}
         type={props.type}
-        render={Assets => (
-          <div>
-            {!Assets.isLoading &&
-              Assets.items.map(asset => (
-                <AssetCard key={asset.id} {...asset} />
-              ))}
-          </div>
-        )}
+        render={Assets => !Assets.isLoading && <AssetsList {...Assets} />}
       />
     </div>
+  )
+}
+
+const ROW_HEIGHT = 71
+
+const AssetsList = ({ items }) => {
+  const rowRenderer = ({ key, index, style, ...props }) => {
+    const asset = items[index]
+    return (
+      <div key={key} style={style}>
+        <AssetCard {...asset} />
+      </div>
+    )
+  }
+
+  return (
+    <AutoSizer>
+      {({ height, width }) => (
+        <List
+          width={width}
+          height={height}
+          rowHeight={ROW_HEIGHT}
+          rowCount={items.length}
+          overscanRowCount={5}
+          rowRenderer={rowRenderer}
+        />
+      )}
+    </AutoSizer>
   )
 }
 
