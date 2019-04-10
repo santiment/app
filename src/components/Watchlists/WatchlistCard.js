5d32e029b30dff20fb47dc2ebe712dcb27fbc21c
devanshu <devanshu.g@santiment.net>
2019-04-10 22:12:11 +0530

    Hide content if data is unavailable

--- src/components/Watchlists/WatchlistCard.js
+++ src/components/Watchlists/WatchlistCard.js
@@ -31,29 +31,34 @@ const WatchlistCard = ({ name, isPublic, stats, to, isError, isLoading }) => {
           <Icon type={isPublic ? 'eye' : 'lock-small'} fill='var(--casper)' />
         )}
       </div>
-      <div className={cx(styles.flexRow, styles.content)}>
-        {latestMarketcap ? (
-          <h3 className={styles.marketcap}>
-            $&nbsp;{millify(latestMarketcap)}
-          </h3>
-        ) : (
-          '. . .'
-        )}
-        <AreaChart width={65} height={30} data={stats}>
-          <Area
-            type='monotone'
-            dataKey='marketcap'
-            stroke={change < 0 ? 'var(--persimmon)' : 'var(--jungle-green)'}
-            dot={null}
-            fillOpacity={0.5}
-            fill={change < 0 ? '#fcf4f4' : '#f4fcf5'}
-          />
-        </AreaChart>
-      </div>
-      <div className={styles.flexRow}>
-        <PercentChanges changes={change} className={styles.change} />
-        &nbsp;&nbsp;<span className={styles.volumeLabel}> total cap, 7d </span>
-      </div>
+      {latestMarketcap ? (
+        <>
+          <div className={cx(styles.flexRow, styles.content)}>
+            {latestMarketcap ? (
+              <h3 className={styles.marketcap}>
+                $&nbsp;{millify(latestMarketcap)}
+              </h3>
+            ) : (
+              '. . .'
+            )}
+            <AreaChart width={65} height={30} data={stats}>
+              <Area
+                type='monotone'
+                dataKey='marketcap'
+                stroke={change < 0 ? 'var(--persimmon)' : 'var(--jungle-green)'}
+                dot={null}
+                fillOpacity={0.5}
+                fill={change < 0 ? '#fcf4f4' : '#f4fcf5'}
+              />
+            </AreaChart>
+          </div>
+          <div className={styles.flexRow}>
+            <PercentChanges changes={change} className={styles.change} />
+            &nbsp;&nbsp;
+            <span className={styles.volumeLabel}> total cap, 7d </span>
+          </div>
+        </>
+      ) : null}
     </Link>
   )
 }
