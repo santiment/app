42f258d5cad8bc53c299fa115aad4719191f9c44
Yura Zatsepin <yura.zatsepin@gmail.com>
2019-04-10 20:42:57 +0300

    Add predefined assets

--- src/components/Search/SearchContainer.js
+++ src/components/Search/SearchContainer.js
@@ -4,9 +4,12 @@ import { withRouter } from 'react-router-dom'
 import SearchProjects from './SearchProjects'
 import styles from './SearchContainer.module.scss'
 
+import allProjects from './../../allProjects.json'
+
 export const SearchContainer = ({ history, className }) => (
   <SearchProjects
     className={cx(styles.wrapper, className)}
+    data={{ allProjects }}
     iconPosition='left'
     onSuggestionSelect={({ coinmarketcapId }) =>
       history.push(`/projects/${coinmarketcapId}`)
