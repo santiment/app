b521fd437f61797d43b2010cbea68aefbfed8a40
devanshu <devanshu.g@santiment.net>
2019-04-11 15:09:38 +0530

    Implemented Responsive component using react-sizes

---
+++ src/components/Responsive/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import withSizes from 'react-sizes'
+import PropTypes from 'prop-types'
+import { mapSizesToProps } from '../../App'
+
+const enhance = withSizes(mapSizesToProps)
+
+export const DesktopOnly = enhance(({ children, isDesktop }) =>
+  isDesktop ? children : null
+)
+
+DesktopOnly.propTypes = {
+  children: PropTypes.element.isRequired
+}
+
+export const MobileOnly = enhance(({ children, isDesktop }) =>
+  isDesktop ? null : children
+)
+
+MobileOnly.propTypes = {
+  children: PropTypes.element.isRequired
+}
