ecbe69bec47e60bdb120c5610e18e4e77ac167c8
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-11 16:54:37 +0300

    Connecting telegram

--- src/pages/Account/SettingsConnections.js
+++ src/pages/Account/SettingsConnections.js
@@ -1,77 +1,104 @@
-import React from 'react'
+import React, { PureComponent } from 'react'
 import { connect } from 'react-redux'
 import { Tabs, Toggle, Label, Button, Selector } from '@santiment-network/ui'
 import Settings from './Settings'
 import * as actions from '../../actions/types'
+import { hasMetamask } from '../../web3Helpers'
 import styles from './AccountPage.module.scss'
 
-const SettingsConnections = ({
-  address,
-  connectNewWallet,
-  removeConnectedWallet,
-  telegramDeepLink,
-  isConnectWalletPending
-}) => (
-  <Settings id='connections' header='Connections'>
-    <Settings.Row>
-      <div className={styles.setting__left}>
-        <Label>Metamask</Label>
-        <Label className={styles.setting__description} accent='waterloo'>
-          You will get the ability to deposit tokens to your Sanbase account.
-          <br />
-          Please follow futher instructions.
-        </Label>
-      </div>
-      <Button
-        variant='fill'
-        accent='positive'
-        onClick={address ? removeConnectedWallet : connectNewWallet}
-      >
-        {address
-          ? 'Disconnect'
-          : isConnectWalletPending
-            ? 'Connecting'
-            : 'Connect'}
-      </Button>
-    </Settings.Row>
+const getMetamaskBtnText = (address, connecting) => {
+  if (!hasMetamask()) {
+    return "Can't detect Metamask"
+  }
 
-    <Settings.Row>
-      <div className={styles.setting__left}>
-        <Label>Telegram</Label>
-        <Label className={styles.setting__description} accent='waterloo'>
-          You will get the ability to connect the bot and log in through
-          Telegram.
-          <br />
-          Please do not use Telegram Web as it might not be able to link account
-          correctly.
-        </Label>
-      </div>
-      <Button
-        variant='fill'
-        accent='positive'
-        as='a'
-        href={telegramDeepLink}
-        rel='noopener noreferrer'
-        target='_blank'
-      >
-        Connect
-      </Button>
-    </Settings.Row>
-  </Settings>
-)
+  if (address) {
+    return 'Disconnect'
+  }
+
+  return connecting ? 'Connecting' : 'Connect'
+}
+
+const getTelegramBtnText = (connected, connecting) => {
+  if (connected) {
+    return 'Connected'
+  }
+
+  return connecting ? 'Connecting' : 'Connect'
+}
+
+class SettingsConnections extends PureComponent {
+  componentDidMount () {
+    this.props.generateTelegramDeepLink()
+  }
+
+  render () {
+    const {
+      address,
+      connectTelegram,
+      isTelegramConnecting,
+      connectNewWallet,
+      removeConnectedWallet,
+      telegramDeepLink,
+      isConnectWalletPending,
+      hasTelegramConnected
+    } = this.props
+
+    return (
+      <Settings id='connections' header='Connections'>
+        <Settings.Row>
+          <div className={styles.setting__left}>
+            <Label>Metamask</Label>
+            <Label className={styles.setting__description} accent='waterloo'>
+              You will get the ability to deposit tokens to your Sanbase
+              account.
+              <br />
+              Please follow futher instructions.
+            </Label>
+          </div>
+          <Button
+            variant='fill'
+            accent='positive'
+            disabled={!hasMetamask()}
+            onClick={address ? removeConnectedWallet : connectNewWallet}
+          >
+            {getMetamaskBtnText(address, isConnectWalletPending)}
+          </Button>
+        </Settings.Row>
+
+        <Settings.Row>
+          <div className={styles.setting__left}>
+            <Label>Telegram</Label>
+            <Label className={styles.setting__description} accent='waterloo'>
+              You will get the ability to connect the bot and log in through
+              Telegram.
+              <br />
+              Please do not use Telegram Web as it might not be able to link
+              account correctly.
+            </Label>
+          </div>
+          <Button
+            variant='fill'
+            accent='positive'
+            as='a'
+            href={telegramDeepLink}
+            rel='noopener noreferrer'
+            target='_blank'
+            onClick={connectTelegram}
+          >
+            {getTelegramBtnText(hasTelegramConnected, isTelegramConnecting)}
+          </Button>
+        </Settings.Row>
+      </Settings>
+    )
+  }
+}
 
 const mapStateToProps = ({
   user: {
     data: {
       email,
       ethAccounts,
-      settings: {
-        telegramDeepLink,
-        isTelegramConnecting,
-        hasTelegramConnected,
-        signalNotifyEmail,
-        signalNotifyTelegram
-      }
+      settings: { telegramDeepLink, isTelegramConnecting, hasTelegramConnected }
     }
   },
   accountUi: { isConnectWalletPending, isConnectWalletFailed }
@@ -81,8 +108,6 @@ const mapStateToProps = ({
   telegramDeepLink,
   isTelegramConnecting,
   hasTelegramConnected,
-  signalNotifyEmail,
-  signalNotifyTelegram,
   isConnectWalletPending,
   isConnectWalletFailed
 })
@@ -93,10 +118,7 @@ const mapDispatchToProps = dispatch => ({
   connectNewWallet: () =>
     dispatch({ type: actions.SETTINGS_CONNECT_NEW_WALLET }),
   generateTelegramDeepLink: () =>
-    dispatch({ type: actions.SETTINGS_GENERATE_TELEGRAM_DEEP_LINK }),
-  revokeTelegramDeepLink: () =>
-    dispatch({ type: actions.SETTINGS_REVOKE_TELEGRAM_DEEP_LINK }),
-  connectTelegram: () => dispatch({ type: actions.SETTINGS_CONNECT_TELEGRAM })
+    dispatch({ type: actions.SETTINGS_GENERATE_TELEGRAM_DEEP_LINK })
 })
 
 export default connect(
