331993fc0b5e3c50a0c46893cb1cd3d87b25abc9
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-12 19:44:28 +0300

    anon banner component

---
+++ src/pages/Dashboard/AnonBanner.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Button, Icon } from '@santiment-network/ui'
+
+const AnonBanner = () => {
+  return (
+    <div className={styles.banner}>
+      <div className={styles.banner__top}>
+        <div className={styles.banner__title}>
+          Noise control for the crypto market
+        </div>
+        <div className={styles.banner__description}>
+          Santiment provides advanced 360° overview of the crypto market and its
+          biggest driving forces
+        </div>
+        <Button variant='fill' accent='positive' as={Link} to='/login'>
+          Get started
+        </Button>
+      </div>
+      <div className={styles.banner__info}>
+        <div className={styles.advantages}>
+          <div className={styles.advantage}>
+            <div className={styles.advantage__img}>
+              <Icon type='data-big' />
+            </div>
+            <div className={styles.advantage__text}>
+              On-chain, social and development data for 1000+ crypto projects
+            </div>
+          </div>
+          <div className={styles.advantage}>
+            <div className={styles.advantage__img}>
+              <Icon type='crown-big' />
+            </div>
+            <div className={styles.advantage__text}>
+              Exclusive crypto metrics and curated, data-driven daily insights
+            </div>
+          </div>
+          <div className={styles.advantage}>
+            <div className={styles.advantage__img}>
+              <Icon type='community-big' />
+            </div>
+
+            <div className={styles.advantage__text}>
+              Growing community of crypto traders and market analysts
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default React.memo(AnonBanner)
