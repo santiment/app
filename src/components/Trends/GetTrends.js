274511d63e58d258f67861e433410e09eadb45a2
DmitriVanGuard <dimavanguard@gmail.com>
2019-04-12 17:35:15 +0300

    Cache friendly requests GetTrends

--- src/components/Trends/GetTrends.js
+++ src/components/Trends/GetTrends.js
@@ -2,7 +2,7 @@ import { graphql } from 'react-apollo'
 import { compose } from 'recompose'
 import moment from 'moment'
 import { trendsExploreGQL } from '../../components/Trends/trendsExploreGQL'
-import { getTimeFromFromString } from './../../utils/utils'
+import { getTimeIntervalFromToday, MONTH, DAY } from './../../utils/dates.js'
 
 const GetTrends = ({ render, sources = {}, ...props }) =>
   render({ sources, ...props })
@@ -32,19 +32,35 @@ const parseTrendsGQLProps = sourceType => ({
   }
 }
 
+const customTimeRange = timeRange => {
+  switch (timeRange) {
+    case '1w':
+      return getTimeIntervalFromToday(-7, DAY)
+    case '1m':
+      return getTimeIntervalFromToday(-1, MONTH)
+    case '3m':
+      return getTimeIntervalFromToday(-3, MONTH)
+    case '6m':
+      return getTimeIntervalFromToday(-6, MONTH)
+  }
+}
+
 const makeAllQueries = () =>
   ['TELEGRAM', 'PROFESSIONAL_TRADERS_CHAT', 'REDDIT', 'DISCORD'].map(source =>
     graphql(trendsExploreGQL, {
       props: parseTrendsGQLProps(source),
-      options: ({ topic, timeRange, interval }) => ({
-        variables: {
-          searchText: normalizeTopic(topic),
-          source: source,
-          interval: interval,
-          to: moment().toISOString(),
-          from: getTimeFromFromString(timeRange)
+      options: ({ topic, timeRange, interval }) => {
+        const { from, to } = customTimeRange(timeRange)
+        return {
+          variables: {
+            searchText: normalizeTopic(topic),
+            source: source,
+            interval: interval,
+            to: to.toISOString(),
+            from: from.toISOString()
+          }
         }
-      })
+      }
     })
   )
 
