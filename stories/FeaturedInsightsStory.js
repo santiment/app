886ad0cd7ba78fd049e36d9f09ee79fa408f1c70
haritonasty <a.kharitonovaspb@gmail.com>
2019-04-18 18:14:21 +0300

    finished story

--- stories/FeaturedInsightsStory.js
+++ stories/FeaturedInsightsStory.js
@@ -1,37 +1,38 @@
 import React from 'react'
 import StoryRouter from 'storybook-react-router'
 import {storiesOf} from '@storybook/react'
-import { Provider } from 'react-redux'
-import { MockedProvider } from 'react-apollo/test-utils'
+import {Provider} from 'react-redux'
+import {MockedProvider} from 'react-apollo/test-utils'
 import configureStore from 'redux-mock-store'
-import { FEATURED_INSIGHTS_QUERY } from './../src/queries/InsightsGQL'
+import {FEATURED_INSIGHTS_QUERY} from './../src/queries/InsightsGQL'
 import FeaturedInsightsHorizontal from "../src/components/FeaturedInsights/FeaturedInsightsHorizontal";
 import FeaturedInsightsWithTitle from "../src/components/FeaturedInsights/FeaturedInsightsWithTitle";
 import FeaturedInsightsScrollable from "../src/components/FeaturedInsights/FeaturedInsightsScrollable";
+
+const title = 'How Stable Are Stablecoins? ';
+const usernames = ['N', 'Nastya', 'Nastya Kharitonova', 'LooooooooooongNameAndSurname'];
+
 const mockedData = {
-  insights: [0, 1].map(index => ({
+  insights: [0, 1, 2, 3].map(index => ({
     "__typename": "Post",
     readyState: "published",
     id: index,
-    title: 'ANy',
+    title: title.repeat(index+1),
     createdAt: '2018-12-01T13:37:02.07080',
     publishedAt: '2018-12-01T13:37:02.07080',
     updatedAt: '2018-12-01T13:37:02.07080',
     tags: [],
     votedAt: null,
-    votes:{"__typename":"Vote","totalVotes":0},
-    user:{"__typename":"PostAuthor","id":"2","username":"Iurii"}
+    votes: {"__typename": "Vote", "totalVotes": index},
+    user: {"__typename": "PostAuthor", "id": index, "username": usernames[index]}
   }))
 }
 
 const query = FEATURED_INSIGHTS_QUERY
-
-const mocks = [{ request: { query }, result: { data: mockedData } }]
-
-const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount))
+const mocks = [{request: {query}, result: {data: mockedData}}]
 
 const mockStore = configureStore([])
-const initialState = { user: { token: 'any' } }
+const initialState = {user: {token: 'any'}}
 let store = mockStore(initialState)
 
 storiesOf('FeaturedInsights', module)
@@ -40,21 +41,26 @@ storiesOf('FeaturedInsights', module)
     <MockedProvider mocks={mocks}>{story()}</MockedProvider>
   ))
   .addDecorator(story => <Provider store={store}>{story()}</Provider>)
+  .add('Scrollable', () => (
+    <>
+      <h2>Component for flex container with scrollable Panel</h2>
+      <div style={{width: '300px', height: '350px', display: 'flex'}}>
+        <FeaturedInsightsScrollable maxLines={2} multilineTextId="testScrollable" />
+      </div>
+    </>
+  ))
+  .add('WithTitle', () => (
+    <>
+      <h2>Component for desktop and without scrollable Panel</h2>
+      <div style={{width: '300px'}}>
+        <FeaturedInsightsWithTitle maxLines={2} multilineTextId="testWithTitle" />
+      </div>
+    </>
+  ))
   .add('Horizontal', () => (
     <>
       <h2>Component for mobile and tablet</h2>
-      <FeaturedInsightsHorizontal maxLines={3} multilineTextId="testHorizontal" />
+      <p>Choose mobile/tablet device below</p>
+        <FeaturedInsightsHorizontal maxLines={3} multilineTextId="testHorizontal" />
     </>
   ))
-  //.add('Scrollable', () => (
-    //<>
-      //<h2>Component for flex container with scrollable Panel</h2>
-      //<FeaturedInsightsScrollable client={() => {}} maxLines={2} multilineTextId="testScrollable" />
-    //</>
-  //))
-  //.add('WithTitle', () => (
-    //<>
-      //<h2>Component for desktop and without scrollable</h2>
-      //<FeaturedInsightsWithTitle client={() => {}} maxLines={2} multilineTextId="testWithTitle" />
-    //</>
-  //))
