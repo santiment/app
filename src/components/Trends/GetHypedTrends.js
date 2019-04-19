51257af42dda541b24af35fbcdaaf55a1f2aa3a3
haritonasty <a.kharitonovaspb@gmail.com>
2019-04-19 13:08:57 +0300

    added sortBy hypeScore

--- src/components/Trends/GetHypedTrends.js
+++ src/components/Trends/GetHypedTrends.js
@@ -2,17 +2,37 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { compose, pure } from 'recompose'
 import * as actions from './actions.js'
+import { simpleSort } from '../../utils/sortMethods'
+
+export const SORT_TYPES = {
+  hypeScore: 'score'
+}
 
 class GetHypedTrends extends React.Component {
   render () {
-    const { render, ...rest } = this.props
-    return render(rest)
+    const { render, items, ...rest } = this.props
+    const trends = items.slice(-1)
+    const props = {
+      ...rest,
+      items: trends.length > 0 ? [sortByHype(trends)] : trends
+    }
+    return render(props)
   }
 
   componentDidMount () {
     this.props.fetchHypedTrends()
   }
 }
+
+const sortByHype = items => {
+  const sortedData = items[0].topWords.sort(sort(SORT_TYPES.hypeScore))
+  return {
+    ...items,
+    topWords: sortedData
+  }
+}
+
+const sort = sortBy => (a, b) => simpleSort(a[sortBy], b[sortBy])
 
 const mapStateToProps = state => {
   return {
