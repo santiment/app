b3a1968a4f974c43997bb1211bfa6c075780361b
DmitriVanGuard <dimavanguard@gmail.com>
2019-05-01 09:59:59 +0300

    Updated structure

--- src/components/AnonBanner/AnonBannerExperiment.js
+++ src/components/AnonBanner/AnonBannerExperiment.js
@@ -2,11 +2,6 @@ import React, { PureComponent } from 'react'
 import Loadable from 'react-loadable'
 import Experiment from 'react-ab-test/lib/Experiment'
 import Variant from 'react-ab-test/lib/Variant'
-import emitter from 'react-ab-test/lib/emitter'
-import mixpanelHelper from 'react-ab-test/lib/helpers/mixpanel'
-
-// window.mixpanel has been set by Mixpanel's embed snippet.
-mixpanelHelper.enable()
 
 const AnonBannerA = Loadable({
   loader: () => import('./AnonBannerA'),
@@ -38,22 +33,5 @@ class AnonBannerExperiment extends PureComponent {
     )
   }
 }
-
-// Called when the experiment is displayed to the user.
-emitter.addPlayListener(function (Experiment, Variant) {
-  console.log(`Displaying experiment "${Experiment}" variant "${Variant}"`)
-  window.mixpanel.track('Experiment Play', {
-    Experiment,
-    Variant
-  })
-})
-
-// Called when a 'win' is emitted, in this case by this.refs.experiment.win()
-emitter.addWinListener(function (Experiment, Variant) {
-  console.log(
-    `Variant "${Variant}" of the experiment "${Experiment}" was clicked`
-  )
-  window.mixpanel.track('Experiment Win', { Experiment, Variant })
-})
 
 export default AnonBannerExperiment
