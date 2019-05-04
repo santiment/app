44a7ef4f46abf28aaeeee9d64e92033e9ceca11b
haritonasty <a.kharitonovaspb@gmail.com>
2019-05-04 22:51:16 +0300

    rewrote insights

--- src/components/Trends/Explore/TrendsExploreAdditionalInfo.js
+++ src/components/Trends/Explore/TrendsExploreAdditionalInfo.js
@@ -2,37 +2,25 @@ import React, { useState } from 'react'
 import { compose } from 'recompose'
 import { graphql } from 'react-apollo'
 import { Tabs } from '@santiment-network/ui'
-import { NEWS_QUERY } from '../../News/NewsGQL'
+import { DAY, getTimeIntervalFromToday } from '../../../utils/dates'
 import { ALL_INSIGHTS_BY_TAG_QUERY } from '../../../queries/InsightsGQL'
-import InsightCard from '../../Insight/InsightCardWithMarketcap'
-import News from '../../../components/News/News'
-import styles from './TrendsExploreAdditionalInfo.module.scss'
+import { NEWS_QUERY } from '../../News/NewsGQL'
 import {
   getInsightTrendTagByDate,
   oneDayTimestamp
 } from '../../Insight/InsightsTrends'
-import { DAY, getTimeIntervalFromToday } from '../../../utils/dates'
+import InsightCard from '../../Insight/InsightCard'
+import News from '../../News/News'
+import styles from './TrendsExploreAdditionalInfo.module.scss'
 
 const NEWS_INDEX = 'News'
 const INSIGHTS_INDEX = 'Insights'
 
-const TrendsExploreAdditionalInfo = ({
-  news,
-  isLoadingNews,
-  isErrorNews,
-  allInsightsByTag: insights,
-  isLoadingInsights
-}) => {
-  let [insightsLength, setInsightsLength] = useState(insights.length)
-  let [selectedTab, setSelectedTab] = useState(NEWS_INDEX)
-
-  if (insights.length !== insightsLength && insights.length > 0) {
-    setInsightsLength(insights.length)
-    setSelectedTab(INSIGHTS_INDEX)
-  }
+const TrendsExploreAdditionalInfo = ({ news, allInsightsByTag: insights }) => {
+  let [selectedTab, setSelectedTab] = useState(null)
 
   function handleSelectTab (tab) {
-    if (tab !== selectedTab) {
+    if (tab !== (selectedTab || defaultSelectedTab)) {
       setSelectedTab(tab)
     }
   }
@@ -47,23 +35,27 @@ const TrendsExploreAdditionalInfo = ({
     content: `${INSIGHTS_INDEX} (${insights.length})`
   }
 
-  const isNotVisible = isErrorNews || isLoadingNews || isLoadingInsights
-
   const tabs = []
   if (insights.length > 0) tabs.push(insightsTab)
   if (news.length > 0) tabs.push(newsTab)
 
-  if (tabs.length === 0 || isNotVisible) return null
+  let defaultSelectedTab
+
+  if (tabs.length === 0) return null
+  else defaultSelectedTab = tabs[0].index
+
   return (
     <section className={styles.wrapper}>
       <Tabs
         options={tabs}
-        defaultSelectedIndex={selectedTab}
+        defaultSelectedIndex={defaultSelectedTab}
         onSelect={handleSelectTab}
       />
       <div className={styles.tabsContent}>
-        {news.length > 0 && selectedTab === NEWS_INDEX && <News data={news} />}
-        {insights.length > 0 && selectedTab === INSIGHTS_INDEX && (
+        {(selectedTab || defaultSelectedTab) === NEWS_INDEX && (
+          <News data={news} />
+        )}
+        {(selectedTab || defaultSelectedTab) === INSIGHTS_INDEX && (
           <div className={styles.insights}>
             {insights.map(insight => (
               <InsightCard
@@ -109,12 +101,11 @@ const getPast3DaysInsightsByTrendTag = () => {
       },
       props: ({
         data: { allInsightsByTag = [] },
-        ownProps: { allInsightsByTag: ownInsights = [], loading }
+        ownProps: { allInsightsByTag: ownInsights = [] }
       }) => ({
         allInsightsByTag: allInsightsByTag.concat(
           filteredInsightsByWord(ownInsights, filterInsightsByWord)
-        ),
-        isLoading: loading
+        )
       })
     })
   )
