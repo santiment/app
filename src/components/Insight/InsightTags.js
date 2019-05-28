09df1a7710028d97d3727c5f06e976be9a07c6f7
haritonasty <a.kharitonovaspb@gmail.com>
2019-05-28 08:21:25 +0300

    Hide tags in insight when there are a lot of them

--- src/components/Insight/InsightTags.js
+++ src/components/Insight/InsightTags.js
@@ -1,16 +1,49 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import cx from 'classnames'
+import { ContextMenu, Panel, Button } from '@santiment-network/ui'
 import { noTrendTagsFilter } from './utils'
-import styles from './InsightCard.module.scss'
+import styles from './InsightTags.module.scss'
+
+const VISIBLE_TAGS_BY_DEFAULT = 3
 
 const InsightTags = ({ tags = [] }) => {
-  return tags.filter(noTrendTagsFilter).map(({ name }) => {
-    return (
-      <Link to={`/insights/tags/${name}`} key={name} className={styles.tag}>
-        {name}
-      </Link>
-    )
-  })
+  const filteredTags = tags.filter(noTrendTagsFilter)
+  const tagsOverflow =
+    filteredTags.length > VISIBLE_TAGS_BY_DEFAULT
+      ? filteredTags.length - VISIBLE_TAGS_BY_DEFAULT
+      : 0
+  return (
+    <>
+      {filteredTags.slice(0, VISIBLE_TAGS_BY_DEFAULT).map(({ name }) => (
+        <Link to={`/insights/tags/${name}`} key={name} className={styles.tag}>
+          {name}
+        </Link>
+      ))}
+      {tagsOverflow > 0 && (
+        <ContextMenu
+          trigger={
+            <Button className={cx(styles.tag, styles.moreTagsTrigger)}>
+              +{tagsOverflow}
+            </Button>
+          }
+          position='bottom'
+        >
+          <Panel className={styles.overflowTags}>
+            {filteredTags.slice(VISIBLE_TAGS_BY_DEFAULT).map(({ name }) => (
+              <Link
+                to={`/insights/tags/${name}`}
+                key={name}
+                className={styles.tag}
+              >
+                {name}
+              </Link>
+            ))}
+          </Panel>
+        </ContextMenu>
+      )}
+    </>
+  )
 }
 
 export default InsightTags
