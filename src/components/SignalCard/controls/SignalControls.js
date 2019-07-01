009e861479a879d840eb9fc2c1494b66ac0ee6a3
garageinc <9-b-rinat@rambler.ru>
2019-07-01 16:36:27 +0300

    5100: updated signal icon colors by changed figma

--- src/components/SignalCard/controls/SignalControls.js
+++ src/components/SignalCard/controls/SignalControls.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Button, Icon } from '@santiment-network/ui'
-import styles from './SignalControls.module.scss'
 import { Link } from 'react-router-dom'
 import {
   DAILY_ACTIVE_ADDRESSES,
@@ -10,15 +9,16 @@ import {
   PRICE_VOLUME_DIFFERENCE
 } from '../../../ducks/Signals/utils/constants'
 import DeleteDialog from '../../DeleteDialog/DeleteDialog'
+import styles from './SignalControls.module.scss'
 
 const getIconStyles = type => {
   switch (type) {
-    case ETH_WALLET:
     case PRICE_VOLUME_DIFFERENCE:
     case PRICE_ABSOLUTE_CHANGE:
     case PRICE_PERCENT_CHANGE: {
       return ['finance', styles.iconFinance]
     }
+    case ETH_WALLET:
     case DAILY_ACTIVE_ADDRESSES: {
       return ['connection', styles.iconConnection]
     }
