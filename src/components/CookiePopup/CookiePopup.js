125ad4dc6482bcd92fad76c8a45f8e4d8698176d
DmitriVanGuard <dimavanguard@gmail.com>
2019-07-02 19:29:37 +0300

    State for showing banner. Saving to localstorage. Term and conditions link

--- src/components/CookiePopup/CookiePopup.js
+++ src/components/CookiePopup/CookiePopup.js
@@ -1,25 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import Panel from '@santiment-network/ui/Panel/Panel'
 import Button from '@santiment-network/ui/Button'
 import styles from './CookiePopup.module.scss'
 
+const COOKIE_POLICY_ACCEPTED = 'COOKIE_POLICY_ACCEPTED'
+
+const acceptCookiePolicy = () =>
+  localStorage.setItem(COOKIE_POLICY_ACCEPTED, true)
+
 const CookiePopup = () => {
+  const [shown, setShown] = useState(
+    !localStorage.getItem(COOKIE_POLICY_ACCEPTED)
+  )
+
+  function accept () {
+    acceptCookiePolicy()
+    setShown(false)
+  }
+
   return (
-    <Panel className={styles.wrapper} variant='modal'>
-      <h2 className={styles.title}>We are using cookies</h2>
-      <div className={styles.bottom}>
-        <p className={styles.text}>
-          This website uses the following types of cookies; strictly necessary,
-          functional, performance
-          <br />
-          and marketing cookies. By using this website, you accept our Terms &
-          Conditions.
-        </p>
-        <Button variant='fill' accent='positive' className={styles.btn}>
-          Accept
-        </Button>
-      </div>
-    </Panel>
+    shown && (
+      <Panel className={styles.wrapper} variant='modal'>
+        <h2 className={styles.title}>We are using cookies</h2>
+        <div className={styles.bottom}>
+          <p className={styles.text}>
+            This website uses the following types of cookies; strictly
+            necessary, functional, performance
+            <br />
+            and marketing cookies. By using this website, you accept our{' '}
+            <a
+              href='https://santiment.net/terms-conditions/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className={styles.link}
+            >
+              Terms & Conditions
+            </a>
+            .
+          </p>
+          <Button
+            variant='fill'
+            accent='positive'
+            className={styles.btn}
+            onClick={accept}
+          >
+            Accept
+          </Button>
+        </div>
+      </Panel>
+    )
   )
 }
 
