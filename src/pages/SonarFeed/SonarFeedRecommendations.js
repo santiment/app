bdaa46760d55b6b5b9169e32f11d7a62c824b155
garageinc <9-b-rinat@rambler.ru>
2019-07-17 15:05:39 +0300

    updated sonar feed activities description

--- src/pages/SonarFeed/SonarFeedRecommendations.js
+++ src/pages/SonarFeed/SonarFeedRecommendations.js
@@ -6,13 +6,14 @@ import Image from './sonar_activity_artboard.png'
 import SignalMasterModalForm from '../../ducks/Signals/signalModal/SignalMasterModalForm'
 import styles from './SonarFeedRecommendations.module.scss'
 
-const SonarFeedRecommendations = ({ showButton }) => (
+const SonarFeedRecommendations = ({
+  showButton,
+  description = 'Start to add signals you want to track or just interested in'
+}) => (
   <div className={styles.wrapper}>
     <div className={styles.container}>
       <img className={styles.pic} alt='Artboard' src={Image} />
-      <div className={styles.description}>
-        Start to add signals you want to track or just interested in
-      </div>
+      <div className={styles.description}>{description}</div>
       {showButton && <SignalMasterModalForm label='Add your first signal' />}
     </div>
 
