19972a244eca92ce9fd2ffcb90cbe7ac439c3038
garageinc <9-b-rinat@rambler.ru>
2019-07-23 14:27:34 +0300

    5536: fixed select box height in trigger form

--- src/components/formik-santiment-ui/FormikSelect.js
+++ src/components/formik-santiment-ui/FormikSelect.js
@@ -19,7 +19,7 @@ const FormikSelect = ({
       name={name}
       render={({ field, form }) => {
         return (
-          <Fragment>
+          <>
             <div
               className={
                 multi ? 'select__container-multi' : 'select__container-single'
@@ -43,7 +43,7 @@ const FormikSelect = ({
                 <div className='error error-message'>{form.errors[name]}</div>
               )}
             </div>
-          </Fragment>
+          </>
         )
       }}
     />
