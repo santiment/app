677ba0a4dfd7553856398e015eb57d3d8f37c049
DmitriVanGuard <dimavanguard@gmail.com>
2019-07-31 19:05:43 +0300

    Showing tooltip after delay

--- src/ducks/SANCharts/SidecarExplanationTooltip.js
+++ src/ducks/SANCharts/SidecarExplanationTooltip.js
@@ -1,14 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ExplanationTooltip from '../../components/ExplanationTooltip/ExplanationTooltip'
 import styles from './SidecarExplanationTooltip.module.scss'
 
+const LS_SIDECAR_TOOLTIP_SHOWN = 'LS_SIDECAR_TOOLTIP_SHOWN'
+const TOOLTIP_DELAY_IN_MS = 5000
+
 const SidecarExplanationTooltip = props => {
-  const [shown, setShown] = useState(true)
+  const wasShown = localStorage.getItem(LS_SIDECAR_TOOLTIP_SHOWN)
+
+  const [shown, setShown] = useState()
+  const [timer, setTimer] = useState()
 
   function hideTooltip () {
-    setShown(false)
+    localStorage.setItem(LS_SIDECAR_TOOLTIP_SHOWN, '+')
+    setShown(false) // HACK(vanguard): To immediatly hide tooltip and then back to not controlled state
     setTimeout(() => setShown(undefined))
   }
+
+  function disableHelp () {
+    localStorage.setItem(LS_SIDECAR_TOOLTIP_SHOWN, '+')
+    clearTimeout(timer)
+  }
+
+  useEffect(() => {
+    if (!wasShown) {
+      setTimer(setTimeout(() => setShown(true), TOOLTIP_DELAY_IN_MS))
+    }
+
+    return () => clearTimeout(timer)
+  }, [])
 
   return (
     <ExplanationTooltip
@@ -18,6 +38,7 @@ const SidecarExplanationTooltip = props => {
       position='left'
       align='start'
       as='div'
+      onOpen={shown ? undefined : disableHelp}
       text={
         <>
           Explore assets
