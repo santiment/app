7b2dded948b50278d8d51063fa2368b0d8b1de8e
haritonasty <a.kharitonovaspb@gmail.com>
2019-08-05 21:16:40 +0300

    delete legacy loader

--- src/components/Loader/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import cx from 'classnames'
-import styles from './Loader.module.scss'
-
-const Loader = ({ className }) => {
-  return <div className={cx(styles.loader, className)}>Loading...</div>
-}
-
-export default Loader
