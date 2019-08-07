2a73fbf4691ba312ac5252a58f21d2532fe69013
haritonasty <a.kharitonovaspb@gmail.com>
2019-08-07 18:16:42 +0300

    correct id

--- src/utils/plans.js
+++ src/utils/plans.js
@@ -7,7 +7,7 @@ export const formatPrice = (price, name, billing) => {
   return [`$${parseInt(price / devider, 10)}`, '/mo']
 }
 
-const sanbaseProductId = '3'
+const sanbaseProductId = '2'
 export const findSanbasePlan = ({ id }) => id === sanbaseProductId
 
 export const getCurrentSanbaseSubscription = user => {
