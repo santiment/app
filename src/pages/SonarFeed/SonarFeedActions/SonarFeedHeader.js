a87b9fc6a316868e668a5df1cf7acc577730e5af
garageinc <9-b-rinat@rambler.ru>
2019-08-07 14:43:51 +0300

    fix: sonar explanation tooltip

--- src/pages/SonarFeed/SonarFeedActions/SonarFeedHeader.js
+++ src/pages/SonarFeed/SonarFeedActions/SonarFeedHeader.js
@@ -7,7 +7,9 @@ const SonarFeedHeader = () => {
     <div className={styles.header}>
       <h1>Sonar</h1>
       <div className={styles.explanation}>
-        <HelpPopup>Create your own signal or subscribe to existing</HelpPopup>
+        <HelpPopup position='bottom left'>
+          Create your own signal or subscribe to existing
+        </HelpPopup>
       </div>
     </div>
   )
