e77ca749907ba6f7a9b4fcb721089915076e6c2c
DmitriVanGuard <dimavanguard@gmail.com>
2019-09-01 22:01:26 +0300

    better dialog

--- src/components/Tutorials/TutorialsDialog.js
+++ src/components/Tutorials/TutorialsDialog.js
@@ -8,39 +8,48 @@ const TutorialsDialog = () => {
   return (
     <Dialog title='🎓  Santiment Academy' open={true} classes={styles}>
       <div className={styles.content}>
-        <div className={styles.tutorials}>
-          <TutorialCard
-            className={cx(styles.card, styles.card_active)}
-            title='How to use Price metric?'
-            duration='0:53'
-          />
-          <TutorialCard
-            className={styles.card}
-            title='How to use Price metric?'
-            duration='0:53'
-          />
-          <TutorialCard
-            className={styles.card}
-            title='How to use Price metric?'
-            duration='0:53'
-          />
-        </div>
-        <div className={styles.tutorial}>
-          <div className={styles.video}>
-            <iframe
-              className={styles.video__iframe}
-              src='https://www.youtube-nocookie.com/embed/DQPrfUbcNrA'
-              frameborder='0'
-              allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
-              allowfullscreen
+        <div className={cx(styles.block, styles.tutorials)}>
+          <div className={styles.block__scroll}>
+            <TutorialCard
+              className={cx(styles.card, styles.card_active)}
+              title='How to use Price metric?'
+              duration='0:53'
+            />
+            <TutorialCard
+              className={styles.card}
+              title='How to use Price metric?'
+              duration='0:53'
+            />
+            <TutorialCard
+              className={styles.card}
+              title='How to use Price metric?'
+              duration='0:53'
             />
           </div>
+        </div>
 
-          <h2 className={styles.tutorial__title}>How to use MVRV Ratio?</h2>
-          <p className={styles.tutorial__text}>
-            This metric shows the whole market value divided by the realized
-            value of the network, available to ETH, all ERC-20s and BTC
-          </p>
+        <div className={cx(styles.block, styles.block_tutorial)}>
+          <div
+            className={cx(styles.block__scroll, styles.block__scroll_tutorial)}
+          >
+            <div className={styles.tutorial}>
+              <div className={styles.video}>
+                <iframe
+                  className={styles.video__iframe}
+                  src='https://www.youtube-nocookie.com/embed/DQPrfUbcNrA'
+                  frameborder='0'
+                  allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+                  allowfullscreen
+                />
+              </div>
+
+              <h2 className={styles.tutorial__title}>How to use MVRV Ratio?</h2>
+              <p className={styles.tutorial__text}>
+                This metric shows the whole market value divided by the realized
+                value of the network, available to ETH, all ERC-20s and BTC
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </Dialog>
