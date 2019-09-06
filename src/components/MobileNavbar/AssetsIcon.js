418c8e4efa40ccb8a631ae15be3e7c457de8a83e
haritonasty <a.kharitonovaspb@gmail.com>
2019-09-06 19:42:28 +0300

    for mobile navbar menu icons

--- src/components/MobileNavbar/AssetsIcon.js
+++ src/components/MobileNavbar/AssetsIcon.js
@@ -11,13 +11,13 @@ export default props => (
   >
     <path
       d='M13.9929 6.33887C13.82 10.4876 10.4876 13.82 6.33887 13.9929C7.16055 16.327 9.38487 17.9999 11.9999 17.9999C15.3137 17.9999 17.9999 15.3137 17.9999 11.9999C17.9999 9.38487 16.327 7.16055 13.9929 6.33887Z'
-      fill='var(--menu-active-shade, #E7EAF3)'
+      fill='var(--menu-active-shade, var(--porcelain))'
     />
     <path
       fillRule='evenodd'
       clipRule='evenodd'
       d='M12 6C12 9.31371 9.31371 12 6 12C2.68629 12 0 9.31371 0 6C0 2.68629 2.68629 0 6 0C9.31371 0 12 2.68629 12 6ZM6 10C8.20914 10 10 8.20914 10 6C10 3.79086 8.20914 2 6 2C3.79086 2 2 3.79086 2 6C2 8.20914 3.79086 10 6 10Z'
-      fill='var(--menu-active-accent,#181B2B)'
+      fill='var(--menu-active-accent, var(--mirage))'
     />
   </svg>
 )
