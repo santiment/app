2ceacadbae80d2e02aeb374f48ae7c7e768b821e
DmitriVanGuard <dimavanguard@gmail.com>
2019-09-10 19:50:08 +0300

    Logout icon

--- src/components/Navbar/NavbarProfileDropdown.js
+++ src/components/Navbar/NavbarProfileDropdown.js
@@ -12,15 +12,31 @@ import styles from './NavbarProfileDropdown.module.scss'
 import dropdownStyles from './NavbarDropdown.module.scss'
 
 const mys = [
-  { link: '/sonar/my-signals', label: 'My signals' },
-  { link: '/assets', label: 'My watchlists' },
-  { link: '/insights/my', label: 'My insights' }
+  { to: '/sonar/my-signals', children: 'My signals' },
+  { to: '/assets', children: 'My watchlists' },
+  { to: '/insights/my', children: 'My insights' }
 ]
 
 const links = [
-  { link: '/account', label: 'Account settings' },
+  { to: '/account', children: 'Account settings' },
   // { link: '/upgrade', label: 'Upgrade plan' },
-  { link: '/logout', label: 'Log out' }
+  {
+    to: '/logout',
+    className: styles.logout,
+    children: (
+      <>
+        <svg
+          className={styles.logout__icon}
+          width='16'
+          height='16'
+          xmlns='http://www.w3.org/2000/svg'
+        >
+          <path d='M8 16a8 8 0 0 0 7.11-4.36.67.67 0 1 0-1.18-.61A6.65 6.65 0 0 1 1.33 8a6.65 6.65 0 0 1 12.6-3.03.67.67 0 0 0 1.11.15.67.67 0 0 0 .07-.76A8 8 0 1 0 8 16zm.67-4.66a.67.67 0 0 0 .46-1.14L7.6 8.67h7.72a.67.67 0 1 0 0-1.34H7.6L9.13 5.8a.67.67 0 1 0-.94-.94l-2.6 2.61a.67.67 0 0 0 0 1.06l2.6 2.6a.67.67 0 0 0 .48.21z' />
+        </svg>{' '}
+        Log out
+      </>
+    )
+  }
 ]
 
 export const NavbarProfileDropdown = ({
@@ -72,19 +88,17 @@ export const NavbarProfileDropdown = ({
       {isLoggedIn && (
         <>
           <div className={dropdownStyles.list}>
-            {mys.map(({ link, label }) => {
+            {mys.map((props, index) => {
               return (
                 <Button
                   variant='ghost'
-                  key={label}
+                  key={index}
                   fluid
                   as={Link}
                   className={dropdownStyles.item}
-                  to={link}
-                  isActive={link === activeLink}
-                >
-                  {label}
-                </Button>
+                  isActive={props.to === activeLink}
+                  {...props}
+                />
               )
             })}
           </div>
@@ -93,19 +107,17 @@ export const NavbarProfileDropdown = ({
       )}
       <div className={dropdownStyles.list}>
         {isLoggedIn ? (
-          links.map(({ link, label }) => {
+          links.map((props, index) => {
             return (
               <Button
                 variant='ghost'
-                key={label}
+                key={index}
                 fluid
                 as={Link}
                 className={dropdownStyles.item}
-                to={link}
-                isActive={link === activeLink}
-              >
-                {label}
-              </Button>
+                isActive={props.to === activeLink}
+                {...props}
+              />
             )
           })
         ) : (
