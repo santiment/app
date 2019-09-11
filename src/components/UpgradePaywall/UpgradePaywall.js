898de50d860a78b6c0824a1bde70815381a9be7f
haritonasty <a.kharitonovaspb@gmail.com>
2019-09-11 13:37:56 +0300

    added list

--- src/components/UpgradePaywall/UpgradePaywall.js
+++ src/components/UpgradePaywall/UpgradePaywall.js
@@ -12,11 +12,31 @@ const UpgradePaywall = () => (
         Upgrade
       </UpgradeBtn>
     </div>
+    <ul className={styles.list}>
+      <UpgradeItem
+        iconType='finance'
+        text='Unlimited metrics'
+        className={styles.item}
+      />
+      <UpgradeItem
+        iconType='connection'
+        text='All types of signals'
+        className={styles.item}
+      />
+      <UpgradeItem
+        iconType='words-list'
+        text='Handcrafted reports'
+        className={styles.item}
+      />
+    </ul>
   </div>
 )
 
-const UpgradeItem = ({ iconType, text }) => (
-  <li className={styles.item}>
+const UpgradeItem = ({ iconType, text, className }) => (
+  <li className={className}>
+    <div className={styles.iconWrapper}>
+      <Icon type={iconType} className={styles.icon} />
+    </div>
     <span>{text}</span>
   </li>
 )
