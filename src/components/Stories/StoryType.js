4062f269b411f210c430167171f4691886b78dec
haritonasty <a.kharitonovaspb@gmail.com>
2019-09-25 17:34:34 +0300

    upd sun-ui version

--- src/components/Stories/StoryType.js
+++ src/components/Stories/StoryType.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Label from '@santiment-network/ui/Label'
-import { TYPES, ICONS } from './utils'
+import Icon from '@santiment-network/ui/Icon'
+import { TYPES } from './utils'
 import styles from './StoryType.module.scss'
 
 const StoryType = ({ type, className, slides, minutes }) => {
   return (
     <div className={className}>
       <Label variant='round' accent={TYPES[type].color} className={styles.icon}>
-        {/* <Icon className={styles.icon} type={ICONS[type]} /> */}
-        {ICONS[TYPES[type].icon]}
+        <Icon type={TYPES[type].icon} />
       </Label>
       <Label accent={TYPES[type].color} className={styles.info}>
         {TYPES[type].previewInfo({ slides, minutes })}
--- src/components/Stories/content.js
+++ src/components/Stories/content.js
@@ -1,12 +1,3 @@
-import img1 from './images/1slide.png'
-import img2 from './images/2slide.png'
-import img3 from './images/3slide.png'
-import img4 from './images/4slide.png'
-import img5 from './images/5slide.png'
-import img6 from './images/6slide.png'
-import img7 from './images/7slide.png'
-import img8 from './images/8slide.png'
-
 export const stories = [
   {
     type: 'VIDEO',
@@ -15,7 +6,7 @@ export const stories = [
     minutes: 5,
     slides: [
       {
-        title: 'Sonar Introduction',
+        title: 'Sonar Introduction: Tracking the Social Trends',
         description:
           'Setup push or Telegram notifications to trigger any time an asset starts trending on social media via our Emerging Trends Tool to potentially spot local tops!',
         videoId: 'AsZRjm9x5HI'
@@ -49,84 +40,6 @@ export const stories = [
         description:
           "Populate a private or public watchlist with assets you'd like to track to see their price/volume action, development activity, on-chain addresses, and several other features!",
         videoId: 'yzo1gxoTFsk'
-      }
-    ]
-  },
-  {
-    type: 'SLIDES',
-    previewTitle: 'Are we in the ETH accumulation stage? 🕵️',
-    storyHeaderName: 'Are we in the ETH accumulation stage? 🕵️',
-    slides: [
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img1,
-        isDarkImage: true,
-        description:
-          '1. Token Circulation (# of unique ETH being used each day) continues to decline over the past 5 months.',
-        buttonText: 'See chart',
-        buttonLink: 'https://tinyurl.com/y54xlm89'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img2,
-        isDarkImage: true,
-        description:
-          "2. Ethereum's maximal mean age (average age that each coin has remained in its current address) continues to rise.",
-        buttonText: 'See chart',
-        buttonLink: 'http://santiment.net/sangraphs'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img3,
-        isDarkImage: true,
-        description:
-          '3. # of active coins over a period of 90 days has begun to decline as of recently.',
-        buttonText: 'See chart',
-        buttonLink: 'https://t.co/rGvvCISdBM?amp=1'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img4,
-        isDarkImage: true,
-        description:
-          '4. Percent/share of ETH on exchanges has been declining ever since february.',
-        buttonText: 'See chart',
-        buttonLink: 'http://santiment.net/sangraphs'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img5,
-        isDarkImage: true,
-        description:
-          '5. Total # of addresses holding 10k+ ETH has been on the rise, adding 1.44 million ETH since early January',
-        buttonText: 'See chart',
-        buttonLink: 'http://santiment.net/sangraphs'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img6,
-        isDarkImage: true,
-        description:
-          '6. Amount of retail holders (holding btw 1-10 ETH) has also been growing, adding 75K ETH since start of year.',
-        buttonText: 'See chart',
-        buttonLink: 'http://santiment.net/sangraphs'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img7,
-        isDarkImage: true,
-        description: '7. The ETH Miners balance continues to amass',
-        buttonText: 'See chart',
-        buttonLink: 'https://t.co/rGvvCISdBM?amp=1'
-      },
-      {
-        title: 'Here are 6 metrics that suggest we may be:',
-        image: img8,
-        isDarkImage: true,
-        description:
-          '8. After a steady decline, the number of daily withdrawals of ETH from exchanges is now back in an uptrend',
-        buttonText: 'See chart',
-        buttonLink: 'http://santiment.net/sangraphs'
       }
     ]
   }
