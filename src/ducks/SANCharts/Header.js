8eb6f5c635d1d1af4d030eca1ba4f67d80c632b7
rfihtengolts <9-b-rinat@rambler.ru>
2019-10-03 14:46:11 +0300

    skip loading slug if not selected

--- src/ducks/SANCharts/Header.js
+++ src/ducks/SANCharts/Header.js
@@ -205,6 +205,9 @@ const Header = ({
 
 export default compose(
   graphql(PROJECT_BY_SLUG_QUERY, {
+    skip: ({ slug }) => {
+      return !slug
+    },
     options: ({ slug }) => ({ variables: { slug } })
   }),
   withSizes(mapSizesToProps)
