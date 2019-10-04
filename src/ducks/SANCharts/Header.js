5be71fc7a17120b87b3a2f2810da77c232df093c
rfihtengolts <9-b-rinat@rambler.ru>
2019-10-04 10:16:07 +0300

    review: simplify code

--- src/ducks/SANCharts/Header.js
+++ src/ducks/SANCharts/Header.js
@@ -205,9 +205,7 @@ const Header = ({
 
 export default compose(
   graphql(PROJECT_BY_SLUG_QUERY, {
-    skip: ({ slug }) => {
-      return !slug
-    },
+    skip: ({ slug }) => !slug,
     options: ({ slug }) => ({ variables: { slug } })
   }),
   withSizes(mapSizesToProps)
