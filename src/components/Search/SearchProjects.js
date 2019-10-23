d4eca683a90bc44f7d05c883a8c7f2b72e4c6750
rfihtengolts <9-b-rinat@rambler.ru>
2019-10-23 15:26:00 +0300

    sort projects by marketcap

--- src/components/Search/SearchProjects.js
+++ src/components/Search/SearchProjects.js
@@ -20,7 +20,7 @@ const SearchProjects = ({
   const projects = projectsList.length > 0 ? projectsList : ALL_PROJECTS
   return (
     <SearchWithSuggestions
-      sorter={({ name: { length: a } }, { name: { length: b } }) => a - b}
+      sorter={({ marketcapUsd: a }, { marketcapUsd: b }) => b - a}
       {...props}
       iconPosition={searchIconPosition}
       data={projects}
