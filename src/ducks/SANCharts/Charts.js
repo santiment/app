6d6b54fd62cd66d7b93958a5a5c0f7b39b940165
rfihtengolts <9-b-rinat@rambler.ru>
2019-10-31 09:36:27 +0300

    merge master fixes

--- src/ducks/SANCharts/Charts.js
+++ src/ducks/SANCharts/Charts.js
@@ -31,7 +31,9 @@ import {
   generateMetricsMarkup,
   findYAxisMetric,
   chartBars,
-  getCrossYValue, isDayStartMetric
+  getCrossYValue,
+  isDayStartMetric,
+  assignToPointDayStartValue
 } from './utils'
 import { Metrics } from './data'
 import { checkHasPremium } from '../../pages/UserSelectors'
@@ -366,8 +368,6 @@ class Charts extends React.Component {
       signalData
     } = this.props
 
-    console.log('inside props', this.props)
-
     const {
       refAreaLeft,
       refAreaRight,
@@ -377,7 +377,8 @@ class Charts extends React.Component {
       yValue,
       activePayload,
       hovered,
-      tooltipMetric
+      tooltipMetric,
+      dayMetrics
     } = this.state
 
     const [bars, ...lines] = generateMetricsMarkup(metrics, {
