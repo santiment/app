de22baa808e5598ada846454e5ce416f74e5e6c1
rfihtengolts <9-b-rinat@rambler.ru>
2019-10-31 16:15:16 +0300

    save commit

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -7,19 +7,23 @@ import Breadcrumbs from './breadcrumbs/Breadcrumbs'
 import Loader from '@santiment-network/ui/Loader/Loader'
 import MobileHeader from '../../components/MobileHeader/MobileHeader'
 import PageLoader from '../../components/Loader/PageLoader'
+import { compose } from 'redux'
+import { graphql } from 'react-apollo'
+import gql from 'graphql-tag'
 import styles from './ProfilePage.module.scss'
 
 const ProfilePage = ({
   profile = {},
   isDesktop,
   isUserLoading,
-  defaultUser,
   defaultUser: { id: defaultUserId, username: defaultUserName },
   match: { params: { id = defaultUserId } = {} } = {}
 }) => {
   const { username = id } = profile
 
   const isCurentUser = defaultUserId === id
+
+  console.log(profile)
 
   if (isUserLoading) {
     return <PageLoader />
@@ -67,4 +71,49 @@ const mapStateToProps = state => ({
   defaultUser: state.user.data
 })
 
-export default connect(mapStateToProps)(ProfilePage)
+const PUBLIC_USER_DATA_QUERY = gql`
+  query getUser($userId: ID) {
+    getUser(selector: { id: $userId }) {
+      id
+      email
+      username
+      watchlists {
+        id
+      }
+      followers {
+        count
+        users {
+          id
+          email
+        }
+      }
+      insights {
+        id
+      }
+      triggers {
+        id
+      }
+    }
+  }
+`
+
+const enhance = compose(
+  connect(mapStateToProps),
+  graphql(PUBLIC_USER_DATA_QUERY, {
+    name: 'profile',
+    skip: ({ userId }) => {
+      return !userId
+    },
+    options: props => {
+      const { userId } = props
+      return {
+        fetchPolicy: 'cache-and-network',
+        variables: {
+          userId: +userId
+        }
+      }
+    }
+  })
+)
+
+export default enhance(ProfilePage)
