18f102bd81e9516cbc478226f2393b7471bc4416
DmitriVanGuard <dimavanguard@gmail.com>
2019-12-03 14:56:31 +0300

    Merging anomalies to csv

--- src/ducks/SANCharts/DownloadCSVBtn.js
+++ src/ducks/SANCharts/DownloadCSVBtn.js
@@ -4,6 +4,32 @@ import Button from '@santiment-network/ui/Button'
 import PropTypes from 'prop-types'
 import { getDateFormats, getTimeFormats } from '../../utils/dates'
 import { mergeTimeseriesByKey } from '../../utils/utils'
+import { Metrics } from './data'
+
+const getEventsWithAnomaly = (headers, data) => {
+  const anomaly = data.find(({ metricAnomalyKey }) => metricAnomalyKey)
+
+  if (!anomaly) {
+    return [headers, data]
+  }
+
+  const newHeaders = headers.slice()
+
+  newHeaders.push({
+    label: `${Metrics[anomaly.metricAnomalyKey].label} Anomaly`,
+    key: 'metricAnomalyKey'
+  })
+
+  const newData = data.map(event =>
+    event.metricAnomalyKey
+      ? {
+        ...event,
+        metricAnomalyKey: true
+      }
+      : event
+  )
+  return [newHeaders, newData]
+}
 
 const DownloadCSVBtn = ({
   metrics,
@@ -18,9 +44,14 @@ const DownloadCSVBtn = ({
   const { HH, mm, ss } = getTimeFormats(date)
   const filename = `${title} [${HH}.${mm}.${ss}, ${DD} ${MMM}, ${YYYY}].csv`
 
+  const [eventHeaders, eventsDataWithAnomalies] = getEventsWithAnomaly(
+    events,
+    eventsData
+  )
+
   const headers = [
     { label: 'Date', key: 'datetime' },
-    ...metrics.concat(events).map(({ label, key, dataKey = key }) => ({
+    ...metrics.concat(eventHeaders).map(({ label, key, dataKey = key }) => ({
       label,
       key: dataKey
     }))
@@ -31,7 +62,7 @@ const DownloadCSVBtn = ({
       filename={filename}
       headers={headers}
       data={mergeTimeseriesByKey({
-        timeseries: [chartData, eventsData]
+        timeseries: [chartData, eventsDataWithAnomalies]
       })}
       {...props}
       as={CSVLink}
