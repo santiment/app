2f8802d4d554dfdfa494c43bbc5dd27e42804806
rfihtengolts <9-b-rinat@rambler.ru>
2019-12-05 15:48:09 +0300

    help btn and mobile version

--- src/pages/feed/Feed.js
+++ src/pages/feed/Feed.js
@@ -2,16 +2,23 @@ import React from 'react'
 import StoriesList from '../../components/Stories/StoriesList'
 import GeneralFeed from './GeneralFeed/GeneralFeed'
 import RecentsFeed from './RecentsFeed/RecentsFeed'
+import { DesktopOnly } from '../../components/Responsive'
+import MobileHeader from '../../components/MobileHeader/MobileHeader'
 import styles from './Feed.module.scss'
 
 const FeedPage = ({}) => {
   return (
     <div className='page'>
+      <MobileHeader title='Feed' />
+
       <StoriesList classes={styles} />
 
       <div className={styles.feeds}>
         <GeneralFeed />
-        <RecentsFeed />
+
+        <DesktopOnly>
+          <RecentsFeed />
+        </DesktopOnly>
       </div>
     </div>
   )
--- src/pages/feed/GeneralFeed/GeneralFeed.js
+++ src/pages/feed/GeneralFeed/GeneralFeed.js
@@ -1,11 +1,24 @@
 import React from 'react'
 import SonarFeedActivityPage from '../../SonarFeed/SonarFeedActivityPage'
 import styles from './GeneralFeed.module.scss'
+import HelpTooltip from '../../../components/WatchlistOverview/WatchlistAnomalies/HelpTooltip'
 
 const GeneralFeed = ({}) => {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>General feed</div>
+      <div className={styles.title}>
+        <div>General feed</div>
+        <HelpTooltip
+          position='bottom'
+          align='start'
+          classes={styles}
+          withDesc={false}
+        >
+          This is a continuous stream of updates on cryptocurrency assets, your
+          personal watchlists and general market conditions, using various
+          Santiment metrics and tools
+        </HelpTooltip>
+      </div>
 
       <div className={styles.scrollable}>
         <SonarFeedActivityPage classes={styles} />
