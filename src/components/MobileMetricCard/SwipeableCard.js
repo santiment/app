6259a801e1946b7fa02793a8f84cf8508874b635
haritonasty <a.kharitonovaspb@gmail.com>
2020-01-05 21:22:08 +0300

    move button width to js file

--- src/components/MobileMetricCard/SwipeableCard.js
+++ src/components/MobileMetricCard/SwipeableCard.js
@@ -3,8 +3,13 @@ import cx from 'classnames'
 import Icon from '@santiment-network/ui/Icon'
 import styles from './SwipeableCard.module.scss'
 
+const BUTTON_WIDTH = 85
+
 const SwipeableCard = ({ children }) => (
-  <div className={styles.container}>
+  <div
+    className={styles.container}
+    style={{ '--button-width': `${BUTTON_WIDTH}px` }}
+  >
     <button className={cx(styles.button, styles.info)}>
       <Icon type='info-round' />
     </button>
