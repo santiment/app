8ad36f0298f584ab4bf65a496f7eb984aeb8bd33
haritonasty <a.kharitonovaspb@gmail.com>
2020-01-16 07:08:37 +0300

    rotate or no

--- src/pages/Detailed/mobile/MobileFullscreenChart.js
+++ src/pages/Detailed/mobile/MobileFullscreenChart.js
@@ -1,13 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import cx from 'classnames'
 import Icon from '@santiment-network/ui/Icon'
 import Button from '@santiment-network/ui/Button'
 import Modal from '@santiment-network/ui/Modal'
 import styles from './MobileFullscreenChart.module.scss'
 
 const MobileFullscreenChart = ({ isOpen, onToggleFullscreen }) => {
+  const [landscapeMode, setLandscapeMode] = useState(false)
+
+  const setOrientation = () => {
+    switch (window.orientation) {
+      case -90:
+      case 90:
+        if (!landscapeMode) {
+          setLandscapeMode(true)
+        }
+        break
+      default:
+        if (landscapeMode) {
+          setLandscapeMode(false)
+        }
+        break
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('orientationchange', setOrientation)
+    return () => {
+      window.removeEventListener('orientationchange', setOrientation)
+    }
+  })
+
   const toggleFullScreen = isOpen => {
     if (document.body.requestFullscreen) {
-      isOpen ? document.body.requestFullscreen() : document.exitFullscreen()
+      if (isOpen) {
+        document.body.requestFullscreen()
+        window.screen.orientation
+          .lock('landscape-primary')
+          .then(() => setLandscapeMode(true))
+          .catch(setOrientation)
+      } else {
+        if (landscapeMode) {
+          window.screen.orientation.unlock()
+        }
+        window.document.exitFullscreen()
+      }
+    } else {
+      setOrientation()
     }
   }
 
@@ -26,16 +65,22 @@ const MobileFullscreenChart = ({ isOpen, onToggleFullscreen }) => {
       open={isOpen}
     >
       {closeModal => (
-        <section className={styles.wrapper}>
+        <section className={cx(styles.wrapper, !landscapeMode && styles.dark)}>
           <Button
             onClick={() => {
               onToggleFullscreen(false)
               toggleFullScreen(false)
               closeModal()
             }}
+            className={cx(styles.button, !landscapeMode && styles.dark__button)}
           >
             <Icon type='close' />
           </Button>
+          {!landscapeMode && (
+            <span className={styles.message}>
+              Please, turn your phone horizontally to see a fullscreen chart
+            </span>
+          )}
         </section>
       )}
     </Modal>
--- src/utils/withSizes.js
+++ src/utils/withSizes.js
@@ -1,5 +1,5 @@
-export const mapSizesToProps = ({ width }) => ({
-  isDesktop: width > 768,
+export const mapSizesToProps = ({ width, height }) => ({
+  isDesktop: width > 768 && height > 500,
   isLaptop: width <= 1200 && width > 992,
   isTablet: width <= 992 && width > 768,
   isPhone: width <= 768
