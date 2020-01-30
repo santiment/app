e7670b4c9b21517aa49f16f56dd1075f65fd0380
rfihtengolts <9-b-rinat@rambler.ru>
2020-01-30 19:17:25 +0300

    hide feed sort by comments

--- src/pages/feed/filter/FeedSorters.js
+++ src/pages/feed/filter/FeedSorters.js
@@ -27,7 +27,7 @@ export const AUTHOR_SORT = {
 export const FILTER_OPTIONS = [
   DATETIME_SORT,
   VOTES_SORT,
-  COMMENTS_SORT,
+  // COMMENTS_SORT, #GarageInc: temporary removed before backend San-7187
   AUTHOR_SORT
 ]
 
