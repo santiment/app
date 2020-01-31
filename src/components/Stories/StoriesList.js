36b93a4d34b0e76eec1d1f131d2864ea1692fe42
rfihtengolts <9-b-rinat@rambler.ru>
2020-01-31 13:45:22 +0300

    add optional scroll btns for desktop stories

--- src/components/Stories/StoriesList.js
+++ src/components/Stories/StoriesList.js
@@ -1,18 +1,67 @@
 import React, { useState } from 'react'
 import cx from 'classnames'
 import Dialog from '@santiment-network/ui/Dialog'
+import Icon from '@santiment-network/ui/Icon'
 import StoryPreview from './StoryPreview'
 import GA from './../../utils/tracking'
 import Story from './Story'
 import { stories } from './content'
 import styles from './StoriesList.module.scss'
+import { DesktopOnly } from '../Responsive'
 
-const StoriesList = ({ classes = {} }) => {
+const ScrollBtn = ({ isRight, show = true, onClick }) => {
+  if (!show) {
+    return null
+  }
+
+  return (
+    <div
+      className={cx(styles.scrollBtn, isRight && styles.btnRight)}
+      onClick={onClick}
+    >
+      <Icon
+        type={isRight ? 'arrow-right' : 'arrow-left'}
+        className={styles.scrollIcon}
+      />
+    </div>
+  )
+}
+
+const SCROLL_OFFSET = 300
+
+const StoriesList = ({ classes = {}, showScrollBtns }) => {
   const [selected, setSelected] = useState()
+
+  const [canScrollLeft, setLeft] = useState(false)
+  const [canScrollRight, setRight] = useState(true)
+  const scrollRef = React.createRef()
+
+  const scroll = isRight => {
+    scrollRef.current.scrollLeft += (isRight ? 1 : -1) * SCROLL_OFFSET
+    setTimeout(() => {
+      if (scrollRef && scrollRef.current) {
+        const current = scrollRef.current
+        setLeft(current.scrollLeft > 0)
+        setRight(
+          current.scrollWidth - current.clientWidth - current.scrollLeft > 0
+        )
+      }
+    }, 500)
+  }
 
   return (
     <section className={cx(styles.list, classes.stories)}>
-      <div className={styles.scrollableWrapper}>
+      <DesktopOnly>
+        {showScrollBtns && canScrollLeft && (
+          <ScrollBtn
+            onClick={() => {
+              scroll(false)
+            }}
+          />
+        )}
+      </DesktopOnly>
+
+      <div className={styles.scrollableWrapper} ref={scrollRef}>
         <div className={cx(styles.scrollable, classes.storiesScrollable)}>
           {stories.map(story => (
             <StoryPreview
@@ -30,6 +79,18 @@ const StoriesList = ({ classes = {} }) => {
           ))}
         </div>
       </div>
+
+      <DesktopOnly>
+        {showScrollBtns && canScrollRight && (
+          <ScrollBtn
+            isRight
+            onClick={() => {
+              scroll(true)
+            }}
+          />
+        )}
+      </DesktopOnly>
+
       <Dialog
         title={(selected || {}).storyHeaderName || ''}
         open={!!selected}
