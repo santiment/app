933014cf618e35dfbedb5329cb714fa476c4da35
rfihtengolts <9-b-rinat@rambler.ru>
2020-01-31 16:50:23 +0300

    add score for trending card

--- src/components/SignalCard/card/TrendingWordsSignalCard.js
+++ src/components/SignalCard/card/TrendingWordsSignalCard.js
@@ -1,59 +1,14 @@
 import React from 'react'
 import cx from 'classnames'
-import { Link } from 'react-router-dom'
 import { DesktopOnly } from '../../Responsive'
 import Panel from '@santiment-network/ui/Panel/Panel'
 import SignalCardHeader from './SignalCardHeader'
 import CopySignal from '../controls/CopySignal'
 import LikeBtnWrapper from '../../Like/LikeBtnWrapper'
 import TrendingCardInsights from './trendingInsights/TrendingCardInsights'
+import TrendingCardWords from './trendingCard/TrendingCardWords'
 import externalStyles from './SignalCard.module.scss'
 import styles from './TrendingWordsSignalCard.module.scss'
-
-const PRESAVED_WORDS = [
-  'vechain',
-  'binance',
-  'crypto',
-  'moon',
-  'ethereum',
-  'ico'
-]
-const MAX_WORDS_COUNT = 10
-
-const getExpectedCount = settings => {
-  if (settings && settings.operation && settings.operation.size) {
-    return settings.operation.size
-  }
-
-  return MAX_WORDS_COUNT
-}
-
-const getWords = (triggerWords, activityPayload) => {
-  if (triggerWords) {
-    return Array.isArray(triggerWords) ? triggerWords : [triggerWords]
-  }
-
-  if (activityPayload) {
-    try {
-      const spliced = activityPayload.split('\n').splice(5, 10)
-
-      return spliced.reduce((acc, item) => {
-        const firstWord = item.split('|')[0]
-
-        if (item) {
-          acc.push(firstWord.trim())
-        }
-
-        return acc
-      }, [])
-    } catch (e) {
-      console.error(e)
-      return PRESAVED_WORDS
-    }
-  }
-
-  return PRESAVED_WORDS
-}
 
 const TrendingWordsSignalCard = ({
   className,
@@ -62,16 +17,7 @@ const TrendingWordsSignalCard = ({
   onLike
 }) => {
   const { id: creatorId } = user
-  const {
-    title,
-    settings,
-    settings: { target = {} },
-    isPublic
-  } = signal
-  const words = getWords(target.word, activityPayload)
-  const showingWords = words.slice(0, 6)
-
-  const moreCount = getExpectedCount(settings) - showingWords.length
+  const { title, settings, isPublic } = signal
 
   return (
     <Panel padding className={cx(externalStyles.wrapper, className)}>
@@ -86,23 +32,10 @@ const TrendingWordsSignalCard = ({
       <div className={externalStyles.wrapper__right}>
         <h2 className={externalStyles.title}>{title}</h2>
 
-        <div className={styles.words}>
-          {showingWords.map((word, index) => (
-            <Link
-              key={index}
-              className={styles.word}
-              to={`/labs/trends/explore/${word}`}
-            >
-              {word}
-            </Link>
-          ))}
-
-          {moreCount && (
-            <Link className={styles.more} to={`/labs/trends`}>
-              +{moreCount} more
-            </Link>
-          )}
-        </div>
+        <TrendingCardWords
+          settings={settings}
+          activityPayload={activityPayload}
+        />
 
         <TrendingCardInsights date={new Date(date)} />
 
---
+++ src/components/SignalCard/card/trendingCard/TrendingCardWords.js
@@ -0,0 +1,107 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Tooltip from '@santiment-network/ui/Tooltip'
+import styles from './TrendingCardWords.module.scss'
+
+const PRESAVED_WORDS = [
+  {
+    word: 'vechain'
+  },
+  {
+    word: 'binance'
+  },
+  {
+    word: 'crypto'
+  },
+  {
+    word: 'moon'
+  },
+  {
+    word: 'ethereum'
+  },
+  {
+    word: 'ico'
+  }
+]
+const MAX_WORDS_COUNT = 10
+
+const getExpectedCount = settings => {
+  if (settings && settings.operation && settings.operation.size) {
+    return settings.operation.size
+  }
+
+  return MAX_WORDS_COUNT
+}
+
+const getWords = (triggerWords, activityPayload) => {
+  if (triggerWords) {
+    return Array.isArray(triggerWords) ? triggerWords : [triggerWords]
+  }
+
+  if (activityPayload) {
+    try {
+      const spliced = activityPayload.split('\n').splice(5, 10)
+
+      return spliced.reduce((acc, item) => {
+        if (item) {
+          const splitted = item.split('|')
+          acc.push({
+            word: splitted[0].trim(),
+            score: splitted[1].trim()
+          })
+        }
+
+        return acc
+      }, [])
+    } catch (e) {
+      console.error(e)
+      return PRESAVED_WORDS
+    }
+  }
+
+  return PRESAVED_WORDS
+}
+
+const TrendingCardWords = ({
+  settings,
+  settings: { target },
+  activityPayload
+}) => {
+  const words = getWords(target.word, activityPayload)
+  const showingWords = words.slice(0, 6)
+
+  const moreCount = getExpectedCount(settings) - showingWords.length
+  return (
+    <div className={styles.words}>
+      {showingWords.map((item, index) => {
+        const { word, score } = item
+
+        return (
+          <Tooltip
+            key={index}
+            position='top'
+            align='center'
+            trigger={
+              <a className={styles.word} href={`/labs/trends/explore/${word}`}>
+                <div className={styles.wordIndex}>{index + 1}</div>
+                {word}
+              </a>
+            }
+          >
+            <div className={styles.tooltip}>
+              <div className={styles.scoreTitle}>Score:</div> {score}
+            </div>
+          </Tooltip>
+        )
+      })}
+
+      {moreCount && (
+        <Link className={styles.more} to={`/labs/trends`}>
+          +{moreCount} more
+        </Link>
+      )}
+    </div>
+  )
+}
+
+export default TrendingCardWords
