3c5c0bb5d29dcfa06df55fe4c2910917f8a65d06
Yura Zatsepin <yura.zatsepin@gmail.com>
2020-02-02 14:37:42 +0500

    Add update method

--- src/utils/tracking.js
+++ src/utils/tracking.js
@@ -23,21 +23,19 @@ function loadScript () {
 }
 
 const initTwitterPixel = () => {
-  !(function (e, t, n, s, u, a) {
-    e.twq ||
-      ((s = e.twq = function () {
-        s.exe ? s.exe.apply(s, arguments) : s.queue.push(arguments)
-      }),
-      (s.version = '1.1'),
-      (s.queue = []),
-      (u = t.createElement(n)),
-      (u.async = !0),
-      (u.src = '//static.ads-twitter.com/uwt.js'),
-      (a = t.getElementsByTagName(n)[0]),
-      a.parentNode.insertBefore(u, a))
-  })(window, document, 'script')
-  // Insert Twitter Pixel ID and Standard Event data below
-  window.twq = twq
+  const importPixel = document.createElement('script')
+  importPixel.async = true
+  importPixel.type = 'text/javascript'
+  importPixel.src = '//static.ads-twitter.com/uwt.js'
+  const head = document.getElementsByTagName('head')[0]
+  head.appendChild(importPixel)
+  window.twq = function twq () {
+    window.twq.exe
+      ? window.twq.exe.apply(window.twq, arguments)
+      : window.twq.queue.push(arguments)
+  }
+  window.twq.version = '1.1'
+  window.twq.queue = []
   window.twq('init', 'o0e0e')
   window.twq('track', 'PageView')
 }
@@ -151,5 +149,6 @@ export function pageview (rawPath, trackerIDs = TRACKER_IDs) {
 export default {
   initializeTracking,
   event,
-  pageview
+  pageview,
+  update
 }
