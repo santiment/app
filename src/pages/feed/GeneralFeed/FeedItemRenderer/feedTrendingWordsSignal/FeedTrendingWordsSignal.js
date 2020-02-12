165ef44e84913bf032045f98bf3548a75c968902
rfihtengolts <9-b-rinat@rambler.ru>
2020-02-12 17:07:13 +0300

    bug with undefined user_trigger_data

--- src/pages/feed/GeneralFeed/FeedItemRenderer/feedTrendingWordsSignal/FeedTrendingWordsSignal.js
+++ src/pages/feed/GeneralFeed/FeedItemRenderer/feedTrendingWordsSignal/FeedTrendingWordsSignal.js
@@ -2,8 +2,6 @@ import React from 'react'
 import styles from './FeedTrendingWordsSignal.module.scss'
 
 const FeedTrendingWordsSignal = ({ trigger, user_trigger_data }) => {
-  console.log(trigger, user_trigger_data)
-
   const { description } = trigger
   const { chart_url } = user_trigger_data
 
