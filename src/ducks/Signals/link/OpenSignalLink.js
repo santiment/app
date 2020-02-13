8f5618f85c90643c707f5507ba363dcbe9d74965
rfihtengolts <9-b-rinat@rambler.ru>
2020-02-13 15:37:12 +0300

    updated regexp for signal title parser

--- src/ducks/Signals/link/OpenSignalLink.js
+++ src/ducks/Signals/link/OpenSignalLink.js
@@ -2,7 +2,10 @@ import React from 'react'
 import SignalMasterModalForm from '../signalModal/SignalMasterModalForm'
 import styles from './OpenSignalLink.module.scss'
 
-const PERCENT_REGEXP = new RegExp('( )?\\d+(\\s{0,1}\\%?( )?|\\s\\b)', 'gi')
+const PERCENT_REGEXP = new RegExp(
+  '( ?\\$?\\d+ {0,1}?.{0,1}?\\d+.?\\d{0,} ?\\%? {0,1}?)',
+  'gi'
+)
 
 const prepareTitle = title => {
   const digits = title.match(PERCENT_REGEXP)
@@ -18,9 +21,14 @@ const prepareTitle = title => {
       if (item) {
         let partsForHighline = temporaryTitle.split(item)
 
-        result.push(partsForHighline[0])
+        let first = partsForHighline[0]
 
-        if (item[0] === '-') {
+        result.push(first)
+
+        const isForceNegative =
+          first.contains('down') || first.contains('below')
+
+        if (item[0] === '-' || isForceNegative) {
           result.push(
             <span key={i} className={styles.down}>
               {item}
