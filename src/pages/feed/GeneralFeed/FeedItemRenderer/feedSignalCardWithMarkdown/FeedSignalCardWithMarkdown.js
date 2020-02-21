9e36ef0e39a8699ca1fa663597c6b6e6e5ff901b
rfihtengolts <9-b-rinat@rambler.ru>
2020-02-21 16:40:37 +0300

    add link to labs for trending cards

--- src/pages/feed/GeneralFeed/FeedItemRenderer/feedSignalCardWithMarkdown/FeedSignalCardWithMarkdown.js
+++ src/pages/feed/GeneralFeed/FeedItemRenderer/feedSignalCardWithMarkdown/FeedSignalCardWithMarkdown.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import styles from './FeedSignalCardWithMarkdown.module.scss'
 import { Link } from 'react-router-dom'
+import { TRENDING_WORDS } from '../../../../../ducks/Signals/utils/constants'
 
-export const MoreInfo = ({ slug }) => {
+export const MoreInfo = ({ slug, type }) => {
+  const link = type === TRENDING_WORDS ? '/labs/trends' : '/projects/' + slug
+
   return (
     slug && (
-      <Link to={'/projects/' + slug} className={styles.more}>
+      <Link to={link} className={styles.more}>
         More info
       </Link>
     )
@@ -14,13 +17,13 @@ export const MoreInfo = ({ slug }) => {
 
 const FeedSignalCardWithMarkdown = ({ trigger, user_trigger_data }) => {
   const { description } = trigger
-  const { chart_url, project_slug } = user_trigger_data
+  const { chart_url, project_slug, trending_words } = user_trigger_data
 
   return (
     <>
       <div className={styles.description}>{description}</div>
 
-      <MoreInfo slug={project_slug} />
+      <MoreInfo slug={project_slug} type={trending_words} />
 
       <img src={chart_url} alt='Backtesting chart' className={styles.img} />
     </>
