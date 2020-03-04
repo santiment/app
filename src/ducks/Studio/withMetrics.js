72c96d30558f25ea682ef0141fecf63e1583fd23
rfihtengolts <9-b-rinat@rambler.ru>
2020-03-04 19:08:34 +0300

    move DAA metrics above timebound metrics

--- src/ducks/Studio/withMetrics.js
+++ src/ducks/Studio/withMetrics.js
@@ -14,6 +14,41 @@ const PROJECT_METRICS_BY_SLUG_QUERY = gql`
   }
 `
 
+function array_move (arr, old_index, new_index) {
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length + 1
+    while (k--) {
+      arr.push(undefined)
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0])
+  return arr // for testing
+}
+
+const moveDaaAbove = categories => {
+  if (categories['On-chain']) {
+    const groups = categories['On-chain']
+
+    if (groups) {
+      let keys = Object.keys(groups)
+
+      const indexNetworkActivity = keys.indexOf('Network Activity')
+      if (keys.length > 1 && indexNetworkActivity !== 1) {
+        keys = array_move(keys, indexNetworkActivity, 1)
+
+        const newGroups = {}
+        keys.forEach(key => {
+          newGroups[key] = groups[key]
+        })
+
+        categories['On-chain'] = newGroups
+      }
+    }
+  }
+
+  return categories
+}
+
 export default graphql(PROJECT_METRICS_BY_SLUG_QUERY, {
   props: ({
     data: {
@@ -26,12 +61,14 @@ export default graphql(PROJECT_METRICS_BY_SLUG_QUERY, {
     },
     ownProps: { noMarketSegments, hiddenMetrics = [] }
   }) => {
-    const categories = getCategoryGraph(
+    let categories = getCategoryGraph(
       availableQueries
         .concat(availableMetrics)
         .concat(noMarketSegments ? [] : marketSegments.map(getMarketSegment)),
       hiddenMetrics
     )
+
+    categories = moveDaaAbove(categories)
 
     return {
       loading,
