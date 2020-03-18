9ea7dbf7655067329a7b9007f355bcb8b7bc2ed9
rfihtengolts <9-b-rinat@rambler.ru>
2020-03-18 15:36:28 +0300

    removed junk code

--- src/pages/Login/LoginEmailForm.js
+++ src/pages/Login/LoginEmailForm.js
@@ -10,9 +10,7 @@ import { InputWithIcon as Input } from '@santiment-network/ui/Input'
 import Button from '@santiment-network/ui/Button'
 import { PATHS } from '../../App'
 import MobileWrapper from './Mobile/MobileWrapper'
-import FormikInput, {
-  validateEmail
-} from '../../components/formik-santiment-ui/FormikInput'
+import FormikInput from '../../components/formik-santiment-ui/FormikInput'
 import FormikEffect from '../../components/formik-santiment-ui/FormikEffect'
 import styles from './index.module.scss'
 
