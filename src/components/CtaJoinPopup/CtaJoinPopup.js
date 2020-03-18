46b692e239bf92f5bb7cac4e521b2a1ed8cd5653
rfihtengolts <9-b-rinat@rambler.ru>
2020-03-18 11:41:37 +0300

    add timeout

--- src/components/CtaJoinPopup/CtaJoinPopup.js
+++ src/components/CtaJoinPopup/CtaJoinPopup.js
@@ -1,32 +1,70 @@
-import React from 'react'
-import Button from '@santiment-network/ui/Button'
-import joinImg from './../../../src/assets/join.svg'
-import styles from './CtaJoinPopup.module.scss'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 import { PATHS } from '../../App'
+import Button from '@santiment-network/ui/Button'
+import Dialog from '@santiment-network/ui/Dialog'
+import joinImg from './../../../src/assets/join.svg'
+import Panel from '@santiment-network/ui/Panel'
+import { checkIsLoggedIn } from '../../pages/UserSelectors'
+import styles from './CtaJoinPopup.module.scss'
 
-const CtaJoinPopup = () => {
+const TIMEOUT = 2 * 60 * 1000
+
+let timeoutId = null
+
+const CtaJoinPopup = isLoggedIn => {
+  if (isLoggedIn) {
+    return null
+  }
+
+  const [isOpen, setOpen] = useState(false)
+
+  useEffect(() => {
+    timeoutId = setTimeout(() => {
+      if (!isLoggedIn) {
+        setOpen(true)
+      }
+    }, TIMEOUT)
+
+    return () => {
+      timeoutId && clearTimeout(timeoutId)
+    }
+  }, [])
+
   return (
-    <div className={styles.container}>
-      <img src={joinImg} alt='Join' />
+    <Dialog
+      title=''
+      open={isOpen}
+      onOpen={() => setOpen(true)}
+      onClose={() => setOpen(false)}
+      classes={styles}
+    >
+      <Panel padding className={styles.container}>
+        <img src={joinImg} alt='Join' />
 
-      <div className={styles.title}>Join the community!</div>
-      <div className={styles.description}>
-        Santiment is a behavior analytics platform for cryptocurrencies,
-        sourcing on-chain, social and development information on 900+ coins.
-        Sign up now to receive 20% discount!
-      </div>
-      <Button
-        as={Link}
-        to={PATHS.LOGIN}
-        variant='fill'
-        accent='positive'
-        className={styles.btn}
-      >
-        Start crypto journey
-      </Button>
-    </div>
+        <div className={styles.join}>Join the community!</div>
+        <div className={styles.description}>
+          Santiment is a behavior analytics platform for cryptocurrencies,
+          sourcing on-chain, social and development information on 900+ coins.
+          Sign up now to receive 20% discount!
+        </div>
+        <Button
+          as={Link}
+          to={PATHS.LOGIN}
+          variant='fill'
+          accent='positive'
+          className={styles.btn}
+        >
+          Start crypto journey
+        </Button>
+      </Panel>
+    </Dialog>
   )
 }
 
-export default CtaJoinPopup
+const mapStateToProps = state => ({
+  isLoggedIn: checkIsLoggedIn(state)
+})
+
+export default connect(mapStateToProps)(CtaJoinPopup)
