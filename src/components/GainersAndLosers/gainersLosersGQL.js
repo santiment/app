e54e1b2942ea03e4c3b73649c4bc83aba5f634b4
haritonasty <a.kharitonovaspb@gmail.com>
2020-03-27 22:28:37 +0300

    delete devanshu unused page

--- src/components/GainersAndLosers/gainersLosersGQL.js
+++ src/components/GainersAndLosers/gainersLosersGQL.js
@@ -1,24 +1,5 @@
 import gql from 'graphql-tag'
 
-export const SOCIAL_GAINERS_LOSERS_STATUS_QUERY = gql`
-  query socialGainersLosersStatus(
-    $slug: String!
-    $timeWindow: String!
-    $from: DateTime!
-    $to: DateTime!
-  ) {
-    socialGainersLosersStatus(
-      slug: $slug
-      from: $from
-      to: $to
-      timeWindow: $timeWindow
-    ) {
-      datetime
-      change
-    }
-  }
-`
-
 export const TOP_SOCIAL_GAINERS_LOSERS_QUERY = gql`
   query topSocialGainersLosers(
     $status: SocialGainersLosersStatusEnum!
