bb38330cf474aeb6f933cc420706b074c9d4b812
DmitriVanGuard <dimavanguard@gmail.com>
2020-04-11 15:26:55 +0300

    Updating templates cache after template creation

--- src/ducks/Studio/Template/gql/hooks.js
+++ src/ducks/Studio/Template/gql/hooks.js
@@ -1,7 +1,23 @@
 import { useQuery, useMutation } from '@apollo/react-hooks'
 import { TEMPLATES_QUERY, CREATE_TEMPLATE_MUTATION } from './index'
+import { store } from '../../../../index'
 
 const DEFAULT_TEMPLATES = []
+
+function updateTemplatesCache (cache, { data: { template } }) {
+  const variables = { userId: +store.getState().user.data.id }
+
+  const { templates } = cache.readQuery({
+    variables,
+    query: TEMPLATES_QUERY
+  })
+
+  cache.writeQuery({
+    variables,
+    query: TEMPLATES_QUERY,
+    data: { templates: [template].concat(templates) }
+  })
+}
 
 export function useUserTemplates (id) {
   const { data, loading, error } = useQuery(TEMPLATES_QUERY, {
@@ -15,7 +31,9 @@ export function useUserTemplates (id) {
 }
 
 export function useCreateTemplate () {
-  const [mutate, data] = useMutation(CREATE_TEMPLATE_MUTATION)
+  const [mutate, data] = useMutation(CREATE_TEMPLATE_MUTATION, {
+    update: updateTemplatesCache
+  })
 
   function createTemplate (title) {
     return mutate({
