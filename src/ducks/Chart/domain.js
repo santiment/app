7a9a0a336bbd011d21e86027038c242fb618b29e
DmitriVanGuard <dimavanguard@gmail.com>
2020-04-13 11:19:38 +0300

    Chart domain +/- 1%

--- src/ducks/Chart/domain.js
+++ src/ducks/Chart/domain.js
@@ -1,14 +1,24 @@
 import { Metric } from '../dataHub/metrics'
 
+const DEFAULT_DOMAIN_METRIC_KEYS = [
+  Metric.twitter_followers.key,
+  Metric.minersBalance.key
+]
+
 export function domainModifier (metricKey, minMax) {
+  if (DEFAULT_DOMAIN_METRIC_KEYS.some(key => metricKey.startsWith(key))) {
+    return
+  }
+
   let { min, max } = minMax
 
   const metric = Metric[metricKey]
   if (metric && metric.node === 'bar') {
-    max *= 1.03
+    max *= 1.01
     min = 0
   } else {
-    return
+    max *= 1.01
+    min *= min > 0 ? 0.99 : 1.01
   }
 
   minMax.max = max
