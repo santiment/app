0bba634c766c52b7596c1664880dd1aee60e99cf
DmitriVanGuard <dimavanguard@gmail.com>
2020-04-16 14:46:57 +0300

    Pulse insight card styles

--- src/components/Insight/PulseInsight/PulseInsight.svelte
+++ src/components/Insight/PulseInsight/PulseInsight.svelte
@@ -1,10 +1,10 @@
 <script>
   import { setContext } from 'svelte'
   import SveltePulseInsight from 'insights-app/lib/components/insights/PulseCard'
-  import { toggleInsightLike } from './utils'
+  import { toggleInsightLike, transformLink } from './utils'
 
   export let insight, className
   setContext('toggleInsightLike', toggleInsightLike)
 </script>
 
-<SveltePulseInsight {insight} class="{className}" />
+<SveltePulseInsight {insight} {transformLink} class="{className}" />
--- src/components/Insight/PulseInsight/utils.js
+++ src/components/Insight/PulseInsight/utils.js
@@ -1,14 +1,21 @@
-import { client } from "../../../index";
+import { client } from '../../../index'
 import {
   LIKE_INSIGHT_MUTATION,
-  UNLIKE_INSIGHT_MUTATION
-} from "../../Like/likesGQL";
+  UNLIKE_INSIGHT_MUTATION,
+} from '../../Like/likesGQL'
+import { getAPIUrl } from '../../../utils/utils'
+
+const insightsBaseLink = getAPIUrl().includes('stage')
+  ? 'https://insights-stage.santiment.net'
+  : 'https://insights.santiment.net'
+
+export const transformLink = (link) => insightsBaseLink + link
 
 export function toggleInsightLike(insightId, shouldLike) {
   return client.mutate({
     mutation: shouldLike ? LIKE_INSIGHT_MUTATION : UNLIKE_INSIGHT_MUTATION,
     variables: {
-      id: +insightId
-    }
-  });
+      id: +insightId,
+    },
+  })
 }
