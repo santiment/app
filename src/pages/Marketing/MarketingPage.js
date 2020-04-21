cdff470b88ad09461ecc2184d20fe906ea2ab573
rinatf <9-b-rinat@rambler.ru>
2020-04-21 13:46:47 +0300

    add categories and airtable insights

--- src/pages/Marketing/MarketingPage.js
+++ src/pages/Marketing/MarketingPage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import cx from "classnames";
 import ProMetricsFooter from "../ProMetrics/ProMetricsFooter/ProMetricsFooter";
 import StoriesList from "../../components/Stories/StoriesList";
+import WatchlistCards from "../../components/Watchlists/WatchlistCards";
+import {CATEGORIES} from "../assets/assets-overview-constants";
 import styles from './MarketingPage.module.scss'
 
 const MarketingPage = () => {
@@ -9,8 +11,23 @@ const MarketingPage = () => {
 
     <StoriesList classes={styles} showScrollBtns />
 
+    <div className={styles.inner}>
+      <div className={styles.block}>
+        <div className={styles.subTitle}>Indices</div>
+        <WatchlistCards watchlists={CATEGORIES}/>
+      </div>
 
-    MarketingPage
+      <div className={styles.block}>
+        <div className={styles.subTitle}>Insights Signals Leaderboard</div>
+        <iframe title='insights' className="airtable-embed"
+                src="https://airtable.com/embed/shrCwTMKbFLiRn3Eq?backgroundColor=gray&viewControls=on" frameBorder="0"
+                onmousewheel="" width="100%" height="533"
+                style={{
+                  background: "transparent",
+                  border: "1px solid #ccc"
+                }}></iframe>
+      </div>
+    </div>
 
     <ProMetricsFooter />
   </div>
