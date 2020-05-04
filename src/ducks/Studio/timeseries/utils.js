eeb13bf37a2de5082888c3df852dca1b373afb3a
DmitriVanGuard <dimavanguard@gmail.com>
2020-05-04 14:50:49 +0300

    Complete mergeTimeseries algorithm

--- src/ducks/Studio/timeseries/utils.js
+++ src/ducks/Studio/timeseries/utils.js
@@ -1,13 +1,151 @@
+const OLD_DATE = { datetime: 0 }
+
 // TODO: Remove this after moving to dynamic query aliasing instead of preTransform [@vanguard | March 4, 2020]
 export const aliasTransform = (key, dataKey = key) => alias => data =>
   extractTimeseries(key)(data).map(({ datetime, ...value }) => ({
     datetime,
-    [alias]: value[dataKey]
+    [alias]: value[dataKey],
   }))
 
 export const extractTimeseries = name => ({ data }) => data[name]
 
 export const normalizeDatetimes = data => ({
   ...data,
-  datetime: +new Date(data.datetime)
+  datetime: +new Date(data.datetime),
 })
+
+const newDataMapper = data => Object.assign({}, data)
+
+function findDatetimeBorder(baseTs, cursor, targetDatetime) {
+  const baseTsLength = baseTs.length
+
+  do {
+    cursor++
+  } while (
+    cursor < baseTsLength &&
+    targetDatetime > new Date(baseTs[cursor].datetime)
+  )
+
+  return cursor
+}
+
+export const mergeTimeseries = (timeseries, options) => {
+  const timeseriesAmount = timeseries.length
+
+  if (timeseriesAmount === 1) {
+    return timeseries[0]
+  }
+
+  // Finding longest timeserie
+  let longestTS = timeseries[0]
+  for (let i = 0; i < timeseriesAmount; i++) {
+    longestTS =
+      longestTS.length > timeseries[i].length ? longestTS : timeseries[i]
+  }
+
+  let baseTs = longestTS.map(newDataMapper)
+
+  for (let i = 0; i < timeseriesAmount; i++) {
+    const timeserie = timeseries[i]
+
+    if (timeserie === longestTS) continue
+
+    const tsLength = timeserie.length
+    let baseTsCursor = 0
+
+    for (
+      let timeserieCursor = 0;
+      timeserieCursor < tsLength;
+      timeserieCursor++, baseTsCursor++
+    ) {
+      const isBaseInBounds = baseTsCursor < baseTs.length
+      const timeserieData = timeserie[timeserieCursor]
+      const baseTsData = isBaseInBounds ? baseTs[baseTsCursor] : OLD_DATE
+
+      if (timeserieData.datetime === baseTsData.datetime) {
+        Object.assign(baseTsData, timeserieData)
+        continue
+      }
+
+      const timeserieDatetime = new Date(timeserieData.datetime)
+      const baseTsDatetime = new Date(baseTsData.datetime)
+
+      if (timeserieDatetime > baseTsDatetime) {
+        // current timeserie's datetime is greater than the base
+
+        /* do {
+         *   baseTsCursor++
+         * } while (
+         *   baseTsCursor < baseTs.length &&
+         *   timeserieDatetime > new Date(baseTs[baseTsCursor].datetime)
+         * ) */
+
+        baseTsCursor = findDatetimeBorder(
+          baseTs,
+          baseTsCursor,
+          timeserieDatetime,
+        )
+
+        if (baseTsCursor >= baseTs.length) {
+          // Base doesn't have data after this datetime
+          baseTs = baseTs.concat(
+            timeserie.slice(timeserieCursor).map(newDataMapper),
+          )
+          break
+        } else {
+          // Found base
+          const foundBaseTs = baseTs[baseTsCursor]
+          if (timeserieData.datetime === foundBaseTs.datetime) {
+            // Merging timeseries with same datetime
+            Object.assign(foundBaseTs, timeserieData)
+          } else {
+            // Inserting it before found base
+            baseTs.splice(baseTsCursor, 0, newDataMapper(timeserieData))
+          }
+        }
+      } else {
+        // current timeserie's datetime is less than the base
+        const timeserieLeftCursor = timeserieCursor
+
+        /* do {
+         *   timeserieCursor++
+         * } while (
+         *   timeserieCursor < timeserie.length &&
+         *   baseTsDatetime > new Date(timeserie[timeserieCursor].datetime)
+         * ) */
+
+        timeserieCursor = findDatetimeBorder(
+          timeserie,
+          timeserieCursor,
+          baseTsDatetime,
+        )
+
+        if (timeserieCursor >= timeserie.length) {
+          // No base found with similar datetime
+          baseTs.splice(
+            baseTsCursor,
+            0,
+            ...timeserie.slice(timeserieLeftCursor).map(newDataMapper),
+          )
+          break
+        } else {
+          // Found potentially similar base datetime
+          baseTs.splice(
+            baseTsCursor,
+            0,
+            ...timeserie
+              .slice(timeserieLeftCursor, timeserieCursor)
+              .map(newDataMapper),
+          )
+
+          const foundTimeserieData = timeserie[timeserieCursor]
+          if (baseTsData.datetime === foundTimeserieData.datetime) {
+            Object.assign(baseTsData, foundTimeserieData)
+          }
+        }
+      }
+    }
+  }
+
+  return baseTs
+}
