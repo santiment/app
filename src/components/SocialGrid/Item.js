b349820affa9c11cf22836a3016b569c75438e37
haritonasty <a.kharitonovaspb@gmail.com>
2020-05-05 12:18:54 +0300

    link from card

--- src/components/SocialGrid/Item.js
+++ src/components/SocialGrid/Item.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styles from './Item.module.scss'
 
-const Item = ({ topic }) => (
-  <article className={styles.wrapper}>{topic}</article>
+const Item = ({ topic, onTopicClick }) => (
+  <article className={styles.wrapper}>
+    <Link to={`/labs/trends/explore/${topic}`} className={styles.text}>
+      {topic}
+    </Link>
+  </article>
 )
 
 export default Item
--- src/components/SocialGrid/index.js
+++ src/components/SocialGrid/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import cx from 'classnames'
+import { updateHistory } from '../../utils/utils'
 import { TOPICS } from './topics'
 import Item from './Item'
 import styles from './index.module.scss'
 
-const SocialGrid = ({ className }) => (
-  <section className={cx(styles.wrapper, className)}>
-    {TOPICS.map((topic, idx) => (
-      <Item key={idx} topic={topic} />
-    ))}
-  </section>
-)
+const SocialGrid = ({ className, onTopicClick }) => {
+  return (
+    <section className={cx(styles.wrapper, className)}>
+      {TOPICS.map((topic, idx) => (
+        <Item key={idx} topic={topic} onTopicClick={onTopicClick} />
+      ))}
+    </section>
+  )
+}
 
 export default SocialGrid
