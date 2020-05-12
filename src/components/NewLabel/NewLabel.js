b241f96452624932a380b5602956587d25e204f9
rinatf <9-b-rinat@rambler.ru>
2020-05-12 11:47:18 +0300

    add new label for templates

---
+++ src/components/NewLabel/NewLabel.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import cx from 'classnames'
+import Label from '@santiment-network/ui/Label'
+import styles from './NewLabel.module.scss'
+import { addDays, dateDifferenceInWords } from '../../utils/dates'
+import DarkTooltip from '../Tooltip/DarkTooltip'
+
+const NOW = new Date()
+
+const showNewLabel = (date, limitDays = 7) => {
+  return date && addDays(date, limitDays).getTime() > NOW.getTime()
+}
+
+const NewLabel = ({ date, className }) => {
+  if (!showNewLabel(date)) {
+    return null
+  }
+
+  return (
+    <DarkTooltip
+      position='top'
+      align='start'
+      on='hover'
+      className={styles.tooltip}
+      trigger={
+        <Label
+          className={cx(styles.label, className)}
+          accent='jungle-green'
+          variant='fill'
+        >
+          NEW
+        </Label>
+      }
+    >
+      Created{' '}
+      {dateDifferenceInWords({ from: new Date(date), to: NOW, format: 'd' })}
+    </DarkTooltip>
+  )
+}
+
+export default NewLabel
