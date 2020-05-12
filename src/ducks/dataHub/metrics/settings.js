7c01f5d61bf17bf8000e511138c036174a2aa471
DmitriVanGuard <dimavanguard@gmail.com>
2020-05-12 10:25:33 +0300

    Top holder selectors

--- src/ducks/dataHub/metrics/settings.js
+++ src/ducks/dataHub/metrics/settings.js
@@ -1,15 +1,18 @@
 import { Metric } from './index'
 
-export const MetricSettings = Object.assign(Object.create(null), {
-  [Metric.amount_in_top_holders.key]: [
-    {
-      key: 'holdersCount',
-      label: 'Top Holders',
-      defaultValue: 10,
-      constraints: {
-        min: 1,
-        max: 1000000
-      }
-    }
-  ]
-})
+// NOTE: It's safe to pass it as reference, because it will not be modified [@vanguard | May12, 2020]
+const TOP_HOLDERS = {
+  key: 'holdersCount',
+  label: 'Top Holders',
+  defaultValue: 10,
+  constraints: {
+    min: 1,
+    max: 1000000,
+  },
+}
+
+export const MetricSettings = {
+  [Metric.amount_in_top_holders.key]: [TOP_HOLDERS],
+  [Metric.amount_in_exchange_top_holders.key]: [TOP_HOLDERS],
+  [Metric.amount_in_non_exchange_top_holders.key]: [TOP_HOLDERS],
+}
