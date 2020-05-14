ee6e90d20a83e7e903b0adae82f4899bbdc40761
DmitriVanGuard <dimavanguard@gmail.com>
2020-05-14 14:21:16 +0300

    Chart png download fix

--- src/ducks/Chart/history.js
+++ src/ducks/Chart/history.js
@@ -1,41 +1,41 @@
-export function initHistory(chart) {
-  const { canvas, ctx } = chart;
-  const history = [];
-  let cursor = -1;
+export function initHistory (chart) {
+  const { canvas, ctx } = chart
+  const history = []
+  let cursor = -1
 
-  function save() {
+  function save () {
     if (++cursor < history.length) {
-      history.length = cursor;
+      history.length = cursor
     }
 
-    history.push(canvas.toDataURL());
+    history.push(canvas.toDataURL())
   }
 
-  function undo() {
-    if (cursor < 0) return;
+  function undo () {
+    if (cursor < 0) return
 
-    redraw();
+    redraw()
 
-    cursor -= 1;
+    cursor -= 1
   }
 
-  function redo() {
-    if (cursor > history.length - 1) return;
+  function redo () {
+    if (cursor > history.length - 1) return
 
-    redraw();
+    redraw()
 
-    cursor += 1;
+    cursor += 1
   }
 
-  function redraw() {
-    const savedCtx = new Image();
-    savedCtx.src = history[cursor];
-    savedCtx.onload = () => ctx.drawImage(savedCtx, 0, 0);
+  function redraw () {
+    const savedCtx = new Image()
+    savedCtx.src = history[cursor]
+    savedCtx.onload = () => ctx.drawImage(savedCtx, 0, 0)
   }
 
   return {
     save,
     undo,
     redo
-  };
+  }
 }
