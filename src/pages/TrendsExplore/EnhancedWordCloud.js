e660b279d5e3cfb3e11bfb3b9bd328096f2c0abf
haritonasty <a.kharitonovaspb@gmail.com>
2020-05-25 14:08:29 +0300

    add dropdown with word selection

--- src/pages/TrendsExplore/EnhancedWordCloud.js
+++ src/pages/TrendsExplore/EnhancedWordCloud.js
@@ -1,8 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import cx from 'classnames'
+import Tooltip from '@santiment-network/ui/Tooltip'
+import Button from '@santiment-network/ui/Button'
+import Panel from '@santiment-network/ui/Panel'
 import Calendar from '../../ducks/Studio/AdvancedView/Calendar'
 import WordCloud from '../../components/WordCloud/WordCloud'
 import { getTimePeriod } from './Sidebar'
-import styles from './Sidebar.module.scss'
+import styles from './EnhancedWordCloud.module.scss'
 
 const MAX_DATE = new Date()
 
@@ -11,18 +15,55 @@ const EnhancedWordCloud = ({ words, isDesktop, ...props }) => {
   const [date, setDate] = useState([MAX_DATE])
   const [period, setPeriod] = useState(getTimePeriod(MAX_DATE))
 
+  useEffect(
+    () => {
+      if (words.length > 1) {
+        setDate([MAX_DATE])
+        setPeriod(getTimePeriod(MAX_DATE))
+      }
+
+      if (!words.includes(word)) {
+        setWord(words[0])
+      }
+    },
+    [words]
+  )
+
   function onCalendarChange (datetime) {
     setDate([datetime])
     setPeriod(getTimePeriod(datetime))
   }
 
   return (
-    <div className={styles.cloud__wrapper}>
-      {isDesktop && (
+    <div className={styles.wrapper}>
+      {words.length > 1 ? (
+        <Tooltip
+          on='click'
+          trigger={
+            <Button variant='flat' border className={styles.trigger}>
+              {word}
+            </Button>
+          }
+          position='bottom'
+          align='end'
+        >
+          <Panel className={styles.panel}>
+            {words.map(item => (
+              <span
+                className={cx(styles.word, item === word && styles.selected)}
+                key={item}
+                onClick={() => setWord(item)}
+              >
+                {item}
+              </span>
+            ))}
+          </Panel>
+        </Tooltip>
+      ) : (
         <Calendar
           dates={date}
           onChange={onCalendarChange}
-          className={styles.cloud__calendar}
+          className={styles.calendar}
           maxDate={MAX_DATE}
         />
       )}
@@ -31,6 +72,7 @@ const EnhancedWordCloud = ({ words, isDesktop, ...props }) => {
         size={15}
         className={styles.cloud}
         infoClassName={styles.cloud__header}
+        contentClassName={styles.cloud__content}
         word={word}
         {...period}
       />
