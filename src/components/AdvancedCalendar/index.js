a8d16b9d7d266fa6bf117e64e1439e947c65fb1e
DmitriVanGuard <dimavanguard@gmail.com>
2020-06-01 01:24:19 +0300

    junk removed

--- src/components/AdvancedCalendar/index.js
+++ src/components/AdvancedCalendar/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React from 'react'
 import cx from 'classnames'
 import Icon from '@santiment-network/ui/Icon'
 import AdvancedCalendarPopup from './Popup'
