18341420f47f655cca83e0113b28177f11ca72b7
rinatf <9-b-rinat@rambler.ru>
2020-06-04 17:09:25 +0300

    add dev_activity_changed_30d

--- src/pages/MarketSegments/epics.js
+++ src/pages/MarketSegments/epics.js
@@ -1,8 +1,8 @@
 import { Observable } from 'rxjs'
-import { PROJECTS_QUERY } from './queries'
+import { DEV_ACTIVITY_CHANGE_QUERY, PROJECTS_QUERY } from './queries'
 import { handleErrorAndTriggerAction } from '../../epics/utils'
 import * as actions from './actions'
-import devActChange30d from './devActChange.json'
+import { addDays } from '../../utils/dates'
 
 const getFunction = segment =>
   `{"name": "market_segments", "args":{"market_segments": [ "${segment}" ]}}`
@@ -25,15 +25,35 @@ export const fetchMarketSegments = (action$, store, { client }) =>
           query: PROJECTS_QUERY,
           variables: { fn: Fn[segment] },
           fetchPolicy
+        }),
+        client.query({
+          query: DEV_ACTIVITY_CHANGE_QUERY,
+          variables: {
+            from: addDays(new Date(), -30).toISOString(),
+            to: new Date().toISOString()
+          },
+          fetchPolicy
         })
       ])
     })
-    .mergeMap(([forced, { data: { assets } }]) => {
+    .mergeMap(([forced, { data: { assets } }, { data: { allProjects } }]) => {
+      const activityChangeMapping = allProjects.reduce((acc, item) => {
+        acc[item.slug] = item
+        return acc
+      }, {})
+
       const payload = {
-        assets: assets.map(asset => ({
-          ...asset,
-          devActChange30d: devActChange30d[asset.slug]
-        }))
+        assets: assets.map(asset => {
+          if (!activityChangeMapping[asset.slug]) {
+            return asset
+          }
+
+          return {
+            ...asset,
+            devActChange30d:
+              activityChangeMapping[asset.slug].dev_activity_change_30d
+          }
+        })
       }
       if (forced) {
         payload.timestamp = Date.now()
--- src/pages/MarketSegments/queries.js
+++ src/pages/MarketSegments/queries.js
@@ -15,3 +15,19 @@ export const PROJECTS_QUERY = gql`
     }
   }
 `
+
+export const DEV_ACTIVITY_CHANGE_QUERY = gql`
+  query allProjectsDevActivity($from: DateTime!, $to: DateTime!) {
+    allProjects {
+      name
+      slug
+      dev_activity_change_30d: aggregatedTimeseriesData(
+        metric: "dev_activity_change_30d"
+        from: $from
+        to: $to
+        aggregation: LAST
+        includeIncompleteData: true
+      )
+    }
+  }
+`
