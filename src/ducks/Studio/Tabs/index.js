42fa7bb0f26bffe2865418f17a1cf14cffcc15ed
rinatf <9-b-rinat@rambler.ru>
2020-06-16 18:25:19 +0300

    add checking templates for tabs on studio

--- src/ducks/Studio/Tabs/index.js
+++ src/ducks/Studio/Tabs/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import cx from 'classnames'
 import { NavLink, withRouter } from 'react-router-dom'
 import styles from './index.module.scss'
+import { getTemplateIdFromURL } from '../Template/utils'
+
+const SUB_PATHS = {
+  STATS: 'stats',
+  STUDIO: ''
+}
 
 const tabs = [
   {
@@ -9,16 +15,28 @@ const tabs = [
     label: 'Studio'
   },
   {
-    index: 'stats',
+    index: SUB_PATHS.STATS,
     label: 'Key Stats'
   }
 ]
+
+function getPreparedLink (root, index) {
+  if (getTemplateIdFromURL()) {
+    const parts = window.location.pathname.split('/')
+    const templateUrl = parts[parts.length - 1]
+
+    const indexPart = index ? '/' + index : ''
+    return root + indexPart + '/' + templateUrl
+  }
+
+  return root + '/' + index
+}
 
 const Tabs = ({ root, location: { search } }) => {
   return (
     <div className={styles.tabs}>
       {tabs.map(({ index, label }) => {
-        const to = root + '/' + index
+        const to = getPreparedLink(root, index)
         return (
           <NavLink
             key={to}
