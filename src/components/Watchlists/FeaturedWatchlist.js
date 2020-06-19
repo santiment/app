7e0e1e9b93007d482b9f7082489d7eabc01ff68b
rinatf <9-b-rinat@rambler.ru>
2020-06-19 19:54:05 +0300

    update filters for  featured watchlists

--- src/components/Watchlists/FeaturedWatchlist.js
+++ src/components/Watchlists/FeaturedWatchlist.js
@@ -1,31 +1,29 @@
 import React from 'react'
-import AssetsOverviewCard from './WatchlistCard'
+import WatchlistCard from './WatchlistCard'
 import GetFeaturedWatchlists from './../../ducks/Watchlists/GetFeaturedWatchlists'
 import { getWatchlistLink } from './../../ducks/Watchlists/watchlistUtils'
 
 export const getSharedWatchlistLink = watchlist =>
   getWatchlistLink(watchlist) + '#shared'
 
-const FeaturedWatchlists = ({ additional = [] }) => (
+const FeaturedWatchlists = () => (
   <GetFeaturedWatchlists
     render={({ isWatchlistsLoading, watchlists }) => {
       if (isWatchlistsLoading) {
         return null
       }
 
-      return watchlists
-        .filter(({ listItems }) => !listItems.length)
-        .map(watchlist => (
-          <AssetsOverviewCard
+      return watchlists.map(watchlist => {
+        return (
+          <WatchlistCard
             key={watchlist.id}
-            price={32}
-            change={1.22}
-            name={watchlist.name}
-            isPublic={watchlist.isPublic}
+            watchlist={watchlist}
             to={getSharedWatchlistLink(watchlist)}
             slugs={watchlist.listItems.map(({ project }) => project.slug)}
+            {...watchlist}
           />
-        ))
+        )
+      })
     }}
   />
 )
