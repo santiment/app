18a658908028f152f7c713f22dd3f90682b61ff3
rinatf <9-b-rinat@rambler.ru>
2020-06-19 19:03:23 +0300

    show featured watchlists on home page

--- src/components/Watchlists/FeaturedWatchlist.js
+++ src/components/Watchlists/FeaturedWatchlist.js
@@ -2,31 +2,32 @@ import React from 'react'
 import AssetsOverviewCard from './WatchlistCard'
 import GetFeaturedWatchlists from './../../ducks/Watchlists/GetFeaturedWatchlists'
 import { getWatchlistLink } from './../../ducks/Watchlists/watchlistUtils'
-import styles from './WatchlistCards.module.scss'
 
 export const getSharedWatchlistLink = watchlist =>
   getWatchlistLink(watchlist) + '#shared'
 
-const FeaturedWatchlists = () => (
-  <div className={styles.wrapper}>
-    <GetFeaturedWatchlists
-      render={({ isWatchlistsLoading, watchlists }) =>
-        watchlists
-          .filter(({ listItems }) => Boolean(listItems.length))
-          .map(watchlist => (
-            <AssetsOverviewCard
-              key={watchlist.id}
-              price={32}
-              change={1.22}
-              name={watchlist.name}
-              isPublic={watchlist.isPublic}
-              to={getSharedWatchlistLink(watchlist)}
-              slugs={watchlist.listItems.map(({ project }) => project.slug)}
-            />
-          ))
+const FeaturedWatchlists = ({ additional = [] }) => (
+  <GetFeaturedWatchlists
+    render={({ isWatchlistsLoading, watchlists }) => {
+      if (isWatchlistsLoading) {
+        return null
       }
-    />
-  </div>
+
+      return watchlists
+        .filter(({ listItems }) => !listItems.length)
+        .map(watchlist => (
+          <AssetsOverviewCard
+            key={watchlist.id}
+            price={32}
+            change={1.22}
+            name={watchlist.name}
+            isPublic={watchlist.isPublic}
+            to={getSharedWatchlistLink(watchlist)}
+            slugs={watchlist.listItems.map(({ project }) => project.slug)}
+          />
+        ))
+    }}
+  />
 )
 
 export default FeaturedWatchlists
