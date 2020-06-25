a865421111ee27549b0381839ee9116211572c41
rinatf <9-b-rinat@rambler.ru>
2020-06-25 22:49:18 +0300

    fix margin for descriptions

--- src/pages/Marketing/AlphaBlock/AlphaBlock.js
+++ src/pages/Marketing/AlphaBlock/AlphaBlock.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import cx from 'classnames'
 import { checkIsProState } from '../../../utils/account'
 import ProPriceDivergenceCard from '../ProTemplateCard/ProPriceDivergenceCard'
 import TokenDistributionCard from '../TokenDistributionCard/TokenDistributionCard'
@@ -8,7 +9,7 @@ import styles from './AlphaBlock.module.scss'
 const AlphaBlock = ({ classes = {}, isProSanbase }) => {
   return (
     <>
-      <div className={classes.description}>
+      <div className={cx(classes.description, styles.description)}>
         A growing collection of in-house strategies and new approaches to market
         analysis developed by the Santiment team. New Alphas added weekly!
       </div>
