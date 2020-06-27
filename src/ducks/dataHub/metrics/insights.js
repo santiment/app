0c21a5af5e7e68394bf98d16ea370ce15e38b64e
haritonasty <a.kharitonovaspb@gmail.com>
2020-06-27 16:05:09 +0300

    delete unused file

--- src/ducks/dataHub/metrics/insights.js
+++ src/ducks/dataHub/metrics/insights.js
@@ -1,16 +1,16 @@
-import { Metrics } from '../../SANCharts/data'
+import { Metric } from './index'
 
 export const Insights = {
-  [Metrics.topHoldersPercentOfTotalSupply.key]: [5618, 5637, 5647],
-  [Metrics.dev_activity.key]: [5639, 5637, 4644],
-  [Metrics.social_volume_total.key]: [5632, 3229, 2959],
-  [Metrics.mean_dollar_invested_age.key]: [5639, 5627, 5601],
-  [Metrics.exchange_balance.key]: [5639, 5623, 2959],
-  [Metrics.age_destroyed.key]: [5639, 5631, 5601],
-  [Metrics.transaction_volume.key]: [5639, 5253, 4991],
-  [Metrics.circulation.key]: [5359, 5253, 2691],
-  [Metrics.nvt.key]: [5627, 4260, 2043],
-  [Metrics.mvrv_usd.key]: [5643, 5639, 5620],
-  [Metrics.daily_active_addresses.key]: [5639, 5632, 4991],
-  [Metrics.network_growth.key]: [5639, 5627, 2959]
+  [Metric.topHoldersPercentOfTotalSupply.key]: [5618, 5637, 5647],
+  [Metric.dev_activity.key]: [5639, 5637, 4644],
+  [Metric.social_volume_total.key]: [5632, 3229, 2959],
+  [Metric.mean_dollar_invested_age.key]: [5639, 5627, 5601],
+  [Metric.exchange_balance.key]: [5639, 5623, 2959],
+  [Metric.age_destroyed.key]: [5639, 5631, 5601],
+  [Metric.transaction_volume.key]: [5639, 5253, 4991],
+  [Metric.circulation.key]: [5359, 5253, 2691],
+  [Metric.nvt.key]: [5627, 4260, 2043],
+  [Metric.mvrv_usd.key]: [5643, 5639, 5620],
+  [Metric.daily_active_addresses.key]: [5639, 5632, 4991],
+  [Metric.network_growth.key]: [5639, 5627, 2959]
 }
