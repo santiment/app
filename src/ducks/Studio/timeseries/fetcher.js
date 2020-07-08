9c7c75e8e66355712299581daa8a6c33c13f8474
rinatf <9-b-rinat@rambler.ru>
2020-07-08 14:19:22 +0300

    bug with not loaded dormant_circulation_365d

--- src/ducks/Studio/timeseries/fetcher.js
+++ src/ducks/Studio/timeseries/fetcher.js
@@ -69,7 +69,8 @@ const transformAliases = [
 export const getQuery = (metric, metricSettings) => {
   const { key, queryKey = key, withoutRoot } = metric
 
-  const metricFetcher = withoutRoot ? Fetcher[key] : Fetcher[queryKey]
+  const metricFetcher =
+    withoutRoot && Fetcher[key] ? Fetcher[key] : Fetcher[queryKey]
 
   if (!metricFetcher) return
 
