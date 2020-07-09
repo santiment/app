6cd8e8ef39bc1c97050cc3fe5d454659c6666b06
DmitriVanGuard <dimavanguard@gmail.com>
2020-07-09 21:09:08 +0300

    Basic user hook

---
+++ src/contexts/user/index.js
@@ -0,0 +1,53 @@
+import { useMemo } from 'react'
+import gql from 'graphql-tag'
+import { useState, useEffect } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import { client } from '../../index'
+
+export const USER_QUERY = gql`
+  {
+    currentUser {
+      id
+      username
+      email
+      avatarUrl
+      consentId
+      sanBalance
+      firstLogin
+      stripeCustomerId
+      marketingAccepted
+      privacyPolicyAccepted
+    }
+  }
+`
+
+export function updateUser (newUser) {
+  const { currentUser } = client.readQuery({
+    query: USER_QUERY
+  })
+
+  client.writeQuery({
+    query: USER_QUERY,
+    data: {
+      currentUser: newUser && {
+        ...currentUser,
+        ...newUser
+      }
+    }
+  })
+}
+
+export function useUser () {
+  const query = useQuery(USER_QUERY)
+
+  return useMemo(
+    () => {
+      const { loading, data } = query
+      return {
+        loading,
+        user: data && data.currentUser
+      }
+    },
+    [query]
+  )
+}
