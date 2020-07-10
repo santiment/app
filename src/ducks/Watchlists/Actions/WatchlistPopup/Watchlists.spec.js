0f24a5fc5aba12bad7074f67ed2489b433724edb
haritonasty <a.kharitonovaspb@gmail.com>
2020-07-10 08:00:18 +0300

    fix path in test

--- src/ducks/Watchlists/Actions/WatchlistPopup/Watchlists.spec.js
+++ src/ducks/Watchlists/Actions/WatchlistPopup/Watchlists.spec.js
@@ -1,4 +1,4 @@
-import { hasAssetById } from './WatchlistsPopup'
+import { hasAssetById } from '../../utils'
 
 describe('hasListItemsThisAssetById', () => {
   it('should return false if we dont have item in the list', () => {
