e4397dc68f29464d835f611e171f790dd4759f02
DmitriVanGuard <dimavanguard@gmail.com>
2020-07-10 16:09:36 +0300

    Cache watcher

--- src/stores/ui/index.js
+++ src/stores/ui/index.js
@@ -1,1 +1,18 @@
-export function useIsLoggedIn () {}
+import { useState, useEffect } from 'react'
+import { USER_QUERY } from '../user'
+import { client } from '../../index'
+
+export function useIsLoggedIn () {
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(
+    () =>
+      client.cache.watch({
+        query: USER_QUERY,
+        callback: ({ result: { currentUser } }) => setIsLoggedIn(!!currentUser)
+      }),
+    []
+  )
+
+  return isLoggedIn
+}
