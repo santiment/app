5f89e9f5b5d933dc1c24e098d2bb88559ff4b81b
Rinat F <rinatficht@gmail.com>
2020-08-10 10:03:17 +0300

    add links to project's site to transaction label

--- src/components/WalletLink/TransactionTableLabels.js
+++ src/components/WalletLink/TransactionTableLabels.js
@@ -1,23 +1,69 @@
 import React from 'react'
+import cx from 'classnames'
 import Label from '@santiment-network/ui/Label'
 import styles from './TransactionTableLabels.module.scss'
 
-const LabelWrapper = ({ name }) => (
-  <Label className={styles.label}>{name}</Label>
-)
+const HARDCODED_LINKS = {
+  defisaver: 'https://defisaver.com/',
+  pooltogether: 'https://www.pooltogether.com/',
+  dydx: 'https://dydx.exchange/',
+  compound: 'https://compound.finance/',
+  bzx: 'https://bzx.network/',
+  defizap: 'https://defizap.com/',
+  makerdao: 'https://makerdao.com/ru/',
+  iearn: 'https://yearn.finance/',
+  defimoneymarket: 'https://defimoneymarket.com/',
+  chai: 'https://chai.money/',
+  tokensets: 'https://www.tokensets.com/',
+  eidoo: 'https://eidoo.io/',
+  aave: 'https://aave.com/',
+  celsius: 'https://celsius.network/',
+  nexo: 'https://nexo.io/',
+  nuonetwork: 'https://www.nuo.network/',
+  synthetix: 'https://www.synthetix.io/',
+  instadapp: 'https://instadapp.io/',
+  wbtc: 'https://wbtc.network/'
+}
 
-const LabelRenderer = ({ name, metadata }) => {
+const LabelWrapper = ({ metadata }) => {
+  if (!metadata) {
+    return null
+  }
+
   const decoded = JSON.parse(metadata)
   const { owner } = decoded
+
+  const linkRef = HARDCODED_LINKS[owner]
+
+  if (linkRef) {
+    return (
+      <a
+        className={cx(styles.label, styles.link)}
+        target='_blank'
+        rel='noopener noreferrer'
+        href={linkRef}
+        onClick={e => {
+          e.stopPropagation()
+        }}
+      >
+        {owner}
+      </a>
+    )
+  } else {
+    return <Label className={styles.label}>{owner}</Label>
+  }
+}
+
+const LabelRenderer = ({ name, metadata }) => {
   switch (name) {
     case 'decentralized_exchange': {
-      return <LabelWrapper name={owner} />
+      return <LabelWrapper metadata={metadata} />
     }
     case 'centralized_exchange': {
-      return <LabelWrapper name={owner} />
+      return <LabelWrapper metadata={metadata} />
     }
     case 'defi': {
-      return <LabelWrapper name={owner} />
+      return <LabelWrapper metadata={metadata} />
     }
     default: {
       return null
@@ -25,10 +71,7 @@ const LabelRenderer = ({ name, metadata }) => {
   }
 }
 
-const TransactionTableLabels = ({ labels }) => {
-  console.log('labels', labels)
-
-  return labels.map((item, index) => <LabelRenderer key={index} {...item} />)
-}
+const TransactionTableLabels = ({ labels }) =>
+  labels.map((item, index) => <LabelRenderer key={index} {...item} />)
 
 export default TransactionTableLabels
