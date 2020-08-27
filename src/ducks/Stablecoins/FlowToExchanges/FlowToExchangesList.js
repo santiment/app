9347988074e84fe238dd67def1870d67b017c077
Rinat F <rinatficht@gmail.com>
2020-08-27 16:51:53 +0300

    add tabs for flow to exchanges

--- src/ducks/Stablecoins/FlowToExchanges/FlowToExchangesList.js
+++ src/ducks/Stablecoins/FlowToExchanges/FlowToExchangesList.js
@@ -1,14 +1,63 @@
-import React from 'react'
-import { ExchangesAssets } from './utils'
+import React, { useMemo, useState } from 'react'
+import Tabs from '@santiment-network/ui/Tabs'
+import { EXCHANGES_DEFAULT_SETTINGS, useFlowToExchanges } from './utils'
 import FlowToExchanges from './FlowToExchanges'
+import PageLoader from '../../../components/Loader/PageLoader'
 import styles from './FlowToExchangesList.module.scss'
 
+const GROUPS = {
+  'Centralized Exchanges': { key: 'exchange_inflow_centralized' },
+  'Decentralized Exchanges': { key: 'exchange_inflow_decentralized' }
+}
+
+const TABS = Object.keys(GROUPS)
+
 const FlowToExchangesList = () => {
+  const [tab, setTab] = useState(TABS[0])
+  const { data, loading } = useFlowToExchanges(EXCHANGES_DEFAULT_SETTINGS)
+
+  const key = useMemo(
+    () => {
+      const { key } = GROUPS[tab]
+      return key
+    },
+    [data, tab]
+  )
+
+  const prepared = useMemo(
+    () => {
+      console.log('calculate prepared', key)
+      return data
+        .filter(item => item[key] !== null)
+        .map(item => ({
+          ...item,
+          value: item[key]
+        }))
+    },
+    [data, key]
+  )
+
+  console.log('prepared', prepared)
+
   return (
     <div className={styles.container}>
-      {ExchangesAssets.map((item, index) => (
-        <FlowToExchanges item={item} key={index} />
-      ))}
+      <Tabs
+        className={styles.tabs}
+        options={TABS}
+        defaultSelectedIndex={tab}
+        onSelect={tab => setTab(tab)}
+        classes={styles}
+      />
+
+      {!loading && (
+        <div className={styles.list}>
+          {prepared.map((item, index) => (
+            <FlowToExchanges item={item} key={index} />
+          ))}
+        </div>
+      )}
+
+      {loading && <PageLoader />}
     </div>
   )
 }
