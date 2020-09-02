9ee5c76618f095c81683756c92eb293eea84144b
DmitriVanGuard <dimavanguard@gmail.com>
2020-09-02 12:37:25 +0300

    Explanation note

--- src/ducks/dataHub/tooltipSettings.js
+++ src/ducks/dataHub/tooltipSettings.js
@@ -23,6 +23,7 @@ export function FORMATTER (value) {
     return 'No data'
   }
 
+  // NOTE: Handling float type 0's (e.g. "0.0000") [@vanguard | Sep  2, 2020]
   if (value === 0) {
     return 0
   }
