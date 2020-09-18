38dbf2f3b83ced552a726f14e2ba26976f256cf1
DmitriVanGuard <dimavanguard@gmail.com>
2020-09-18 13:07:44 +0300

    Blockchain specific request

--- src/ducks/Studio/Chart/MetricSettings/ExchangeSetting/hooks.js
+++ src/ducks/Studio/Chart/MetricSettings/ExchangeSetting/hooks.js
@@ -1,5 +1,16 @@
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
+import { SUPPORTED_BLOCKCHAIN_SLUGS } from './exchanges'
+
+export const PROJECT_QUERY = gql`
+  query($slug: String!) {
+    project: projectBySlug(slug: $slug) {
+      id
+      infrastructure
+      mainContractAddress
+    }
+  }
+`
 
 const METRIC_EXCHANGES_QUERY = gql`
   query($slug: String!, $isDex: Boolean) {
@@ -10,9 +21,29 @@ const METRIC_EXCHANGES_QUERY = gql`
 export const DEFAULT_EXCHANGE = 'All (CEX+DEX)'
 const DEFAULT_EXCHANGES = [DEFAULT_EXCHANGE]
 
+function useIsERC20 (slug) {
+  const { data } = useQuery(PROJECT_QUERY, {
+    variables: {
+      slug
+    }
+  })
+
+  if (!data) return false
+
+  const { mainContractAddress, infrastructure } = data.project
+  return mainContractAddress && infrastructure === 'ETH'
+}
+
 export function useMetricExchanges (slug, isDex) {
+  const isERC20 = useIsERC20(slug)
+  const isSupported = SUPPORTED_BLOCKCHAIN_SLUGS.has(slug)
+
   const { data, loading } = useQuery(METRIC_EXCHANGES_QUERY, {
-    variables: { slug, isDex }
+    variables: {
+      slug: isSupported ? slug : 'ethereum',
+      isDex
+    },
+    skip: !isSupported && !isERC20
   })
 
   return {
