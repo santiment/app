d866e187f5880b1ee922dc81da365f90a7a4f90f
Nastya Kharitonova <a.kharitonovaspb@gmail.com>
2020-09-18 14:22:59 +0300

    add real api

--- src/components/ClaimersWidgets/TopClaimers/TopClaimersTable.js
+++ src/components/ClaimersWidgets/TopClaimers/TopClaimersTable.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactTable from 'react-table'
 import { columns } from './columns'
-// import { useTopClaimers } from './gql'
+import { useTopClaimers } from './gql'
 import { DAY, getTimeIntervalFromToday } from '../../../utils/dates'
 import styles from '../index.module.scss'
 import tableStyles from './table.module.scss'
@@ -15,20 +15,11 @@ const DEFAULT_SORTED = [
 
 const TopClaimersTable = ({ className }) => {
   const { from, to } = getTimeIntervalFromToday(-1, DAY)
-  // const [data, loading] = useTopClaimers({from: from.toISOString(), to: to.toISOString()})
-
-  // console.log(data)
-
-  const harcoded = [
-    {
-      address: '0xdb08817d917a0ce2c4fd7370f4b9b44c937b4ef1',
-      value: 1944444.455
-    },
-    {
-      address: '0xebabde62e507feca42dbd3e9ce2d2598b1a589cb',
-      value: 4584329.4443
-    }
-  ]
+  const [items, loading] = useTopClaimers({
+    from: from.toISOString(),
+    to: to.toISOString()
+  })
+  console.log(items)
 
   return (
     <div className={className}>
@@ -38,7 +29,7 @@ const TopClaimersTable = ({ className }) => {
         defaultSorted={DEFAULT_SORTED}
         showPagination={false}
         resizable={false}
-        data={harcoded}
+        data={items}
         columns={columns}
       />
     </div>
