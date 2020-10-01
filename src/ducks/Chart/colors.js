fca6cd31b4859f889819df1ff0b78e020812cab4
Rinat F <rinatficht@gmail.com>
2020-10-01 17:21:23 +0300

    feat: add additional colors

--- src/ducks/Chart/colors.js
+++ src/ducks/Chart/colors.js
@@ -22,7 +22,16 @@ export const COLORS = [
   '#FFDAC5', // PEACH
   '#37D7BA', // AQUAMARINE
   '#777777', // GREY
-  '#222222' // BLACK
+  '#222222', // BLACK
+
+  '#14c393', // jungle-green,
+  '#7a859e', // waterloo
+  '#174d4a', // jungle-green-dark-3
+  '#ffe7ca', // texas-rose-light-2
+  '#efa7a7', // persimmon-light-2
+  '#dcf6ef', // jungle-green-light-2,
+  '#3b3130', // texas-rose-dark,
+  '#c9c2ff' // heliotrope-light-2
 ]
 
 export const COLORS_SET = new Set(COLORS)
