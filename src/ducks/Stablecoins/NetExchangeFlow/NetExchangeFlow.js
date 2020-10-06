75e7a2f2aeb652a18297f95b3dff4dd33fc46cb2
Rinat F <rinatficht@gmail.com>
2020-10-06 16:43:59 +0300

    chore: self review

--- src/ducks/Stablecoins/NetExchangeFlow/NetExchangeFlow.js
+++ src/ducks/Stablecoins/NetExchangeFlow/NetExchangeFlow.js
@@ -1,30 +1,26 @@
-import React, { useMemo } from 'react'
+import React from 'react'
 import DashboardMetricChart from '../../../components/DashboardMetricChart/DashboardMetricChart'
 import { DEFAULT_INTERVAL_SELECTORS } from '../../../components/DashboardMetricChart/utils'
 import { Metric } from '../../dataHub/metrics'
 
-const NetExchangeFlow = ({}) => {
-  const metrics = useMemo(() => {
-    return [
-      {
-        ...Metric.exchange_inflow,
-        reqMeta: { market_segments: 'Stablecoin USD' },
-        domainGroup: 'stablecoins'
-      },
-      {
-        ...Metric.exchange_outflow,
-        reqMeta: { market_segments: 'Stablecoin USD' },
-        domainGroup: 'stablecoins'
-      }
-    ]
-  }, [])
+const METRICS = [
+  {
+    ...Metric.exchange_inflow,
+    reqMeta: { market_segments: 'Stablecoin USD' },
+    domainGroup: 'stablecoins'
+  },
+  {
+    ...Metric.exchange_outflow,
+    reqMeta: { market_segments: 'Stablecoin USD' },
+    domainGroup: 'stablecoins'
+  }
+]
 
-  return (
-    <DashboardMetricChart
-      metrics={metrics}
-      intervals={DEFAULT_INTERVAL_SELECTORS}
-    />
-  )
-}
+const NetExchangeFlow = () => (
+  <DashboardMetricChart
+    metrics={METRICS}
+    intervals={DEFAULT_INTERVAL_SELECTORS}
+  />
+)
 
 export default NetExchangeFlow
