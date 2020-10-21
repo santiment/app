504cfb199dc7469a83ab9c0a1ee15d591b83e6f3
Nastya Kharitonova <a.kharitonovaspb@gmail.com>
2020-10-21 09:05:58 +0300

    upd unsub page

---
+++ src/components/Illustrations/Success.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Success = ({ className }) => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='80'
+    height='80'
+    className={className}
+    fill='none'
+  >
+    <path
+      fill='var(--jungle-green-light-2)'
+      d='M19.084 62.571C35.42 39.7 3.217 55.43.457 40.483c-6.254-33.878 76.04-31.591 72.306-5.452-3.734 26.14-70.016 50.412-53.68 27.54z'
+    />
+    <path
+      fill='var(--jungle-green-light-3)'
+      fillRule='evenodd'
+      d='M40 68.5c15.74 0 28.5-12.76 28.5-28.5S55.74 11.5 40 11.5 11.5 24.26 11.5 40 24.26 68.5 40 68.5zm0 1.5c16.569 0 30-13.431 30-30 0-16.569-13.431-30-30-30-16.569 0-30 13.431-30 30 0 16.569 13.431 30 30 30z'
+      clipRule='evenodd'
+    />
+    <path
+      fill='var(--jungle-green)'
+      d='M35.745 53.67l-8.35-9.186a1.482 1.482 0 012.192-1.994l6.967 7.657L52.04 29.822a2 2 0 012.762-.41l.42.304a2 2 0 01.337 2.936l-18.32 21.002a1 1 0 01-1.494.015z'
+    />
+  </svg>
+)
+
+export default Success
