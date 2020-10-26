127eb5217c36ba96e49fed2dd8fe85734a818500
Rinat F <rinatficht@gmail.com>
2020-10-26 17:59:17 +0300

    fix(compare): filter search by predicate

--- src/ducks/Studio/Compare/Comparable/Metric.js
+++ src/ducks/Studio/Compare/Comparable/Metric.js
@@ -3,7 +3,7 @@ import cx from 'classnames'
 import Icon from '@santiment-network/ui/Icon'
 import withMetrics from '../../withMetrics'
 import { getCategoryGraph } from '../../Sidebar/utils'
-import Search, { getMetricSuggestions } from '../../Sidebar/Search'
+import Search, { checkMatch, getMetricSuggestions } from '../../Sidebar/Search'
 import MetricIcon from '../../../SANCharts/MetricIcon'
 import { METRIC } from '../../Sidebar/Button/types'
 import styles from './Metric.module.scss'
@@ -17,35 +17,34 @@ const CustomProjectCategories = {
 }
 
 export const SEARCH_PREDICATE_ONLY_METRICS = searchTerm => {
-  const upperCaseSearchTerm = searchTerm.toUpperCase()
+  const upperCaseSearchTerm = searchTerm ? searchTerm.toUpperCase() : ''
   return ({ label, abbreviation, type }) => {
     if (type && type !== METRIC) {
       return false
     }
 
-    return (
-      (abbreviation &&
-        abbreviation.toUpperCase().includes(upperCaseSearchTerm)) ||
-      (label && label.toUpperCase().includes(upperCaseSearchTerm))
-    )
+    return checkMatch(upperCaseSearchTerm, abbreviation, label)
   }
 }
 
 const MetricSearch = withMetrics(
-  ({ slug, categories, loading, className, ...rest }) => (
-    <Search
-      {...rest}
-      searchPredicate={SEARCH_PREDICATE_ONLY_METRICS}
-      className={cx(className, loading && styles.loading)}
-      categories={CustomProjectCategories[slug] || categories}
-      emptySuggestions={getMetricSuggestions({
-        categories: CustomProjectCategories[slug] || categories
-      })}
-      inputProps={{
-        placeholder: 'Type to search metrics...'
-      }}
-    />
-  )
+  ({ slug, categories, loading, className, ...rest }) => {
+    return (
+      <Search
+        {...rest}
+        searchPredicate={SEARCH_PREDICATE_ONLY_METRICS}
+        className={cx(className, loading && styles.loading)}
+        categories={CustomProjectCategories[slug] || categories}
+        emptySuggestions={getMetricSuggestions({
+          categories: CustomProjectCategories[slug] || categories,
+          predicate: SEARCH_PREDICATE_ONLY_METRICS
+        })}
+        inputProps={{
+          placeholder: 'Type to search metrics...'
+        }}
+      />
+    )
+  }
 )
 
 const Label = ({ comparable, editMetric, colors }) => {
