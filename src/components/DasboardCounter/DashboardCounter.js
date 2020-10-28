cdc52fe5276ca007d1cee99a172eb712c9c30184
Rinat F <rinatficht@gmail.com>
2020-10-28 12:33:38 +0300

    feat: add skeleton for dashboard cards

--- src/components/DasboardCounter/DashboardCounter.js
+++ src/components/DasboardCounter/DashboardCounter.js
@@ -1,25 +1,36 @@
 import React from 'react'
+import cx from 'classnames'
 import Loader from '@santiment-network/ui/Loader/Loader'
 import { formatNumber } from '../../utils/formatting'
+import { Skeleton } from '../Skeleton'
 import styles from './DashboardCounter.module.scss'
 
 const DashboardCounter = ({
   formatter = formatNumber,
   loadings,
   value,
-  title
+  title,
+  classes = {}
 }) => {
   return (
-    <div className={styles.card}>
-      <div className={styles.title}>{title}</div>
-      <div className={styles.value}>
-        {loadings ? (
-          <Loader className={styles.loading} />
-        ) : (
-          formatter(value.toFixed(2))
-        )}
-      </div>
-    </div>
+    <>
+      <Skeleton
+        show={loadings}
+        className={cx(styles.skeleton, classes.skeleton)}
+      />
+      {!loadings && (
+        <div className={styles.card}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.value}>
+            {loadings ? (
+              <Loader className={styles.loading} />
+            ) : (
+              formatter(value.toFixed(2))
+            )}
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 
