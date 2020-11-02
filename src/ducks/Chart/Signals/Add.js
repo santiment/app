eb40cb3729c101d2a26a586d4f1f59b11baf1eaa
DmitriVanGuard <dimavanguard@gmail.com>
2020-11-02 15:51:30 +0300

    Displaying alert box on the right if its not contained in the viewport

--- src/ducks/Chart/Signals/Add.js
+++ src/ducks/Chart/Signals/Add.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import Alerts from '../../Studio/Alerts'
 import styles from './index.module.scss'
 
-export default ({ hoverPoint, ...rest }) => (
-  <div className={styles.add} style={{ '--top': hoverPoint.y + 'px' }}>
-    <Alerts {...rest} {...hoverPoint} className={styles.alerts} />
-  </div>
-)
+export default ({ hoverPoint, ...rest }) => {
+  const containerRef = useRef(null)
+
+  useEffect(
+    () => {
+      const container = containerRef.current
+
+      if (container && container.getBoundingClientRect().x < -15) {
+        container.classList.add(styles.alerts_right)
+      }
+    },
+    [containerRef]
+  )
+
+  return (
+    <div className={styles.add} style={{ '--top': hoverPoint.y + 'px' }}>
+      <Alerts
+        {...rest}
+        {...hoverPoint}
+        className={styles.alerts}
+        containerRef={containerRef}
+      />
+    </div>
+  )
+}
