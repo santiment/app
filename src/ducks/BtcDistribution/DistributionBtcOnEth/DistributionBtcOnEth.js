c40898cc023f41f57792dbd18d7ef9e434d67c5c
haritonasty <a.kharitonovaspb@gmail.com>
2020-11-04 15:35:47 +0300

    change the last one

--- src/ducks/BtcDistribution/DistributionBtcOnEth/DistributionBtcOnEth.js
+++ src/ducks/BtcDistribution/DistributionBtcOnEth/DistributionBtcOnEth.js
@@ -8,7 +8,7 @@ import { Metric } from '../../dataHub/metrics'
 
 function makeMetric (slug) {
   return {
-    key: `total_supply_${slug.replace('-', '_')}`,
+    key: `total_supply_${slug.replaceAll('-', '_')}`,
     queryKey: 'total_supply',
     label: slug,
     node: 'area',
@@ -22,7 +22,7 @@ function makeMetric (slug) {
 export const BTC_RELATED_ASSETS = [
   'wrapped-bitcoin',
   'renbtc',
-  'imBTC',
+  'the-tokenized-bitcoin',
   'sbtc',
   'tbtc',
   'huobi-btc',
