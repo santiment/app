200ca2a04c074cc45a0c2139c40d86e3a82a9e31
DmitriVanGuard <dimavanguard@gmail.com>
2020-11-11 16:24:39 +0300

    Adding fiat tabs

--- src/ducks/Studio/Sidebar/ProjectSelector/index.js
+++ src/ducks/Studio/Sidebar/ProjectSelector/index.js
@@ -1,8 +1,20 @@
 import React, { useState } from 'react'
 import Icon from '@santiment-network/ui/Icon'
 import ProjectSelectDialog from '../../Compare/ProjectSelectDialog'
+import { DEFAULT_TABS } from '../../Compare/ProjectSelectTabs'
+import { FIAT_MARKET_ASSETS } from '../../../dataHub/fiat'
 import ProjectIcon from '../../../../components/ProjectIcon/ProjectIcon'
 import styles from './index.module.scss'
+
+const CUSTOM_CATEGORY = {
+  Fiat: () => Promise.resolve(FIAT_MARKET_ASSETS)
+}
+
+const CUSTOM_TABS = DEFAULT_TABS.concat(Object.keys(CUSTOM_CATEGORY))
+
+const CategoryModifier = {
+  All: assets => assets.concat(FIAT_MARKET_ASSETS)
+}
 
 const Selector = ({ slug, name, onClick }) => (
   <div className={styles.selector} onClick={onClick}>
@@ -36,6 +48,9 @@ const ProjectSelector = ({ project: { slug, name }, onProjectSelect }) => {
       onClose={closeDialog}
       onSelect={onSelect}
       trigger={<Selector slug={slug} name={name} onClick={openDialog} />}
+      customTabs={CUSTOM_TABS}
+      CustomCategory={CUSTOM_CATEGORY}
+      CategoryModifier={CategoryModifier}
     />
   )
 }
