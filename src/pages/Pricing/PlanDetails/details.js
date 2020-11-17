b4636511456ec014847f4d88055ca96f54386a9f
Rinat F <rinatficht@gmail.com>
2020-11-17 15:46:58 +0300

    feat: update PRO+ plan

--- src/pages/Pricing/PlanDetails/details.js
+++ src/pages/Pricing/PlanDetails/details.js
@@ -5,20 +5,31 @@ export default {
       data: [
         {
           name: 'Historical data access',
-          texts: ['Last 3 months', 'Last 6 months']
+          texts: ['Last 3 months', 'Last 6 months', '10K API calls / mo']
         },
         {
           name: 'API calls / minute',
-          texts: ['20 API calls / min', '60 API calls / min']
+          texts: [
+            '20 API calls / min',
+            '60 API calls / min',
+            '120 API calls / min'
+          ]
         },
         {
           name: 'API calls / month',
-          texts: ['5K API calls / mo', '10K API calls / mo']
+          texts: [
+            '5K API calls / mo',
+            '10K API calls / mo',
+            '300K API calls / mo'
+          ]
         },
-        { name: 'Type of metrics', texts: ['Standard', 'Standard'] },
+        {
+          name: 'Type of metrics',
+          texts: ['Standard', 'Standard', 'Advanced']
+        },
         {
           name: 'Attribution',
-          texts: ['Attribution required', 'No attribution']
+          texts: ['Attribution required', 'No attribution', 'No attribution']
         }
       ]
     },
@@ -39,24 +50,33 @@ export default {
         { name: 'Exchange flow' },
         { name: 'Total ERC20 exchange funds flow' },
         { name: 'Transaction volume' },
-        { name: 'Total circulation (beta)', checks: [false, true] },
-        { name: 'Velocity of tokens (beta)', checks: [false, true] },
-        { name: 'ETH gas used', checks: [false, true] },
-        { name: 'Distribution between mining pools', checks: [false, true] },
-        { name: 'Top holders percent of total supply', checks: [false, true] },
-        { name: 'Percent of total supply on exchanges', checks: [false, true] },
+        { name: 'Total circulation (beta)', checks: [false, true, true] },
+        { name: 'Velocity of tokens (beta)', checks: [false, true, true] },
+        { name: 'ETH gas used', checks: [false, true, true] },
+        {
+          name: 'Distribution between mining pools',
+          checks: [false, true, true]
+        },
+        {
+          name: 'Top holders percent of total supply',
+          checks: [false, true, true]
+        },
+        {
+          name: 'Percent of total supply on exchanges',
+          checks: [false, true, true]
+        },
         {
           name: 'Realized value',
-          checks: [false, false]
+          checks: [false, false, true]
         },
         {
           name: 'MVRV ratio',
-          checks: [false, false]
+          checks: [false, false, true]
         },
-        { name: 'NVT', checks: [false, false] },
+        { name: 'NVT', checks: [false, false, true] },
         {
           name: 'Daily active deposits',
-          checks: [false, false]
+          checks: [false, false, true]
         }
       ]
     },
@@ -65,8 +85,8 @@ export default {
       data: [
         { name: 'Dev activity' },
         { name: 'Topic search' },
-        { name: 'Relative social dominance', checks: [false, true] },
-        { name: 'Total social volume', checks: [false, true] }
+        { name: 'Relative social dominance', checks: [false, true, true] },
+        { name: 'Total social volume', checks: [false, true, true] }
       ]
     }
   ]
