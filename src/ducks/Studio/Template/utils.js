e0ce174024ea55752c41f0944a4419b507714ab6
DmitriVanGuard <dimavanguard@gmail.com>
2020-11-27 15:08:06 +0300

    warning fix

--- src/ducks/Studio/Template/utils.js
+++ src/ducks/Studio/Template/utils.js
@@ -66,7 +66,7 @@ export function parseTemplateMetrics (templateMetrics, project) {
         try {
           return getProjectMetricByKey(key, COMPARE_CONNECTOR)
         } catch (e) {
-          return
+          return undefined
         }
       }
 
