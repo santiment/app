89723017aa22b66012084e1f7232fc1df7910fd6
haritonasty <a.kharitonovaspb@gmail.com>
2020-12-01 13:11:04 +0300

    prettify styles

--- src/ducks/Watchlists/Widgets/Table/AssetCard.js
+++ src/ducks/Watchlists/Widgets/Table/AssetCard.js
@@ -18,33 +18,42 @@ const AssetCard = ({
   onAssetClick,
   priceChart7d,
   className
-}) => (
-  <Link
-    className={cx(styles.wrapper, className)}
-    to={`/projects/${slug}`}
-    onClick={onAssetClick}
-  >
-    <div className={styles.left}>
-      <div className={styles.icon}>
-        <ProjectIcon size={20} slug={slug} />
-      </div>
-      <div className={styles.row}>
-        <div className={styles.name}>
-          {capitalizeStr(slug)}{' '}
-          <span className={styles.ticker}>{ticker.toUpperCase()}</span>
+}) => {
+  const includeFloatPart = priceUsd > 99999 ? 0 : 2
+
+  return (
+    <Link
+      className={cx(styles.wrapper, className)}
+      to={`/projects/${slug}`}
+      onClick={onAssetClick}
+    >
+      <div className={styles.left}>
+        <div className={styles.icon}>
+          <ProjectIcon size={20} slug={slug} />
         </div>
-        <div>$ {millify(marketcapUsd, 2)}</div>
+        <div className={styles.row}>
+          <div className={styles.name}>{name}</div>
+          <div>
+            ${millify(marketcapUsd, 0)}{' '}
+            <span className={styles.ticker}>{ticker.toUpperCase()}</span>
+          </div>
+        </div>
       </div>
-    </div>
-    <PriceGraph data={priceChart7d} />
-    <div className={styles.right}>
-      {priceUsd ? formatNumber(priceUsd, { currency: 'USD' }) : 'No data'}
-      <PercentChanges
-        className={styles.percentChanges}
-        changes={percentChange24h}
-      />
-    </div>
-  </Link>
-)
+      <PriceGraph data={priceChart7d} className={styles.chart} width={80} />
+      <div className={styles.right}>
+        {priceUsd
+          ? formatNumber(priceUsd, {
+            currency: 'USD',
+            minimumFractionDigits: includeFloatPart ? 2 : 0
+          })
+          : 'No data'}
+        <PercentChanges
+          className={styles.percentChanges}
+          changes={percentChange24h}
+        />
+      </div>
+    </Link>
+  )
+}
 
 export default AssetCard
