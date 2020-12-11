c15709f34d191b6654d596ea0bd88229138b0f50
Rinat F <rinatficht@gmail.com>
2020-12-11 13:16:45 +0300

    chore: self review

--- src/components/Navbar/NavbarMore/NavbarMore.js
+++ src/components/Navbar/NavbarMore/NavbarMore.js
@@ -6,34 +6,41 @@ import { MAIN_PRODUCTS } from '../SantimentProductsTooltip/Products'
 import LinkWithArrow from '../Link'
 import externalStyles from '../Watchlists/MarketDropdown.module.scss'
 import styles from './NavbarMore.module.scss'
-import { Link } from 'react-router-dom'
 
 const NavbarMore = ({ activeLink, links }) => {
   return (
     <Panel>
       <div className={externalStyles.wrapper}>
         <div className={cx(externalStyles.block, externalStyles.list)}>
-          {links.map((item, index) => (
-            <Button
-              key={index}
-              fluid
-              variant='ghost'
-              isActive={item.link === activeLink}
-              {...item}
-              className={externalStyles.btn}
-            />
-          ))}
+          <div className={styles.leftBlock}>
+            {links.map((item, index) => (
+              <Button
+                key={index}
+                fluid
+                variant='ghost'
+                isActive={item.link === activeLink}
+                {...item}
+                className={externalStyles.btn}
+              />
+            ))}
+          </div>
         </div>
         <div className={externalStyles.block}>
           <h3 className={externalStyles.title}>Santiment Products</h3>
           <div className={styles.products}>
             {MAIN_PRODUCTS.map(({ img, title, to }, index) => {
               return (
-                <Link to={to} key={index} className={styles.product}>
+                <a
+                  href={to}
+                  key={index}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={styles.product}
+                >
                   <img src={img} alt={title} className={styles.productImg} />
 
                   {title}
-                </Link>
+                </a>
               )
             })}
           </div>
