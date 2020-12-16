34953e7a4630cd47bdb202d08a346a8b3931190b
DmitriVanGuard <dimavanguard@gmail.com>
2020-12-16 14:43:03 +0300

    Indices hardcoded sort

--- src/pages/Index/Aside/index.js
+++ src/pages/Index/Aside/index.js
@@ -4,7 +4,14 @@ import Recents from './Recents'
 import Categories from '../../../ducks/SANCharts/Categories'
 import styles from './index.module.scss'
 
-const sortWatchlists = ({ name: a }, { name: b }) => a.length - b.length
+const WatchlistIdOrder = {}
+const WATCHLIST_IDS_ORDER = [5496, 5497, 2046, 86, 749, 127, 272]
+WATCHLIST_IDS_ORDER.forEach((id, i) => {
+  WatchlistIdOrder[id] = i
+})
+
+const sortWatchlists = ({ id: a }, { id: b }) =>
+  WatchlistIdOrder[a] - WatchlistIdOrder[b]
 const sorter = watchlists => watchlists.slice().sort(sortWatchlists)
 
 const Aside = ({ className }) => (
