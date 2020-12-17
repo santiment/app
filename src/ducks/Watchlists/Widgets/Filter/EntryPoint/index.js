ddd557dc4ddd75b584c7eb813c06c4c469739916
haritonasty <a.kharitonovaspb@gmail.com>
2020-12-17 18:02:12 +0300

    updates

--- src/ducks/Watchlists/Widgets/Filter/EntryPoint/index.js
+++ src/ducks/Watchlists/Widgets/Filter/EntryPoint/index.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Panel from '@santiment-network/ui/Panel'
+import Search from '@santiment-network/ui/Search'
+import Message from '@santiment-network/ui/Message'
 import ContextMenu from '@santiment-network/ui/ContextMenu'
 import { InputWithIcon as Input } from '@santiment-network/ui/Input'
-import Panel from '@santiment-network/ui/Panel'
 import styles from './index.module.scss'
 
+const ALL_ASSETS_TEXT = 'All assets'
+
+const MESSAGES = {
+  allAssets: 'Deselect ‘All assets’ to customize your Screener',
+  empty: 'Please select at least one filter'
+}
+
 const EntryPoint = ({ baseProjects }) => {
-  const state = baseProjects || 'All assets'
+  const state = baseProjects || ALL_ASSETS_TEXT
+
+  const [currentSearch, setCurrentSearch] = useState('')
+  const [message, setMessage] = useState(
+    state === ALL_ASSETS_TEXT ? MESSAGES.allAssets : ''
+  )
 
   return (
     <div className={styles.wrapper}>
@@ -29,7 +43,26 @@ const EntryPoint = ({ baseProjects }) => {
           />
         }
       >
-        <Panel className={styles.panel} />
+        <Panel className={styles.panel}>
+          <Search
+            autoFocus
+            onChange={value => setCurrentSearch(value)}
+            placeholder='Type to search'
+            className={styles.search}
+          />
+          <div className={styles.selected}>a;;</div>
+          {message && (
+            <Message
+              variant='warn'
+              icon='info-round'
+              fill={false}
+              className={styles.message}
+            >
+              {message}
+            </Message>
+          )}
+          <div className={styles.watchlists}>fff</div>
+        </Panel>
       </ContextMenu>
     </div>
   )
