9a551bf6c644e17e0c479d4dec1adb6564f8a750
DmitriVanGuard <dimavanguard@gmail.com>
2020-12-18 14:44:03 +0300

    Recent chart layout

---
+++ src/pages/Index/Aside/Recents/ChartLayouts.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import gql from 'graphql-tag'
+import Recent, { getItemBuilder, Column } from './Recent'
+import { getSEOLinkFromIdAndTitle } from '../../../../components/Insight/utils'
+
+const getLink = ({ id, title }) =>
+  `/studio/${id && getSEOLinkFromIdAndTitle(id, title)}`
+const getItem = getItemBuilder(gql`
+  query chartConfiguration($id: Int!) {
+    item: chartConfiguration(id: $id) {
+      id
+      title
+    }
+  }
+`)
+
+const ChartLayout = ({ title }) => (title ? <Column>{title}</Column> : null)
+
+const ChartLayouts = ({ ids }) => (
+  <Recent
+    title='Chart Layouts'
+    rightHeader=''
+    ids={ids}
+    getItem={getItem}
+    getLink={getLink}
+    Item={ChartLayout}
+  />
+)
+
+export default ChartLayouts
--- src/pages/Index/Aside/Recents/index.js
+++ src/pages/Index/Aside/Recents/index.js
@@ -1,26 +1,41 @@
 import React, { useMemo } from 'react'
 import Assets from './Assets'
+import ChartLayouts from './ChartLayouts'
 import Watchlists from './Watchlists'
 import Section from '../Section'
 import {
   getRecentAssets,
   getRecentWatchlists,
-  getRecentScreeners
+  getRecentScreeners,
+  getRecentTemplates
 } from '../../../../utils/recent'
 
-const Recents = () => {
-  const [assets, watchlists, screeners] = useMemo(
-    () => [getRecentAssets(), getRecentWatchlists(), getRecentScreeners()],
-    []
-  )
+const RECENT_GETTERS = [
+  getRecentAssets,
+  () => getRecentTemplates().map(id => +id),
+  getRecentWatchlists,
+  getRecentScreeners
+]
+const recentGetter = getter => getter()
+const getRecents = () => RECENT_GETTERS.map(recentGetter)
 
-  if (assets.length + watchlists.length + screeners.length === 0) {
+const Recents = () => {
+  const [assets, chartLayouts, watchlists, screeners] = useMemo(getRecents, [])
+
+  if (
+    assets.length +
+      chartLayouts.length +
+      watchlists.length +
+      screeners.length ===
+    0
+  ) {
     return null
   }
 
   return (
-    <Section title='Recents'>
+    <Section title='Recently viewed'>
       {!!assets.length && <Assets slugs={assets} />}
+      {!!chartLayouts.length && <ChartLayouts ids={chartLayouts} />}
       {!!watchlists.length && <Watchlists ids={watchlists} />}
       {!!screeners.length && <Watchlists title='Screeners' ids={screeners} />}
     </Section>
