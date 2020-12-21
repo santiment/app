d0acebcb7dbc97d6e78ab5f00c66bf17dcc17cf7
DmitriVanGuard <dimavanguard@gmail.com>
2020-12-21 18:15:47 +0300

    Page layout mobile search icon link

--- src/ducks/Page/index.js
+++ src/ducks/Page/index.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import cx from 'classnames'
+import withSizes from 'react-sizes'
+import { Link } from 'react-router-dom'
 import Icon from '@santiment-network/ui/Icon'
+import { mapSizesToProps } from '../../utils/withSizes'
 import styles from './index.module.scss'
 
 const Page = ({
@@ -17,7 +20,11 @@ const Page = ({
       <div className={cx(styles.header, headerClassName)}>
         <h1 className={styles.title}>{title}</h1>
         {actions}
-        {isPhone && <Icon type='search' />}
+        {isPhone && (
+          <Link to='/search' className={styles.search}>
+            <Icon type='search' className={styles.search__icon} />
+          </Link>
+        )}
       </div>
       <main className={cx(isWithPadding && styles.main)}>{children}</main>
     </div>
@@ -27,4 +34,4 @@ Page.defaulProps = {
   isWithPadding: true
 }
 
-export default Page
+export default withSizes(mapSizesToProps)(Page)
