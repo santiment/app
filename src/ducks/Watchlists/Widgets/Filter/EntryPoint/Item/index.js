ed48c99b9636040ac03a02438d1e9f3b55da99bb
haritonasty <a.kharitonovaspb@gmail.com>
2020-12-22 22:59:31 +0300

    reset button instead 'all assets' checkbox

--- src/ducks/Watchlists/Widgets/Filter/EntryPoint/Item/index.js
+++ src/ducks/Watchlists/Widgets/Filter/EntryPoint/Item/index.js
@@ -1,11 +1,15 @@
 import React from 'react'
+import cx from 'classnames'
 import { Checkbox } from '@santiment-network/ui/Checkboxes'
 import styles from './index.module.scss'
 
-const Item = ({ name, id, slug, isActive, onClick, ...rest }) => {
+const Item = ({ name, id, slug, isActive, onClick, isDisabled, ...rest }) => {
   return (
-    <div className={styles.item} onClick={onClick}>
-      <Checkbox isActive={isActive} />
+    <div
+      className={cx(styles.item, isDisabled && styles.disabled)}
+      onClick={() => (isDisabled ? null : onClick())}
+    >
+      <Checkbox isActive={isActive} disabled={isDisabled} />
       <span className={styles.name}>{name}</span>
     </div>
   )
--- src/ducks/Watchlists/Widgets/Filter/EntryPoint/utils.js
+++ src/ducks/Watchlists/Widgets/Filter/EntryPoint/utils.js
@@ -1,10 +1,5 @@
 export const ALL_ASSETS_TEXT = 'All assets'
 
-export const MESSAGES = {
-  allAssets: 'Deselect ‘All assets’ to customize your Screener',
-  empty: 'Please select at least one filter'
-}
-
 export const MAX_VISIBLE_SYMBOLS = 48
 
 export function makeHumanReadableState (state, idNameMap) {
