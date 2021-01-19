686e85542cff22de7c6678d7d574071ee3eb194c
haritonasty <a.kharitonovaspb@gmail.com>
2021-01-19 14:00:41 +0300

    reverse screeners sorting

--- src/components/Navbar/Watchlists/WatchlistsDropdown.js
+++ src/components/Navbar/Watchlists/WatchlistsDropdown.js
@@ -11,8 +11,8 @@ import { useUser } from '../../../stores/user'
 import { useUserWatchlists } from '../../../ducks/Watchlists/gql/hooks'
 import { useAddressWatchlists } from '../../../ducks/Watchlists/gql/queries'
 import { getAddressesWatchlistLink } from '../../../ducks/Watchlists/url'
-import styles from './WatchlistsDropdown.module.scss'
 import { sortById } from '../../../utils/sortMethods'
+import styles from './WatchlistsDropdown.module.scss'
 
 const WatchlistsDropdown = ({ activeLink }) => {
   const [projectsWatchlists, loading] = useUserWatchlists()
