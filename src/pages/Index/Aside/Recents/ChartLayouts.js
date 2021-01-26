b534980dd3fab720242cb69970cf844ecf2ec3c7
Rinat F <rinatficht@gmail.com>
2021-01-26 16:58:24 +0300

    chore: self-review

--- src/pages/Index/Aside/Recents/ChartLayouts.js
+++ src/pages/Index/Aside/Recents/ChartLayouts.js
@@ -16,17 +16,15 @@ const getItem = getItemBuilder(gql`
 
 const ChartLayout = ({ title }) => (title ? <Column>{title}</Column> : null)
 
-const ChartLayouts = ({ ids }) => {
-  return (
-    <Recent
-      title='Chart Layouts'
-      rightHeader=''
-      ids={ids}
-      getItem={getItem}
-      getLink={getLink}
-      Item={ChartLayout}
-    />
-  )
-}
+const ChartLayouts = ({ ids }) => (
+  <Recent
+    title='Chart Layouts'
+    rightHeader=''
+    ids={ids}
+    getItem={getItem}
+    getLink={getLink}
+    Item={ChartLayout}
+  />
+)
 
 export default ChartLayouts
