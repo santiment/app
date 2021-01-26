ecf109d616b1bc91fd6143fc705c58029e71e75e
haritonasty <a.kharitonovaspb@gmail.com>
2021-01-26 14:21:03 +0300

    queries and mutations for table configurations

---
+++ src/ducks/Watchlists/Widgets/Table/Columns/gql/mutations.js
@@ -0,0 +1,50 @@
+import gql from 'graphql-tag'
+import { TABLE_CONFIG_FRAGMENT } from './queries'
+
+export const DELETE_TABLE_CONFIG_MUTATION = gql`
+  mutation deleteTableConfiguration($id: Int!) {
+    deleteTableConfiguration(id: $id) {
+      id
+    }
+  }
+`
+
+export const CREATE_TABLE_CONFIG_MUTATION = gql`
+  mutation createTableConfiguration(
+    $title: String!
+    $isPublic: Boolean
+    $columns: json
+  ) {
+    createTableConfiguration(
+      settings: { title: $title, isPublic: $isPublic, columns: $columns }
+    ) {
+      ...tableConfig
+    }
+  }
+  ${TABLE_CONFIG_FRAGMENT}
+`
+
+export const UPDATE_TABLE_CONFIG_MUTATION = gql`
+  mutation createTableConfiguration(
+    $id: Int
+    $title: String!
+    $isPublic: Boolean
+    $columns: json
+  ) {
+    createTableConfiguration(
+      id: $id
+      settings: { title: $title, isPublic: $isPublic, columns: $columns }
+    ) {
+      ...tableConfig
+    }
+  }
+  ${TABLE_CONFIG_FRAGMENT}
+`
+
+export const UPDATE_WATCHLIST_TABLE_CONFIG_MUTATION = gql`
+  mutation updateWatchlist($id: Int!, $tableConfigurationId: Int) {
+    updateWatchlist(id: $id, tableConfigurationId: $tableConfigurationId) {
+      id
+    }
+  }
+`
---
+++ src/ducks/Watchlists/Widgets/Table/Columns/gql/queries.js
@@ -0,0 +1,44 @@
+import gql from 'graphql-tag'
+
+export const TABLE_CONFIG_FRAGMENT = gql`
+  fragment tableConfig on UserList {
+    id: Int
+    title: String
+    isPublic: Boolean
+    columns: json
+    pageSize: Int
+    insertedAt: DateTime
+    user {
+      id
+    }
+`
+
+export const FEATURED_TABLE_CONFIGS_QUERY = gql`
+  query featuredTableConfigurations {
+    id
+    title
+    insertedAt: DateTime
+  }
+`
+
+export const TABLE_CONFIGS_QUERY = gql`
+  query TableConfigurations {
+    id
+    title
+    isPublic
+    insertedAt: DateTime
+    user {
+      id
+    }
+`
+
+export const WATCHLIST_TABLE_CONFIG_QUERY = gql`
+  query watchlist($id: ID!) {
+    watchlist(id: $id) {
+      tableConfiguration {
+        id
+        title
+        columns
+    }
+  }
+`
