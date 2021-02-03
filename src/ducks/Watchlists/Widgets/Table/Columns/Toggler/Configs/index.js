424cfaac37ef7be0a8076177d559196dd0439207
haritonasty <a.kharitonovaspb@gmail.com>
2021-02-03 11:14:08 +0300

    icon from san-ui

--- src/ducks/Watchlists/Widgets/Table/Columns/Toggler/Configs/index.js
+++ src/ducks/Watchlists/Widgets/Table/Columns/Toggler/Configs/index.js
@@ -14,21 +14,6 @@ import {
 import UpdateConfig from './UpdateConfig'
 import styles from './index.module.scss'
 
-const SmallSave = ({ onClick }) => (
-  <svg
-    onClick={onClick}
-    xmlns='http://www.w3.org/2000/svg'
-    width='13'
-    height='13'
-  >
-    <path
-      fillRule='evenodd'
-      d='M.5 0C.2 0 0 .2 0 .5v11c0 .3.2.5.5.5h11c.3 0 .5-.2.5-.5V3.6l-.1-.3-3-3.1-.4-.2h-8zM1 11V1h1.5v4c0 .3.2.5.5.5h4c.3 0 .5-.2.5-.5V1h.8L11 3.8V11H1zM6.5 1h-3v3.5h3V1z'
-      clipRule='evenodd'
-    />
-  </svg>
-)
-
 const ConfigsMenu = ({
   setOpen,
   open,
@@ -146,7 +131,8 @@ const ConfigsMenu = ({
                       className={styles.actions}
                       onClick={evt => evt.stopPropagation()}
                     >
-                      <SmallSave
+                      <Icon
+                        type='disk-small'
                         onClick={() =>
                           updateTableConfig(config, {
                             columns: { metrics: activeColumns }
