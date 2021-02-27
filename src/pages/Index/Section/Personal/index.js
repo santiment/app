74c552ff660fda865123ce74cea0828689521a29
Rinat F <rinatficht@gmail.com>
2021-02-27 12:17:23 +0300

    feat: add anchor for sharing cabinet on home page

--- src/pages/Index/Section/Personal/index.js
+++ src/pages/Index/Section/Personal/index.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom'
 import StartGuide from './StartGuide'
 import Cabinet from './Cabinet'
 import { Section, Container, Row } from '../index'
@@ -6,27 +7,53 @@ import { Tab } from '../Trends'
 import Toggle from '../../../../components/VisibilityIndicator/Toggle'
 import styles from './index.module.scss'
 
-const LS_PERSONAL_TAB = 'LS_PERSONAL_TAB'
-const TabType = {
-  START_GUIDE: 'Quick Start Guide',
-  CABINET: 'Cabinet'
-}
-const TabTypeComponent = {
-  [TabType.START_GUIDE]: StartGuide,
-  [TabType.CABINET]: Cabinet
+const tabByAnchor = () => {
+  const hash = window.location.hash
+
+  if (hash === TabType.CABINET.anchor) {
+    return TabType.CABINET.title
+  }
 }
 
-const toggleVisibility = tab => (tab ? null : TabType.START_GUIDE)
+const LS_PERSONAL_TAB = 'LS_PERSONAL_TAB'
+const TabType = {
+  START_GUIDE: {
+    title: 'Quick Start Guide'
+  },
+  CABINET: {
+    title: 'Cabinet',
+    anchor: '#cabinet'
+  }
+}
+const TabTypeComponent = {
+  [TabType.START_GUIDE.title]: StartGuide,
+  [TabType.CABINET.title]: Cabinet
+}
+
+const toggleVisibility = tab => (tab ? null : TabType.START_GUIDE.title)
 const saveTab = tab => localStorage.setItem(LS_PERSONAL_TAB, tab || '')
 function loadTab () {
-  const tab = localStorage.getItem(LS_PERSONAL_TAB)
-  return tab === null ? TabType.START_GUIDE : tab
+  const savedTab = localStorage.getItem(LS_PERSONAL_TAB)
+  const anchorTab = tabByAnchor()
+
+  const target = anchorTab || savedTab
+  const currentTab = target === null ? TabType.START_GUIDE.title : target
+
+  if (currentTab && currentTab !== savedTab) {
+    saveTab(target)
+  }
+
+  return currentTab
 }
 
 const Header = ({ tabState }) => (
   <Row className={styles.header}>
-    <Tab tab={TabType.START_GUIDE} tabState={tabState} />
-    <Tab tab={TabType.CABINET} tabState={tabState} className={styles.cabinet} />
+    <Tab tab={TabType.START_GUIDE.title} tabState={tabState} />
+    <Tab
+      tab={TabType.CABINET.title}
+      tabState={tabState}
+      className={styles.cabinet}
+    />
     <Toggle
       className={styles.toggle}
       isActive={tabState[0]}
@@ -36,16 +63,30 @@ const Header = ({ tabState }) => (
 )
 
 const Personal = () => {
-  const tabState = useState(loadTab)
-  const activeTab = tabState[0]
+  const [activeTab, setTab] = useState(loadTab)
   const Content = TabTypeComponent[activeTab]
 
+  const history = useHistory()
+
   useEffect(() => saveTab(activeTab), [activeTab])
+
+  function updateTab (target) {
+    setTab(target)
+
+    if (target) {
+      const tab = Object.values(TabType).find(({ title }) => title === target)
+      const anchor = tab.anchor ? tab.anchor : ''
+
+      if (tab) {
+        history.replace(`${window.location.pathname}${anchor}`)
+      }
+    }
+  }
 
   return (
     <Section>
       <Container>
-        <Header tabState={tabState} />
+        <Header tabState={[activeTab, updateTab]} />
         {Content && <Content />}
       </Container>
     </Section>
