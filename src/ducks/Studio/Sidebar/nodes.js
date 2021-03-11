1df74bdb4a664f39d4ee8794a9328693e65d3e9c
Rinat F <rinatficht@gmail.com>
2021-03-11 17:15:44 +0300

    chore: rename holder distribution labels

--- src/ducks/Studio/Sidebar/nodes.js
+++ src/ducks/Studio/Sidebar/nodes.js
@@ -3,7 +3,7 @@ import { WIDGET } from './Button/types'
 export const HOLDER_DISTRIBUTION_NODE = {
   key: 'holder_distribution',
   type: WIDGET,
-  label: 'Holders Distribution',
+  label: 'By number of addresses',
   abbreviation: 'hd',
   group: 'Supply distribution',
   category: 'On-chain',
@@ -13,7 +13,7 @@ export const HOLDER_DISTRIBUTION_NODE = {
 export const HOLDER_DISTRIBUTION_COMBINED_BALANCE_NODE = {
   key: 'holder_distribution_combined_balance',
   type: WIDGET,
-  label: 'Holders Distribution Combined Balance',
+  label: 'By balance of addresses',
   abbreviation: 'hdcb',
   group: 'Supply distribution',
   category: 'On-chain',
