68941e11b86268d32cf34ceb90039e6b1f914b8b
Rinat F <rinatficht@gmail.com>
2021-03-18 16:07:07 +0300

    chore: update description for MVRV z score

--- src/ducks/dataHub/metrics/descriptions.js
+++ src/ducks/dataHub/metrics/descriptions.js
@@ -250,7 +250,8 @@ export const Description = {
   ),
   [Metric.whale_transaction_count.key]:
     'Number of Transactions Transferring More Than 100k USD',
-  [Metric.mvrv_usd_z_score.key]: 'Daily MVRV-Z USD Score'
+  [Metric.mvrv_usd_z_score.key]:
+    'Shows the ratio between the difference of market cap and realized cap, and the standard deviation of market cap'
 }
 
 export const rebuildDescriptions = Submetrics => {
