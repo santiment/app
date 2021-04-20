dfbb7f1ac28a97b20344e0ce95835543eaeb69d6
haritonasty <a.kharitonovaspb@gmail.com>
2021-04-20 12:34:00 +0300

    NavbarMore styles

--- src/components/Navbar/NavbarMore/NavbarMore.js
+++ src/components/Navbar/NavbarMore/NavbarMore.js
@@ -1,35 +1,34 @@
 import React from 'react'
-import cx from 'classnames'
 import Panel from '@santiment-network/ui/Panel/Panel'
 import Button from '@santiment-network/ui/Button'
-import { BUSINESS_PRODUCTS } from '../SantimentProductsTooltip/Products'
-import LinkWithArrow from '../Link'
-import externalStyles from '../Watchlists/MarketDropdown.module.scss'
+import SantimentLogo from '../SantimentProductsTooltip/SantimentLogo'
+import {
+  BUSINESS_PRODUCTS,
+  CHAIN_PRODUCTS
+} from '../SantimentProductsTooltip/Products'
 import styles from './NavbarMore.module.scss'
 
 const NavbarMore = ({ activeLink, links }) => {
   return (
     <Panel>
-      <div className={externalStyles.wrapper}>
-        <div className={cx(externalStyles.block, externalStyles.list)}>
-          <div className={styles.leftBlock}>
-            {links.map((item, index) => {
-              const { ddParams, Dropdown, ...rest } = item
-              return (
-                <Button
-                  key={index}
-                  fluid
-                  variant='ghost'
-                  isActive={item.link === activeLink}
-                  {...rest}
-                  className={externalStyles.btn}
-                />
-              )
-            })}
-          </div>
+      <div className={styles.wrapper}>
+        <div className={styles.leftBlock}>
+          {links.map((item, index) => {
+            const { ddParams, Dropdown, ...rest } = item
+            return (
+              <Button
+                key={index}
+                fluid
+                variant='ghost'
+                isActive={item.link === activeLink}
+                {...rest}
+                className={styles.btn}
+              />
+            )
+          })}
         </div>
-        <div className={externalStyles.block}>
-          <h3 className={externalStyles.title}>Santiment Products</h3>
+        <div className={styles.productBlock}>
+          <h3 className={styles.title}>SAN Business</h3>
           <div className={styles.products}>
             {BUSINESS_PRODUCTS.map(({ img, title, to }, index) => {
               return (
@@ -41,19 +40,32 @@ const NavbarMore = ({ activeLink, links }) => {
                   className={styles.product}
                 >
                   <img src={img} alt={title} className={styles.productImg} />
-
                   {title}
                 </a>
               )
             })}
           </div>
-
-          <LinkWithArrow
-            to='https://santiment.net'
-            title='Go to Santiment.net'
-            className={styles.link}
-          />
         </div>
+        <div className={styles.productBlock}>
+          <h3 className={styles.title}>SAN chain</h3>
+          <div className={styles.products}>
+            {CHAIN_PRODUCTS.map(({ img, title, to }, index) => {
+              return (
+                <a
+                  href={to}
+                  key={index}
+                  target='_blank'
+                  rel='noopener noreferrer'
+                  className={styles.product}
+                >
+                  <img src={img} alt={title} className={styles.productImg} />
+                  {title}
+                </a>
+              )
+            })}
+          </div>
+        </div>
+        <SantimentLogo className={styles.mainLink} />
       </div>
     </Panel>
   )
