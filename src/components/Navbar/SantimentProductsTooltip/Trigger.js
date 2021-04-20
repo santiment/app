062c4e1cf210083c82663d4f445d2c8ec2b4b4a1
haritonasty <a.kharitonovaspb@gmail.com>
2021-04-20 20:52:31 +0300

    upd san-ui

--- src/components/Navbar/SantimentProductsTooltip/Trigger.js
+++ src/components/Navbar/SantimentProductsTooltip/Trigger.js
@@ -6,7 +6,14 @@ export const ProductsTrigger = ({ isOpen, className }) => (
   <div className={cx(styles.trigger, isOpen && styles.opened, className)}>
     <svg width='16' height='16' fill='none'>
       <rect width='7' height='7' fill='var(--jungle-green-light-3)' rx='1' />
-      <rect width='7' height='7' x='9' y='9' fill='#FFD49C' rx='1' />
+      <rect
+        width='7'
+        height='7'
+        x='9'
+        y='9'
+        fill='var(--texas-rose-light-3)'
+        rx='1'
+      />
       <rect
         width='7'
         height='7'
