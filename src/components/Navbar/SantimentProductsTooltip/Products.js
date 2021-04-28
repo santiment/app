7ae873acd5f4c9293661b37e35eef19cd7d035bb
Nastya <haritonasty@users.noreply.github.com>
2021-04-28 19:11:28 +0300

    add hunters

--- src/components/Navbar/SantimentProductsTooltip/Products.js
+++ src/components/Navbar/SantimentProductsTooltip/Products.js
@@ -3,7 +3,7 @@ import sanapi from './icons/sanapi.svg'
 import sanbase from './icons/sanbase.svg'
 import insights from './icons/insights.svg'
 import sansheets from './icons/sansheets.svg'
-// import sanhunters from './icons/sanhunters.svg'
+import sanhunters from './icons/sanhunters.svg'
 import datascience from './icons/datascience.svg'
 
 export const BUSINESS_PRODUCTS = [
@@ -43,12 +43,12 @@ export const CHAIN_PRODUCTS = [
     description: 'Decentralized marketplace for crypto price signals',
     to: 'https://sanr.santiment.net'
   },
-  // {
-  //   img: sanhunters,
-  //   title: 'Sanhunters',
-  //   description: 'Get rewarded for market research and your crypto know-how',
-  //   to: 'https://hunters.santiment.net'
-  // },
+  {
+    img: sanhunters,
+    title: 'Sanhunters',
+    description: 'Get rewarded for market research and your crypto know-how',
+    to: 'https://hunters.santiment.net'
+  },
   {
     img: insights,
     title: 'Insights',
