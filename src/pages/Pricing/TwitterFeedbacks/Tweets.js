8926d7bb91ad78cc19c9628e495c033d9aa871be
Rinat F <rinatficht@gmail.com>
2021-05-04 00:06:09 +0300

    feat: add few new tweets

--- src/pages/Pricing/TwitterFeedbacks/Tweets.js
+++ src/pages/Pricing/TwitterFeedbacks/Tweets.js
@@ -164,6 +164,40 @@ export const TweetsParsed = [
     <a href='https://twitter.com/santimentfeed/status/1383450235498754059?ref_src=twsrc%5Etfw'>
       April 17, 2021
     </a>
+  </blockquote>,
+
+  <blockquote className='twitter-tweet'>
+    <p lang='en' dir='ltr'>
+      Signed up to the free trial{' '}
+      <a href='https://twitter.com/santimentfeed?ref_src=twsrc%5Etfw'>
+        @santimentfeed
+      </a>{' '}
+      Love the interface so far.{' '}
+      <a href='https://twitter.com/search?q=%24ETH&amp;src=ctag&amp;ref_src=twsrc%5Etfw'>
+        $ETH
+      </a>{' '}
+      supply on exchanges continues to go down while price is still heading to
+      $3,000.{' '}
+      <a href='https://twitter.com/hashtag/ethererum?src=hash&amp;ref_src=twsrc%5Etfw'>
+        #ethererum
+      </a>{' '}
+      <a href='https://t.co/M9sYzjaiOb'>pic.twitter.com/M9sYzjaiOb</a>
+    </p>
+    &mdash; John 🇦🇺 (@Johninvesting){' '}
+    <a href='https://twitter.com/Johninvesting/status/1389019263902445571?ref_src=twsrc%5Etfw'>
+      May 3, 2021
+    </a>
+  </blockquote>,
+
+  <blockquote className='twitter-tweet'>
+    <p lang='en' dir='ltr'>
+      This product has made me a ton of money. Trade profitability has
+      skyrocketed. Lemme know if you want to chat about it
+    </p>
+    &mdash; Ahab (@Ahab_1337){' '}
+    <a href='https://twitter.com/Ahab_1337/status/1389198702355570694?ref_src=twsrc%5Etfw'>
+      May 3, 2021
+    </a>
   </blockquote>
 ]
 
