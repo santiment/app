84e69a1281fd4073448a48d609a1412d2102910f
Rinat F <rinatficht@gmail.com>
2021-05-04 14:44:49 +0300

    fix: update labels for holders metrics

--- src/ducks/dataHub/metrics/labels.js
+++ src/ducks/dataHub/metrics/labels.js
@@ -2,9 +2,9 @@ import { Metric } from './index'
 
 const Labels = {
   [Metric.dormant_circulation.key]: 'Dormant Circulation (365d)',
-  holders_distribution_1_to_10: 'Supply distribution (number of addresses)',
-  percent_of_holders_distribution_combined_balance_1_to_10:
-    'Supply distribution (balance of addresses)'
+  holders_distribution_1_to_10: '[1 - 10) coins',
+  percent_of_holders_distribution_combined_balance_1_to_10: '[1 - 10) coins %',
+  holders_labeled_distribution_1_to_10: '[1 - 10) coins'
 }
 
 export function getMetricLabel ({ key, label: metricLabel, base }, project) {
