d5659efc8b3758d0a6e0d8486f3fab8c972d2bf1
haritonasty <a.kharitonovaspb@gmail.com>
2021-06-01 15:51:54 +0300

    detect device type

--- src/pages/Account/SettingsSessions.js
+++ src/pages/Account/SettingsSessions.js
@@ -3,6 +3,8 @@ import { useQuery } from '@apollo/react-hooks'
 import Button from '@santiment-network/ui/Button'
 import gql from 'graphql-tag'
 import Settings from './Settings'
+import Mobile from './devices/Mobile'
+import Tablet from './devices/Tablet'
 import Desktop from './devices/Desktop'
 import { sortBy } from '../../utils/sortMethods'
 import { addDays, getDateFormats, getTimeFormats } from '../../utils/dates'
@@ -28,6 +30,17 @@ export const TODAY = new Date().toLocaleDateString()
 export const YESTERDAY = addDays(new Date(), -1).toLocaleDateString()
 
 const sortSessions = sessions => sessions.sort(sortBy('lastActiveAt'))
+
+const getPlatformIcon = platform => {
+  if (platform.match(/Tablet|iPad/i)) return <Tablet />
+  if (
+    platform.match(
+      /Mobile|Windows Phone|Lumia|Android|webOS|iPhone|iPod|Blackberry|PlayBook|BB10|Opera Mini|\bCrMo\/|Opera Mobi/i
+    )
+  ) {
+    return <Mobile />
+  } else return <Desktop />
+}
 
 const formatDate = date => {
   const dateFormat = new Date(date)
@@ -66,9 +79,7 @@ const SettingsSessions = () => {
     <Settings id='sessions' header='Current authorized sessions'>
       {sessions.map(({ client, platform, isCurrent, lastActiveAt }) => (
         <Settings.Row className={styles.wrapper}>
-          <div className={styles.image}>
-            <Desktop />
-          </div>
+          <div className={styles.image}>{getPlatformIcon(platform)}</div>
           <div className={styles.info}>
             <span className={styles.platform}>
               {platform}, {client}
