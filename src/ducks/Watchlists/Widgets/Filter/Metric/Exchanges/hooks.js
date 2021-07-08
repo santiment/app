12f8a3c6a9b9a82c9465e4473a9f107dfb4c84c0
haritonasty <a.kharitonovaspb@gmail.com>
2021-07-08 15:31:06 +0300

    top exchanges at first

--- src/ducks/Watchlists/Widgets/Filter/Metric/Exchanges/hooks.js
+++ src/ducks/Watchlists/Widgets/Filter/Metric/Exchanges/hooks.js
@@ -14,10 +14,41 @@ const EXCHANGES_QUERY = gql`
   }
 `
 
+const TOP_EXCHANGES = new Set([
+  'binance',
+  'ftx',
+  'coinbase',
+  'bitfinex',
+  'kucoin',
+  'huobi',
+  'okex',
+  'bitmex',
+  'bybit',
+  'bitstamp',
+  'bittrex',
+  'gemini',
+  'kraken'
+])
+
+const sortExchanges = (a, b) => {
+  const isTopA = TOP_EXCHANGES.has(a.exchange.toLowerCase())
+  const isTopB = TOP_EXCHANGES.has(b.exchange.toLowerCase())
+  console.log(
+    a.exchange.toLowerCase(),
+    b.exchange.toLowerCase(),
+    isTopA,
+    isTopB
+  )
+  if (isTopA) {
+    return isTopB ? 0 : -1
+  } else return isTopB ? 1 : 0
+}
+
 export function useMarketExchanges () {
   const { data, loading } = useQuery(EXCHANGES_QUERY)
 
-  return useMemo(() => [data ? data.getMarketExchanges : ARRAY, loading], [
-    data
-  ])
+  return useMemo(
+    () => [data ? data.getMarketExchanges.sort(sortExchanges) : ARRAY, loading],
+    [data]
+  )
 }
