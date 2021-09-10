6d3d0c0a66932f46bd4d9558a2f3a09d367ae157
DmitriVanGuard <dimavanguard@gmail.com>
2021-09-10 22:10:49 +0300

    Hiding conversations

--- src/pages/Index/Aside/index.js
+++ src/pages/Index/Aside/index.js
@@ -2,15 +2,11 @@ import React from 'react'
 import Section from './Section'
 import Recents from './Recents'
 import FeaturedWatchlists from '../../../ducks/Watchlists/Cards/Featured'
-import Conversations from '../Conversations/Conversations'
 import styles from './index.module.scss'
 
 const Aside = ({ className }) => (
   <aside className={className}>
     <Recents />
-    <Section title='Conversations'>
-      <Conversations />
-    </Section>
     <Section title='Indices'>
       <FeaturedWatchlists className={styles.watchlist} />
     </Section>
