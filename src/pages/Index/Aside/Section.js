a01284ee769f4175cd946dd73307566c2c3fd823
Anastasya Kharitonova <haritonasty@pm.me>
2021-09-30 17:46:37 +0300

    aside changes

--- src/pages/Index/Aside/Section.js
+++ src/pages/Index/Aside/Section.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import styles from './index.module.scss'
 
-const Section = ({ title, children }) => (
+const Section = ({ title, children, Icon }) => (
   <div className={styles.section}>
-    <h3 className={styles.title}>{title}</h3>
+    <h3 className={styles.title}>
+      <Icon />
+      {title}
+    </h3>
     {children}
   </div>
 )
