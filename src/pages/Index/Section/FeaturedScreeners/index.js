f4d6a4a32af8b989c029a9deb3be512fc365d134
Anastasya Kharitonova <haritonasty@pm.me>
2021-10-06 12:02:40 +0300

    link and query with function

--- src/pages/Index/Section/FeaturedScreeners/index.js
+++ src/pages/Index/Section/FeaturedScreeners/index.js
@@ -1,14 +1,27 @@
 import React, { useMemo, useState, useEffect } from 'react'
+import gql from 'graphql-tag'
+import { Link } from 'react-router-dom'
 import Tabs from '@santiment-network/ui/Tabs'
 import { useFeaturedScreeners } from '../../../../ducks/Watchlists/gql/lists/hooks'
 import { SCREENERS_ANCHOR } from '../../Navigation/anchors'
+import { getScreenerLink } from '../../../../ducks/Watchlists/url'
 import { Section, Container } from '../index'
 import styles from './index.module.scss'
+
+const FEATURED_SCREENERS_QUERY = gql`
+  query featuredScreeners {
+    watchlists: featuredScreeners {
+      id
+      name
+      function
+    }
+  }
+`
 
 const FeaturedScreeners = () => {
   const [tab, setTab] = useState(null)
   const [activeScreener, setActiveScreener] = useState(null)
-  const [featuredScreeners] = useFeaturedScreeners()
+  const [featuredScreeners] = useFeaturedScreeners(FEATURED_SCREENERS_QUERY)
 
   const tabs = useMemo(
     () => featuredScreeners.map(({ name }) => name).slice(0, 4),
@@ -19,14 +32,29 @@ const FeaturedScreeners = () => {
     () => {
       if (featuredScreeners.length !== 0) {
         setTab(featuredScreeners[0].name)
-        setActiveScreener(featuredScreeners[0])
       }
     },
     [featuredScreeners]
   )
 
+  useEffect(
+    () => {
+      if (tab) {
+        const item = featuredScreeners.find(screener => screener.name === tab)
+        setActiveScreener(item)
+      }
+    },
+    [tab]
+  )
+
+  const link = activeScreener && getScreenerLink(activeScreener)
+
   return (
-    <Section title='Explore screeners' id={SCREENERS_ANCHOR}>
+    <Section
+      title='Explore screeners'
+      id={SCREENERS_ANCHOR}
+      className={styles.section}
+    >
       {tab && (
         <Tabs
           className={styles.tabs}
@@ -36,6 +64,14 @@ const FeaturedScreeners = () => {
           classes={styles}
         />
       )}
+      {link && (
+        <Link to={link} className={styles.link}>
+          Start researching {tab}
+        </Link>
+      )}
+      <Container>
+        {activeScreener && JSON.stringify(activeScreener.function)}
+      </Container>
     </Section>
   )
 }
