3a3a17b911410615d85fd936a1cd14740e49e37c
DmitriVanGuard <dimavanguard@gmail.com>
2021-10-21 18:29:38 +0400

    Hiding social volume column. Hiding last trend's chart value

--- src/ducks/TrendsTable/columns.js
+++ src/ducks/TrendsTable/columns.js
@@ -1,26 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import {
-  useTrendWordContext,
-  useTrendSocialVolume,
-  useTrendSocialVolumeChange
-} from './hooks'
+import { useTrendWordContext, useTrendSocialVolume } from './hooks'
 import { prepareColumns } from '../_Table'
 import { INDEX_COLUMN } from '../_Table/columns'
 import { Skeleton } from '../../components/Skeleton'
 import MiniChart from '../../components/MiniChart'
-import PercentChanges from '../../components/PercentChanges'
 import { WordCloud } from '../../components/WordCloud/WordCloud'
 import styles from './index.module.scss'
 
 const Loader = () => <Skeleton show className={styles.chart__skeleton} />
-
-const SocialVolumeScore = ({ value, change }) => (
-  <>
-    {value}
-    <PercentChanges changes={change} className={styles.change__percent} />
-  </>
-)
 
 const SocialVolumeChart = ({ trend, words }) => {
   const { data, isLoading } = useTrendSocialVolume(words, trend)
@@ -41,16 +29,6 @@ const SocialVolumeChart = ({ trend, words }) => {
   )
 }
 
-const SocialVolume = ({ trend, words }) => {
-  const { value, change } = useTrendSocialVolumeChange(words, trend)
-
-  return (
-    <div className={styles.change}>
-      {value ? <SocialVolumeScore value={value} change={change} /> : <Loader />}
-    </div>
-  )
-}
-
 const ConnectedWords = ({ trend, words }) => {
   const { data, isLoading } = useTrendWordContext(words, trend)
 
@@ -67,7 +45,7 @@ export const Column = {
   INDEX: INDEX_COLUMN.id,
   SOCIAL_VOLUME: 'Soc. vol., 24h',
   TRENDING_CHART: 'Trending chart, 7d',
-  CONNECTED_WORDS: 'Connected words'
+  CONNECTED_WORDS: 'Connected words',
 }
 
 export const COLUMNS = [INDEX_COLUMN].concat(
@@ -79,24 +57,20 @@ export const COLUMNS = [INDEX_COLUMN].concat(
         <Link className={styles.word__link} to={`/labs/trends/explore/${word}`}>
           {word}
         </Link>
-      )
-    },
-    {
-      title: Column.SOCIAL_VOLUME,
-      render: (trend, { words }) => <SocialVolume trend={trend} words={words} />
+      ),
     },
     {
       title: Column.TRENDING_CHART,
       render: (trend, { words }) => (
         <SocialVolumeChart trend={trend} words={words} />
-      )
+      ),
     },
     {
       title: Column.CONNECTED_WORDS,
       className: styles.cloud,
       render: (trend, { words }) => (
         <ConnectedWords trend={trend} words={words} />
-      )
-    }
-  ])
+      ),
+    },
+  ]),
 )
