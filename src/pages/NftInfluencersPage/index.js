02ab75f5928bc077b0b3fd9f90ee738a313fd294
SaeidR <saeidr@evenset.com>
2021-12-17 05:45:45 -0500

    header added

--- src/pages/NftInfluencersPage/index.js
+++ src/pages/NftInfluencersPage/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
+import cx from 'classnames'
 import { Helmet } from 'react-helmet'
 import { withRenderQueueProvider } from '../../ducks/renderQueue/viewport'
 import DashboardLayout from '../../ducks/Dashboards/DashboardLayout'
+import SharePage from '../../components/SharePage/SharePage'
+import externalStyles from './../StablecoinsPage/StablecoinsPage.module.scss'
 
 const NftInfluencersPage = () => {
   return (
@@ -19,7 +22,16 @@ const NftInfluencersPage = () => {
           }
         ]}
       />
-      <h1>NFT Influencers Trx</h1>
+
+      <div className={externalStyles.header}>
+        <div className={cx(externalStyles.inner, externalStyles.content)}>
+          <div className={externalStyles.pageDescription}>
+            <h3 className={externalStyles.title}>NFT Influencers Trx</h3>
+            <SharePage />
+          </div>
+        </div>
+      </div>
+
     </DashboardLayout>
   )
 }
