dd7f90a80e788169d1c9e4068bc486319b0742e7
Igor <igor.k@santiment.net>
2021-12-24 16:16:45 +0300

    Fixed code style

--- src/ducks/Alert/components/AlertModalContent/StepsContent/MetricAndConditions/MetricSelector/MetricSelector.js
+++ src/ducks/Alert/components/AlertModalContent/StepsContent/MetricAndConditions/MetricSelector/MetricSelector.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useMemo, useState } from "react";
-import { useField } from "formik";
-import MetricsList from "./MetricsList/MetricsList";
-import Search from "../../../../../../Studio/Sidebar/Search";
-import { SEARCH_PREDICATE_ONLY_METRICS } from "../../../../../../Studio/Compare/Comparable/Metric";
-import { useMergedTimeboundSubmetrics } from "../../../../../../dataHub/timebounds";
-import { getCategoryGraph } from "../../../../../../Studio/Sidebar/utils";
-import { useProject } from "../../../../../../../hooks/project";
-import { useIsBetaMode } from "../../../../../../../stores/ui";
-import { filterOnlyMetrics, getByAvailable } from "./utils";
-import styles from "./MetricSelector.module.scss";
+import React, { useEffect, useMemo, useState } from 'react'
+import { useField } from 'formik'
+import MetricsList from './MetricsList/MetricsList'
+import Search from '../../../../../../Studio/Sidebar/Search'
+import { SEARCH_PREDICATE_ONLY_METRICS } from '../../../../../../Studio/Compare/Comparable/Metric'
+import { useMergedTimeboundSubmetrics } from '../../../../../../dataHub/timebounds'
+import { getCategoryGraph } from '../../../../../../Studio/Sidebar/utils'
+import { useProject } from '../../../../../../../hooks/project'
+import { useIsBetaMode } from '../../../../../../../stores/ui'
+import { filterOnlyMetrics, getByAvailable } from './utils'
+import styles from './MetricSelector.module.scss'
 
 const searchProps = {
-  iconPosition: "left",
+  iconPosition: 'left',
   inputProps: {
-    placeholder: "Search for a metric"
+    placeholder: 'Search for a metric'
   },
   className: styles.search,
   searchPredicate: SEARCH_PREDICATE_ONLY_METRICS
-};
+}
 
 //_: Array(4)
 // 0:
@@ -40,49 +40,49 @@ const suggestedMetrics = {
   Suggested: {
     _: [
       {
-        item: { category: "Suggested", label: "Price", key: "price_usd" },
+        item: { category: 'Suggested', label: 'Price', key: 'price_usd' },
         subitems: []
       },
       {
         item: {
-          category: "Suggested",
-          label: "Daily Active Addresses",
-          key: "daily_active_addresses"
+          category: 'Suggested',
+          label: 'Daily Active Addresses',
+          key: 'daily_active_addresses'
         },
         subitems: []
       }
     ]
   }
-};
+}
 
 const MetricSelector = ({ selectedMetric, metrics, target, onChange }) => {
-  const [, , { setValue: setMetric }] = useField("settings.metric");
-  const isBeta = useIsBetaMode();
-  const [project] = useProject(target.slug);
-  const [categories, setCategories] = useState({});
+  const [, , { setValue: setMetric }] = useField('settings.metric')
+  const isBeta = useIsBetaMode()
+  const [project] = useProject(target.slug)
+  const [categories, setCategories] = useState({})
 
   const allMetrics = useMemo(
     () =>
       getByAvailable(
-        metrics.some(m => m.includes("nvt"))
-          ? metrics.concat("nvt_5min")
+        metrics.some(m => m.includes('nvt'))
+          ? metrics.concat('nvt_5min')
           : metrics,
         target
       ),
     [metrics, target]
-  );
+  )
 
-  const allSubmetrics = useMergedTimeboundSubmetrics(metrics);
+  const allSubmetrics = useMergedTimeboundSubmetrics(metrics)
 
   useEffect(() => {
-    const submetrics = filterOnlyMetrics(allSubmetrics);
-    const newCategories = getCategoryGraph(allMetrics, [], submetrics, isBeta);
-    setCategories({ ...suggestedMetrics, ...newCategories });
-  }, [metrics, allMetrics, isBeta]);
+    const submetrics = filterOnlyMetrics(allSubmetrics)
+    const newCategories = getCategoryGraph(allMetrics, [], submetrics, isBeta)
+    setCategories({ ...suggestedMetrics, ...newCategories })
+  }, [metrics, allMetrics, isBeta])
 
-  function handleSelectMetric(metric) {
-    setMetric(metric.key);
-    onChange(metric);
+  function handleSelectMetric (metric) {
+    setMetric(metric.key)
+    onChange(metric)
   }
 
   return (
@@ -100,7 +100,7 @@ const MetricSelector = ({ selectedMetric, metrics, target, onChange }) => {
         selectedMetric={selectedMetric}
       />
     </>
-  );
-};
+  )
+}
 
-export default MetricSelector;
+export default MetricSelector
