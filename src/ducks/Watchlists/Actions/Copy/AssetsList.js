038d9608d1939703eb0839b864bc6a0ec0071f0d
SaeidR <saeidr@evenset.com>
2022-01-27 02:43:45 -0500

    search refactored

--- src/ducks/Watchlists/Actions/Copy/AssetsList.js
+++ src/ducks/Watchlists/Actions/Copy/AssetsList.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import cx from 'classnames'
 import List from 'react-virtualized/dist/commonjs/List'
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
@@ -12,10 +12,23 @@ const ROW_HEIGHT = 32
 
 const AssetsList = ({ items, selectedItems, onToggleAsset, classes, withSearch = false }) => {
   const [filter, setFilter] = useState()
+  const [filteredItems, setFilteredItems] = useState(items)
+
+  useEffect(() => {
+    let filteredItems = [...items]
+    if (withSearch && filter) {
+      const _filter = filter.trim().toLowerCase()
+      filteredItems = filteredItems.filter(({name, ticker}) => {
+        return name.toLowerCase().includes(_filter) || ticker.toLowerCase().includes(_filter)
+      })
+    }
+    setFilteredItems(filteredItems)
+  }, [items, filter, withSearch])
 
   const rowRenderer = ({ key, index, style }) => {
-    const { name, ticker, id } = items[index]
+    const { name, ticker, id } = filteredItems[index]
     const isSelectedAsset = selectedItems.has(id)
+
     return (
       <div
         key={key}
@@ -31,8 +44,8 @@ const AssetsList = ({ items, selectedItems, onToggleAsset, classes, withSearch =
   }
 
   const wrapperStyles = {
-    '--height': items.length > 4 ? '145px' : `${32 * items.length}px`,
-    '--padding-right': items.length > 4 ? '0' : `5px`
+    '--height': filteredItems.length > 4 ? '145px' : `${32 * filteredItems.length}px`,
+    '--padding-right': filteredItems.length > 4 ? '0' : `5px`
   }
 
   return (
@@ -42,7 +55,7 @@ const AssetsList = ({ items, selectedItems, onToggleAsset, classes, withSearch =
           type='text'
           icon='search-small'
           iconPosition='left'
-          onChange={setFilter}
+          onChange={e => setFilter(e.target.value)}
           defaultValue={filter}
           className={inputStyles.search}
           placeholder='Type to search'
@@ -54,7 +67,7 @@ const AssetsList = ({ items, selectedItems, onToggleAsset, classes, withSearch =
             width={width}
             height={height}
             rowHeight={ROW_HEIGHT}
-            rowCount={items.length}
+            rowCount={filteredItems.length}
             overscanRowCount={5}
             rowRenderer={rowRenderer}
             className={styles.list}
