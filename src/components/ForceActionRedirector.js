0390042b431d6aa5538a275b8833b7945ff7e2e9
SaeidR <saeidr@evenset.com>
2022-02-10 08:32:37 -0500

    phoenix socket connection added

--- src/components/ForceActionRedirector.js
+++ src/components/ForceActionRedirector.js
@@ -3,11 +3,33 @@ import { push } from 'react-router-redux'
 import { useUser } from '../stores/user'
 import { store } from '../redux'
 import { PATHS } from '../paths'
+import { useChannel } from '../utils/phoenixContext'
+import { useUserSubscriptionStatus } from '../stores/user/subscriptions'
 
 const ignoredPages = ['/privacy-policy', '/roadmap']
+const PRICING_PAGE = '/pricing'
+const TRY_WAIT_TIME_MS = 3000
+const MAX_TABS_FREE = 2
+const MAX_TABS_PRO = 4
 
 const ForceActionRedirector = ({ pathname }) => {
   const { user } = useUser()
+  const { isPro, isProPlus } = useUserSubscriptionStatus()
+  const channel = useChannel()
+
+  const checkOpenTabs = () => {
+    if (!channel || isProPlus || ignoredPages.includes(PRICING_PAGE)) return
+    channel
+      .push('open_tabs', {}, 10000)
+      .receive('ok', ({ open_tabs }) => {
+        const max_tabs = isPro ? MAX_TABS_PRO : MAX_TABS_FREE
+        if (open_tabs > max_tabs) {
+          console.log('TODO: SHOW MODAL NOW')
+        }
+      })
+      .receive('error', () => setTimeout(checkOpenTabs, TRY_WAIT_TIME_MS))
+      .receive('timeout', () => setTimeout(checkOpenTabs, TRY_WAIT_TIME_MS))
+  }
 
   useEffect(() => {
     if (!user || ignoredPages.includes(pathname)) return
@@ -15,8 +37,10 @@ const ForceActionRedirector = ({ pathname }) => {
       store.dispatch(push(PATHS.GDPR))
     } else if (!user.username) {
       store.dispatch(push(PATHS.USERNAME))
+    } else {
+      checkOpenTabs()
     }
-  }, [user, pathname])
+  }, [user, isPro, isProPlus, pathname, channel])
 
   return null
 }
---
+++ src/utils/phoenixContext.js
@@ -0,0 +1,43 @@
+import {
+  createElement,
+  createContext,
+  useState,
+  useEffect,
+  useContext
+} from 'react'
+import { Socket } from 'phoenix'
+import { useUserSessions } from '../pages/Account/SettingsSessions'
+import { useUser } from '../stores/user'
+
+export const HOST_NAME = new URL(process.env.REACT_APP_BACKEND_URL).hostname
+
+export const PhoenixContext = createContext({ channel: null })
+
+export const PhoenixProvider = ({ children }) => {
+  const { user } = useUser()
+  const [sessions] = useUserSessions()
+  const session = sessions.find(session => session.isCurrent)
+  const [channel, setChannel] = useState(null)
+
+  useEffect(() => {
+    if (!session || !session.jti || !user) return
+    const socket = new Socket(`wss://${HOST_NAME}/socket`, {
+      params: { jti: session.jti }
+    })
+    socket.connect()
+    const _channel = socket.channel(`users:${user.id}`, {})
+    _channel.join().receive('ok', () => setChannel(_channel))
+  }, [user, session])
+
+  return createElement(
+    PhoenixContext.Provider,
+    {
+      value: {
+        channel
+      }
+    },
+    children
+  )
+}
+
+export const useChannel = () => useContext(PhoenixContext).channel
