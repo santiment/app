ee71ff3defc7255811033d7f3349efd15edc34fc
SaeidR <saeidr@evenset.com>
2022-02-14 04:57:38 -0500

    open_restricted_tabs added

--- src/components/ForceActionRedirector.js
+++ src/components/ForceActionRedirector.js
@@ -3,34 +3,39 @@ import { push } from 'react-router-redux'
 import { useUser } from '../stores/user'
 import { store } from '../redux'
 import { PATHS } from '../paths'
-import { useChannel } from '../utils/socketHooks'
+import { useSocket } from '../utils/socketHooks'
 import { useUserSubscriptionStatus } from '../stores/user/subscriptions'
 
 const ignoredPages = ['/privacy-policy', '/roadmap']
-const LIMIT_TAB_ALLOWED_PAGES = ['/pricing']
+const LIMIT_TAB_PAGES = ['/screener', '/watchlists', '/charts']
 const TRY_WAIT_TIME_MS = 3000
 const MAX_TABS_FREE = 2
 const MAX_TABS_PRO = 4
 
+const shouldCheckPage = pathname => LIMIT_TAB_PAGES.find(page => page.startsWith(pathname))
+
 const ForceActionRedirector = ({ pathname }) => {
   const { user } = useUser()
   const { isPro, isProPlus } = useUserSubscriptionStatus()
-  const { channel, setShowTabLimitModal } = useChannel()
+  const { socket, setShowTabLimitModal } = useSocket()
 
   function checkOpenTabs () {
-    if (!channel || isProPlus || LIMIT_TAB_ALLOWED_PAGES.includes(pathname)) {
+    if (!socket || isProPlus || !shouldCheckPage(pathname)) {
       setShowTabLimitModal(false)
       return
     }
 
-    channel
-      .push('open_tabs', {}, 10000)
-      .receive('ok', ({ open_tabs }) => {
-        const max_tabs = isPro ? MAX_TABS_PRO : MAX_TABS_FREE
-        setShowTabLimitModal(open_tabs > max_tabs)
-      })
-      .receive('error', () => setTimeout(checkOpenTabs, TRY_WAIT_TIME_MS))
-      .receive('timeout', () => setTimeout(checkOpenTabs, TRY_WAIT_TIME_MS))
+    const channel = socket.channel(`open_restricted_tabs:${user.id}`, {})
+    channel.join().receive('ok', () => {
+      channel
+        .push('open_restricted_tabs', {}, 10000)
+        .receive('ok', ({ open_restricted_tabs }) => {
+          const max_tabs = isPro ? MAX_TABS_PRO : MAX_TABS_FREE
+          setShowTabLimitModal(open_restricted_tabs > max_tabs)
+        })
+        .receive('error', () => setTimeout(checkOpenTabs, TRY_WAIT_TIME_MS))
+        .receive('timeout', () => setTimeout(checkOpenTabs, TRY_WAIT_TIME_MS))
+    })
   }
 
   useEffect(() => {
@@ -42,7 +47,7 @@ const ForceActionRedirector = ({ pathname }) => {
     } else {
       checkOpenTabs()
     }
-  }, [user, isPro, isProPlus, pathname, channel])
+  }, [user, isPro, isProPlus, pathname, socket])
 
   return null
 }
--- src/utils/socketHooks.js
+++ src/utils/socketHooks.js
@@ -3,7 +3,7 @@ import {
   createContext,
   useState,
   useEffect,
-  useContext
+  useContext,
 } from 'react'
 import { Socket } from 'phoenix'
 import { useUserSessions } from '../pages/Account/SettingsSessions'
@@ -12,7 +12,7 @@ import { useUser } from '../stores/user'
 export const HOST_NAME = new URL(process.env.REACT_APP_BACKEND_URL).hostname
 
 export const SocketContext = createContext({
-  channel: null,
+  socket: null,
   showTabLimitModal: false,
   setShowTabLimitModal: null
 })
@@ -21,24 +21,23 @@ export const SocketProvider = ({ children }) => {
   const { user } = useUser()
   const [sessions] = useUserSessions()
   const session = sessions.find(session => session.isCurrent)
-  const [channel, setChannel] = useState(null)
+  const [socket, setSocket] = useState(null)
   const [showTabLimitModal, setShowTabLimitModal] = useState(false)
 
   useEffect(() => {
     if (!user || !session || !session.jti) return
-    const socket = new Socket(`wss://${HOST_NAME}/socket`, {
+    const _socket = new Socket(`wss://${HOST_NAME}/socket`, {
       params: { jti: session.jti }
     })
-    socket.connect()
-    const _channel = socket.channel(`users:${user.id}`, {})
-    _channel.join().receive('ok', () => setChannel(_channel))
+    _socket.connect()
+    setSocket(_socket)
   }, [user, session])
 
   return createElement(
     SocketContext.Provider,
     {
       value: {
-        channel,
+        socket,
         showTabLimitModal,
         setShowTabLimitModal
       }
@@ -47,4 +46,4 @@ export const SocketProvider = ({ children }) => {
   )
 }
 
-export const useChannel = () => useContext(SocketContext)
+export const useSocket = () => useContext(SocketContext)
