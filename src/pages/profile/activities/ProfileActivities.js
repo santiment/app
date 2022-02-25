5322947703d6a346fb2f566b99cecaad09a9276e
SaeidR <saeidr@evenset.com>
2022-02-25 02:31:20 -0500

    bugs fixeD

--- src/pages/profile/activities/ProfileActivities.js
+++ src/pages/profile/activities/ProfileActivities.js
@@ -33,24 +33,20 @@ const ProfileActivities = ({ profileId, currentUserId }) => {
   const [step, setStep] = useState(window.location.hash || STEPS.INSIGHTS)
   const [templates] = useUserTemplates(profileId)
   const { data, loading } = useProfileActivities(profileId, currentUserId)
-
-  const goTo = val => {
-    window.location.hash = val
-    return setStep(val)
-  }
-
-  if (loading) {
-    return <LoaderImage />
-  }
-
+  const hasData = !loading && data
   const {
     insightsCount = { totalCount: 0 },
     triggers = ARRAY,
     watchlists = ARRAY,
     addressesWatchlists = ARRAY
-  } = data
+  } = data || {}
   const screeners = watchlists.filter(checkIsScreener)
   const projectWatchlists = watchlists.filter(checkIsNotScreener)
+  const goTo = val => {
+    if (loading) return
+    window.location.hash = val
+    return setStep(val)
+  }
 
   return (
     <div className={styles.container}>
@@ -59,7 +55,7 @@ const ProfileActivities = ({ profileId, currentUserId }) => {
           className={cx(styles.link, step === STEPS.INSIGHTS && styles.active)}
           onClick={() => goTo(STEPS.INSIGHTS)}
         >
-          Insights <Counter value={insightsCount.totalCount} />
+          Insights <Counter value={hasData ? insightsCount.totalCount : 0} />
         </div>
         <div
           className={cx(
@@ -68,7 +64,7 @@ const ProfileActivities = ({ profileId, currentUserId }) => {
           )}
           onClick={() => goTo(STEPS.WATCHLISTS)}
         >
-          Watchlists <Counter value={projectWatchlists.length} />
+          Watchlists <Counter value={hasData ? projectWatchlists.length : 0} />
         </div>
         <div
           className={cx(
@@ -77,19 +73,20 @@ const ProfileActivities = ({ profileId, currentUserId }) => {
           )}
           onClick={() => goTo(STEPS.ADDRESSES_WATCHLISTS)}
         >
-          Addresses Watchlists <Counter value={addressesWatchlists.length} />
+          Addresses Watchlists{' '}
+          <Counter value={hasData ? addressesWatchlists.length : 0} />
         </div>
         <div
           className={cx(styles.link, step === STEPS.SCREENERS && styles.active)}
           onClick={() => goTo(STEPS.SCREENERS)}
         >
-          Screeners <Counter value={screeners.length} />
+          Screeners <Counter value={hasData ? screeners.length : 0} />
         </div>
         <div
           className={cx(styles.link, step === STEPS.SIGNALS && styles.active)}
           onClick={() => goTo(STEPS.SIGNALS)}
         >
-          Alerts <Counter value={triggers.length} />
+          Alerts <Counter value={hasData ? triggers.length : 0} />
         </div>
         <div
           className={cx(
@@ -98,32 +95,40 @@ const ProfileActivities = ({ profileId, currentUserId }) => {
           )}
           onClick={() => goTo(STEPS.CHART_LAYOUTS)}
         >
-          Chart Layouts <Counter value={templates.length} />
+          Chart Layouts <Counter value={hasData ? templates.length : 0} />
         </div>
       </div>
       <div className={styles.right}>
-        {step === STEPS.INSIGHTS && <PublicInsights userId={profileId} />}
-        {step === STEPS.SIGNALS && (
-          <PublicSignals userId={profileId} data={triggers} />
-        )}
-        {step === STEPS.WATCHLISTS && (
-          <PublicWatchlists watchlists={projectWatchlists} Card={ProjectCard} />
-        )}
-        {step === STEPS.ADDRESSES_WATCHLISTS && (
-          <PublicWatchlists
-            watchlists={addressesWatchlists}
-            Card={AddressCard}
-          />
-        )}
-        {step === STEPS.SCREENERS && (
-          <PublicWatchlists
-            watchlists={screeners}
-            path='/screener/'
-            Card={ProjectCard}
-          />
-        )}
-        {step === STEPS.CHART_LAYOUTS && (
-          <ProfileTemplates userId={profileId} data={templates} />
+        {loading && <LoaderImage className={styles.loader} />}
+        {hasData && (
+          <>
+            {step === STEPS.INSIGHTS && <PublicInsights userId={profileId} />}
+            {step === STEPS.SIGNALS && (
+              <PublicSignals userId={profileId} data={triggers} />
+            )}
+            {step === STEPS.WATCHLISTS && (
+              <PublicWatchlists
+                watchlists={projectWatchlists}
+                Card={ProjectCard}
+              />
+            )}
+            {step === STEPS.ADDRESSES_WATCHLISTS && (
+              <PublicWatchlists
+                watchlists={addressesWatchlists}
+                Card={AddressCard}
+              />
+            )}
+            {step === STEPS.SCREENERS && (
+              <PublicWatchlists
+                watchlists={screeners}
+                path='/screener/'
+                Card={ProjectCard}
+              />
+            )}
+            {step === STEPS.CHART_LAYOUTS && (
+              <ProfileTemplates userId={profileId} data={templates} />
+            )}
+          </>
         )}
       </div>
     </div>
