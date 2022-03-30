9e36bc7e70a8d531a45f1b3220241d1ba7c30d88
DmitriVanGuard <dimavanguard@gmail.com>
2022-03-30 16:04:13 +0300

    Using webkit comments

--- src/ducks/HistoricalBalance/Comments/index.js
+++ src/ducks/HistoricalBalance/Comments/index.js
@@ -1,52 +1,32 @@
-import React, { useState, useEffect } from 'react'
+import React, { useMemo } from 'react'
 import toReact from 'svelte-adapter/react'
-import SvelteComments from 'insights-app/lib/components/comments/Comments'
+import { CommentsType } from 'webkit/api/comments'
+import SvelteComments from 'webkit/ui/Comments/svelte'
 import { useBlockchainAddress } from '../hooks'
-import {
-  buildCommentsGetter,
-  buildCommentCreator,
-  editComment,
-  deleteComment
-} from '../../../components/Insight/comments/utils.js'
+import { useUser } from '../../../stores/user'
+import { onAnonComment } from '../../../pages/Studio/utils'
 import styles from './index.module.scss'
-
-const EMPTY = []
-const BLOCKCHAIN_COMMENT = 'BLOCKCHAIN_ADDRESS'
-
-const getComments = buildCommentsGetter(BLOCKCHAIN_COMMENT)
-const createComment = buildCommentCreator(BLOCKCHAIN_COMMENT)
-const dataAccessor = ({ data }) => data.comments
-
-function useComments (id) {
-  const [comments, setComments] = useState(EMPTY)
-
-  useEffect(() => {
-    getComments(id)
-      .then(dataAccessor)
-      .then(setComments)
-      .catch(console.warn)
-  }, [id])
-
-  return comments
-}
 
 const ReactComments = toReact(SvelteComments, {}, 'div')
 const Comments = ({ settings }) => {
-  const { id, commentsCount = 0 } = useBlockchainAddress(settings)
-  const comments = useComments(id)
+  const blockchainAddress = useBlockchainAddress(settings)
+  const { user } = useUser()
+  const commentsFor = useMemo(() => {
+    blockchainAddress.user = {}
+    blockchainAddress.title = settings.address
+    return blockchainAddress
+  }, [settings, blockchainAddress])
 
   return (
     <div className={styles.wrapper}>
-      <ReactComments
-        className={styles.styles}
-        id={id}
-        comments={comments}
-        commentsCount={commentsCount}
-        getComments={getComments}
-        createComment={createComment}
-        editComment={editComment}
-        deleteComment={deleteComment}
-      />
+      {commentsFor.id && (
+        <ReactComments
+          type={CommentsType.Address}
+          commentsFor={commentsFor}
+          currentUser={user}
+          onAnonComment={onAnonComment }
+        />
+      )}
     </div>
   )
 }
