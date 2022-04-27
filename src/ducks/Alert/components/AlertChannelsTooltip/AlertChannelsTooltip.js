c12af240fbd86bb673706e5b7aab55530405ea1c
Dmitri Marov <dimavanguard@gmail.com>
2022-04-27 12:01:27 +0300

    Update AlertChannelsTooltip.js

--- src/ducks/Alert/components/AlertChannelsTooltip/AlertChannelsTooltip.js
+++ src/ducks/Alert/components/AlertChannelsTooltip/AlertChannelsTooltip.js
@@ -34,7 +34,7 @@ const AlertChannelsTooltip = ({ signal }) => {
     (!isEmailConnected && channel.includes('email')) ||
     (!isTelegramConnected && channel.includes('telegram'))
 
-  const channelItems = Array.isArray(channel) ? channel : []
+  const channelItems = Array.isArray(channel) ? channel : [channel]
   const channelTitles = channelItems.map((item) => {
     if (typeof item === 'string') {
       return capitalizeStr(item)
