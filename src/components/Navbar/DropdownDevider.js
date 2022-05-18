21352806f790bfb627ea9e4c791e20d033d80000
DmitriVanGuard <dimavanguard@gmail.com>
2022-05-18 10:20:00 +0300

    Junk removed

--- src/components/Navbar/DropdownDevider.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import styles from './Devider.module.scss'
-
-const DropdownDevider = ({ ...props }) => {
-  return <div {...props} className={styles.wrapper} />
-}
-
-export default DropdownDevider
