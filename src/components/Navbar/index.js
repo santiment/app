29bf4c23c2b9a34ffcf8735c4450812f0cad32a2
SaeidR <saeidr@evenset.com>
2022-06-22 04:14:28 -0400

    variant added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -20,7 +20,7 @@ export default ({ pathname }) => {
     if (user) user.subscriptions = subscriptions || []
     return user
   }, [user, subscriptions])
-  const variant = useGoogleOptimize('CG6tK8zVQ9Ww9wb7WJtYmg', [0, 1, 2], 3000)
+  const variant = useGoogleOptimize('CG6tK8zVQ9Ww9wb7WJtYmg', [0, 1, 2], 1000)
   const appVersionState = useSelector((state) => state.app.appVersionState)
   const isAppUpdateAvailable = appVersionState === APP_STATES.NEW_AVAILABLE
 
