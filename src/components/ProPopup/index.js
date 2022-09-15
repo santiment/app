a9a4ea450270865f9cb0d7d1422973e9bde57a5c
SaeidR <saeid.r@santiment.net>
2022-09-15 10:17:31 -0400

    extra return removed

--- src/components/ProPopup/index.js
+++ src/components/ProPopup/index.js
@@ -7,32 +7,30 @@ import styles from './index.module.scss'
 
 const EMPTY_ARRAY = []
 
-const ProPopup = ({ title, description, features, isOpen = false, ...props }) => {
-  return (
-    <Dialog size='m' title={title} open={isOpen} classes={styles} {...props}>
-      <div className={styles.container}>
-        <div className={styles.description}>{description}</div>
-        <div className={styles.features}>
-          {features.map((item, idx) => (
-            <div className={styles.feature} key={idx}>
-              {item}
-            </div>
-          ))}
-        </div>
-        <Button
-          as={Link}
-          to='/pricing'
-          variant='fill'
-          accent='orange'
-          className={styles.btn}
-          onClick={props.onClose}
-        >
-          Go Pro
-        </Button>
+const ProPopup = ({ title, description, features, isOpen = false, ...props }) => (
+  <Dialog size='m' title={title} open={isOpen} classes={styles} {...props}>
+    <div className={styles.container}>
+      <div className={styles.description}>{description}</div>
+      <div className={styles.features}>
+        {features.map((item, idx) => (
+          <div className={styles.feature} key={idx}>
+            {item}
+          </div>
+        ))}
       </div>
-    </Dialog>
-  )
-}
+      <Button
+        as={Link}
+        to='/pricing'
+        variant='fill'
+        accent='orange'
+        className={styles.btn}
+        onClick={props.onClose}
+      >
+        Go Pro
+      </Button>
+    </div>
+  </Dialog>
+)
 
 ProPopup.propTypes = {
   title: PropTypes.string,
