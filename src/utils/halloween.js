8277b0951d4bb63e4ecc3d33ef658a4852af68df
Igor <ihqrk@pm.me>
2022-10-26 16:03:40 +0300

    Refactored prev halloween components, added nightmode hook

--- src/utils/halloween.js
+++ src/utils/halloween.js
@@ -1,31 +1,16 @@
-import { loadKeyState } from './localStorage'
+import { useEffect } from 'react'
+import { ui, useTheme } from '../stores/ui/theme'
 
-// true if Oct 25 - Nov 1 and user didn't toggle night mode
-export function isShowHalloween() {
-  const isDisabledByUser = loadKeyState('disabledHalloweenMode')
-
-  return isHalloweenDay() && !isDisabledByUser
-}
-
-// true if Oct 25 - Nov 1
 export function isHalloweenDay() {
   const currentDate = new Date()
   const currentMonth = currentDate.getMonth()
   const currentDay = currentDate.getDate()
 
-  return (currentMonth === 9 && currentDay > 24) || (currentMonth === 10 && currentDay < 2)
+  return (currentMonth === 9 && currentDay > 24) || (currentMonth === 10 && currentDay < 5)
 }
 
-export function getCheckedGraves() {
-  const res = localStorage.getItem('halloweenGraves')
+export const useHalloweenMode = () => {
+  const { isNightMode } = useTheme()
 
-  return res ? res.split(',') : []
-}
-
-export function addGrave(slug) {
-  const graves = new Set(getCheckedGraves())
-  graves.add(slug)
-  localStorage.setItem('halloweenGraves', [...graves].toString())
-
-  return graves
+  useEffect(() => !isNightMode && isHalloweenDay() && ui.toggleNightMode(), [])
 }
