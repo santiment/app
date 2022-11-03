1f5cda1eaf077fe4ba86870296d98f599ac62e92
SaeidR <saeid.r@santiment.net>
2022-11-03 02:16:22 -0400

    ethSpentOverTime query updated

--- src/ducks/Studio/timeseries/queries/ethSpentOverTime.js
+++ src/ducks/Studio/timeseries/queries/ethSpentOverTime.js
@@ -1,7 +1,7 @@
 import gql from 'graphql-tag'
 
 export const ETH_SPENT_OVER_TIME_QUERY = gql`
-  query ethSpentOverTime($slug: String, $from: DateTime, $to: DateTime, $interval: interval) {
+  query ethSpentOverTime($slug: String!, $from: DateTime!, $to: DateTime!, $interval: interval) {
     ethSpentOverTime: projectBySlug(slug: $slug) {
       id
       ethSpentOverTime(from: $from, to: $to, interval: $interval) {
