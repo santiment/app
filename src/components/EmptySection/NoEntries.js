89c827aa30d6947315c8475d53a314469c3189a6
SaeidR <saeid.r@santiment.net>
2022-11-09 03:06:46 -0500

    no entries style bugs fixed

--- src/components/EmptySection/NoEntries.js
+++ src/components/EmptySection/NoEntries.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import cx from 'classnames'
 import EmptySection from './EmptySection'
 import styles from './NoEntries.module.scss'
 
 const NoEntries = ({ title, desc, children, maxWidth }) => (
   <EmptySection>
     <div className={styles.description} style={{ maxWidth }}>
-      {title && <div className='body-2 txt-b mrg--b mrg-xs'>{title}</div>}
+      {title && <div className={cx('body-1 txt-b mrg--b mrg-s', styles.title)}>{title}</div>}
       {desc && <div className='body-2 mrg--b mrg-l'>{desc}</div>}
       {children}
     </div>
