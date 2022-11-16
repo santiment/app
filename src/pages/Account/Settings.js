a760e2cd878442e04335971c8d0b1521d6424d4f
SaeidR <saeid.r@santiment.net>
2022-11-16 05:48:30 -0500

    scroll-margin-top added

--- src/pages/Account/Settings.js
+++ src/pages/Account/Settings.js
@@ -3,16 +3,13 @@ import cx from 'classnames'
 import { PanelWithHeader as Panel } from '@santiment-network/ui'
 import styles from './AccountPage.module.scss'
 
-const Settings = ({id, ...props}) => (
-  <div className='relative fluid'>
-    <div id={id} className={styles.virtualDiv} />
-    <Panel
-      {...props}
-      className={cx(styles.settings, props.className)}
-      headerClassName={styles.settings__header}
-      contentClassName={cx(styles.settings__content, props.contentClassName)}
-    />
-  </div>
+const Settings = (props) => (
+  <Panel
+    {...props}
+    className={cx(styles.settings, props.className)}
+    headerClassName={styles.settings__header}
+    contentClassName={cx(styles.settings__content, props.contentClassName)}
+  />
 )
 
 Settings.Row = (props) => <div className={cx(styles.setting, props.className)} {...props} />
