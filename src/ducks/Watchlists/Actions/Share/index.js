c3ee767fc888e27b68fad3382c38c927e35698fa
DmitriVanGuard <dimavanguard@gmail.com>
2022-11-16 23:59:07 +0300

    Watchlist share event source fix

--- src/ducks/Watchlists/Actions/Share/index.js
+++ src/ducks/Watchlists/Actions/Share/index.js
@@ -38,8 +38,8 @@ const Share = ({ watchlist, isAuthor, className, customLink, isMobile }) => {
       shareLink={customLink || shortShareLink}
       isDisabled={!isPublic}
       isMobile={isMobile}
-      source='watchlists'
-      feature='watchlist'
+      source={type}
+      feature={type}
       trigger={(props) => (
         <>
           {isMobile ? (
